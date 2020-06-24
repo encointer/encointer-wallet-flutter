3b69700f71e3bac319f47734873690de3a733958
clangenb <christian.langenbacher91@gmail.com>
2020-06-24 15:20:39 +0200

    [utils/format] fix formatting for strings smaller than pad

--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -19,7 +19,7 @@ class Fmt {
   }
 
   static String address(String addr, {int pad = 6}) {
-    if (addr == null || addr.length == 0) {
+    if (addr == null || addr.length < pad) {
       return addr;
     }
     return addr.substring(0, pad) + '...' + addr.substring(addr.length - pad);
