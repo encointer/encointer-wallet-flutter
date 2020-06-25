encointer/encointer-wallet-flutter

commit ab71cc2679a8cb7411dc5fe275fd70797f42ea7f

[fixpointUtil] fix handling of 0 and add some tests

lib/js_service_encointer/src/utils/fixpointUtil.js
@@ -45,10 +45,15 @@ export function parserFixPoint (upper, lower) {
 export function toFixPoint (upper, lower) {
   assertLength(upper, lower);
   return (num) => {
-    const [upperBits, lowerBits] = num.toString(2).split('.');
+    const [upperBits, lowerBits] = num.toString().split('.');
     assert(upperBits.length <= upper, 'Number is larger than maximum in '.concat(upper, 'bit'));
-    const bits = upperBits.concat(lowerBits.length > lower ? lowerBits.substr(0, lower) : lowerBits.padEnd(lower, 0));
-    return new BN(bits, 2);
+    if (lowerBits !== undefined) {
+      const bits = upperBits.concat(lowerBits.length > lower ? lowerBits.substr(0, lower) : lowerBits.padEnd(lower, 0));
+      return new BN(bits, 2);
+    } else {
+      const bits = upperBits.padEnd(upper + lower, 0);
+      return new BN(bits, 2);
+    }
   };
 }
 

lib/js_service_encointer/src/utils/fixpointUtil.test.js
@@ -1,0 +1,20 @@
+import '../';
+import BN from 'bn.js';
+import { toI16F16 } from './fixpointUtil';
+
+describe('fixpointUtil', () => {
+  describe('toFixPoint', () => {
+    it('should parse integer to fixPoint', async () => {
+      const result = toI16F16(1);
+      expect(result).toEqual(new BN(0x80000000, 2));
+    });
+    it('should parse 0 to fixPoint', async () => {
+      const result = toI16F16(0);
+      expect(result).toEqual(new BN(0x0, 2));
+    });
+    it('should parse 1.1 to fixPoint', async () => {
+      const result = toI16F16(1.1);
+      expect(result).toBeDefined();
+    });
+  });
+});

commit b3c276012e8fa5b773dfe57d0ca59ad4308dc15f

! [fixPointUtil] fix conversion bug introduced with ab71cc2679a8cb7411dc5fe275fd70797f42ea7f

lib/js_service_encointer/src/utils/fixpointUtil.js
@@ -45,7 +45,7 @@ export function parserFixPoint (upper, lower) {
 export function toFixPoint (upper, lower) {
   assertLength(upper, lower);
   return (num) => {
-    const [upperBits, lowerBits] = num.toString().split('.');
+    const [upperBits, lowerBits] = num.toString(2).split('.');
     assert(upperBits.length <= upper, 'Number is larger than maximum in '.concat(upper, 'bit'));
     if (lowerBits !== undefined) {
       const bits = upperBits.concat(lowerBits.length > lower ? lowerBits.substr(0, lower) : lowerBits.padEnd(lower, 0));