c7f141f7f24d73e93b5ddeb52d993fbdcbd658ff
Brenzikofer Alain <alain.brenzikofer@scs.ch>
2020-11-07 17:42:16 +0100

    fix apk build path

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -25,6 +25,6 @@ flutter build apk --release --flavor play
 flutter build appbundle --release --flavor play
 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/play/release/app-play-release.apk $_
+mkdir -p android/app/build/outputs/apk/; cp build/app/outputs/flutter-apk/app-play-release.apk $_
 # copy the AAB where AppCenter will find it
-mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/playRelease/app-play-release.aab $_
+mkdir -p android/app/build/outputs/bundle/; cp build/app/outputs/bundle/playRelease/app-play-release.aab $_
