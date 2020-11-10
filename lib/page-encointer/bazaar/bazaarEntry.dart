59c05435c1b17146ef2749427ab49d1025b14fa3
BignaH <73821294+BignaH@users.noreply.github.com>
2020-11-10 15:51:43 +0100

    Bazaar structure inclusion (#103)
    * Add Bazaar Page to BottomNavigatorBar
    
    * Change bazaar icon
    
    * inclusion of seach bar
    
    * bug fix of i18n bazaar.index
    
    * clean up of imports in bazaarEntry page
    
    * use themecolor instead of hard coded
    
    * change bazar to bazaar
    
    * change button order (wallet - bazaar - ceremonies - profile)

---
+++ lib/page-encointer/bazaar/bazaarEntry.dart
@@ -0,0 +1,80 @@
+import 'package:encointer_wallet/store/app.dart';
+import 'package:encointer_wallet/utils/i18n/index.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BazaarEntry extends StatelessWidget {
+  BazaarEntry(this.store);
+
+  final AppStore store;
+
+  @override
+  Widget build(BuildContext context) {
+    final Map dic = I18n.of(context).bazaar;
+
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    dic['bazaar.title'],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).cardColor,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Expanded(
+            //    child: Observer(
+            //        builder: (_) =>
+            //            Text(store.encointer.currentPhase.toString()))),
+            searchBar(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget searchBar(BuildContext context) {
+  return Stack(
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(left: 40, right: 40, top: 15),
+        child: Material(
+          borderRadius: BorderRadius.circular(30.0),
+          elevation: 8,
+          child: Container(
+            child: TextFormField(
+              cursorColor: Theme.of(context).primaryColor,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                prefixIcon:
+                Icon(
+                    Icons.search,
+                    color: Theme.of(context).primaryColor,
+                    size: 30
+                ),
+                hintText: "What are you looking for?",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
