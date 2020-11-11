8b22d7220875ad37c0588fb0510ffe129434772e
BignaH <bigna.h@hotmail.com>
2020-11-11 13:03:20 +0100

    Add storeClass and vertical scrol option

---
+++ lib/page-encointer/bazaar/components/storeClass.dart
@@ -0,0 +1,12 @@
+class Store{
+  int adID;
+  String dateAdded;
+  String title;
+  String desc;
+  int mobileNo;
+  String image;
+  String location;
+
+  Store(this.adID, this.dateAdded, this.title, this.desc,
+      this.mobileNo, this.image, this.location);
+}
