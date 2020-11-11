d439df43ea158fadea2055c77282eed14229190b
BignaH <bigna.h@hotmail.com>
2020-11-11 11:38:19 +0100

    fine tune layout of articles

--- lib/utils/i18n/bazaar.dart
+++ lib/utils/i18n/bazaar.dart
@@ -1,7 +1,9 @@
 const Map<String, String> enBazaar = {
   'bazaar.title': 'Bazaar',
-  'article': 'Article',
+  'article': 'Articles',
   'article.insert': '+ Add new article',
   'article.new': 'Featuring Newest',
   'looking.for': 'What are you looking for?',
+  'show.all': '+ Show all',
+  'recently.added': 'Recently added'
 };
