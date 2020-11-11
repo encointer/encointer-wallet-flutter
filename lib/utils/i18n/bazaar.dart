04b755b189bde2af10aed39f92fe67dbd35b5e2b
BignaH <bigna.h@hotmail.com>
2020-11-11 10:29:05 +0100

    implementation of article draft

--- lib/utils/i18n/bazaar.dart
+++ lib/utils/i18n/bazaar.dart
@@ -1,4 +1,7 @@
 const Map<String, String> enBazaar = {
   'bazaar.title': 'Bazaar',
-
+  'article': 'Article',
+  'article.insert': '+ Add new article',
+  'article.new': 'Featuring Newest',
+  'looking.for': 'What are you looking for?',
 };
