97ae6130443d44ba4c5748dfc37601db2e40610c
BignaH <bigna.h@hotmail.com>
2020-11-11 13:03:20 +0100

    Add storeClass and vertical scrol option

--- lib/page-encointer/bazaar/bazaarEntry.dart
+++ lib/page-encointer/bazaar/bazaarEntry.dart
@@ -3,6 +3,7 @@ import 'package:encointer_wallet/common/components/roundedButton.dart';
 import 'package:encointer_wallet/common/components/roundedCard.dart';
 import 'package:encointer_wallet/page-encointer/bazaar/components/itemCard.dart';
 import 'package:encointer_wallet/page-encointer/bazaar/components/articleClass.dart';
+import 'package:encointer_wallet/page-encointer/bazaar/components/storeClass.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/utils/i18n/index.dart';
 import 'package:flutter/cupertino.dart';
@@ -16,6 +17,8 @@ class BazaarEntry extends StatelessWidget {
 
   // dummy List creation
   List<Article> dummyList = createDummyList();
+  List<Store> dummyListStore = createDummyListStore();
+
   @override
   Widget build(BuildContext context) {
     final Map dic = I18n.of(context).bazaar;
@@ -42,9 +45,21 @@ class BazaarEntry extends StatelessWidget {
               ),
             ),
             searchBar(context, dic),
-            Container(
-             margin: EdgeInsets.only(left: 10, top: 15),
-             child: articleSection(context, dic, dummyList),
+            Flexible(
+              fit: FlexFit.tight,
+              child: ListView(
+                padding: EdgeInsets.all(8),
+                children: <Widget> [
+                  Container(
+                    margin: EdgeInsets.only(left: 10, top: 15),
+                    child: articleSection(context, dic, dummyList),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10, top: 15),
+                    child: storeSection(context, dic, dummyListStore),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -140,7 +155,7 @@ Widget articleSection(BuildContext context, Map dic, List<Article> itemList) {
                 itemCount: itemList.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (BuildContext context, index) {
-                  return _buildEntries(context, index, itemList);
+                  return _buildArticleEntries(context, index, itemList);
                 },
               ),
             ),
@@ -160,7 +175,80 @@ Widget articleSection(BuildContext context, Map dic, List<Article> itemList) {
   );
 }
 
-Widget _buildEntries(BuildContext context, int index, List<Article> itemList) {
+Widget storeSection(BuildContext context, Map dic, List<Store> itemList) {
+  final double _height = MediaQuery.of(context).size.height;
+  return Column(
+    children: <Widget>[
+      // Title
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Container(
+            margin: EdgeInsets.only(top: 40),
+            child: BorderedTitle(
+              title: dic['article'],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 100, right: 20),
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                dic['show.all'],
+                style: Theme.of(context).textTheme.headline2.apply(fontSizeFactor: 0.7),
+                //TextStyle(
+                // color: Colors.indigo[255],
+                //),
+              ),
+            ),
+          ),
+        ],
+      ),
+      RoundedCard(
+        margin: EdgeInsets.only(top: 16),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 5, left:5, bottom: 5),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    dic['recently.added'],
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: _height/5,
+              child: ListView.builder(
+                padding: EdgeInsets.all(5),
+                shrinkWrap: true,
+                itemCount: itemList.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, index) {
+                  return _buildStoreEntries(context, index, itemList);
+                },
+              ),
+            ),
+            // Add Article button
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40, top: 10, bottom: 15),
+              child: RoundedButton(
+                text: dic['article.insert'] ,
+                onPressed: () {
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _buildArticleEntries(BuildContext context, int index, List<Article> itemList) {
   return GestureDetector(
     onTap: () {
       // Navigator.of(context).pushNamed(DETAIL_UI);
@@ -169,6 +257,23 @@ Widget _buildEntries(BuildContext context, int index, List<Article> itemList) {
       title: '${itemList[index].title}',
       category: 'dummy',
       price: "₹${itemList[index].price}",
+      dateAdded: "${itemList[index].dateAdded}",
+      description: "${itemList[index].desc}",
+      image: "${itemList[index].image}",
+      location: "${itemList[index].location}",
+    ),
+  );
+}
+
+Widget _buildStoreEntries(BuildContext context, int index, List<Store> itemList) {
+  return GestureDetector(
+    onTap: () {
+      // Navigator.of(context).pushNamed(DETAIL_UI);
+    },
+    child: ItemCard(
+      title: '${itemList[index].title}',
+      category: 'dummy',
+      price: " ",
       dateAdded: "${itemList[index].dateAdded}",
       description: "${itemList[index].desc}",
       image: "${itemList[index].image}",
@@ -213,6 +318,44 @@ List<Article> createDummyList() {
         "Coffee",
         "Hmm..! Fein",
         10,
+        0795419141,
+        "assets/images/public/logo_about.png",
+        "Zurich"),
+  ];
+}
+
+List<Store> createDummyListStore() {
+  List<Store> dummyItems;
+  return dummyItems = [
+    Store(
+        123,
+        "02 Apr 2019",
+        "Lenovo T450",
+        "Best Item ever",
+        0795419141,
+        "assets/images/public/logo_about.png",
+        "Zurich"),
+    Store(
+        124,
+        "02 Apr 2019",
+        "Bread",
+        "Best Item ever",
+        0795419141,
+        "assets/images/public/logo_about.png",
+        "Zurich"),
+    Store(
+        125,
+        "05 Mai 2019",
+        "Kohlrabi",
+        "Hmm..! Fein",
+        0795419141,
+        "assets/images/public/logo_about.png",
+        "Zurich"),
+    Store(
+        126,
+        "10 Mai 2019",
+        "Coffee",
+        "Hmm..! Fein",
         0795419141,
         "assets/images/public/logo_about.png",
         "Zurich"),
