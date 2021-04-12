4ae4a3b9214994ea236e973c5f1435525d0b3eda
Christian Langenbacher <christian.langenbacher91@gmail.com>
2021-04-12 07:38:13 +0200

    [app] remove commented code

--- lib/app.dart
+++ lib/app.dart
@@ -118,17 +118,6 @@ class _WalletAppState extends State<WalletApp> {
 
   @override
   void dispose() {
-    // showCupertinoDialog(
-    //   context: context,
-    //   builder: (BuildContext context) {
-    //     final Map<String, String> dic = I18n.of(context).assets;
-    //     return CupertinoAlertDialog(
-    //       title: Container(),
-    //       content: Text('${dic['copy']} ${dic['success']}'),
-    //     );
-    //   },
-    // );
-
     didReceiveLocalNotificationSubject.close();
     selectNotificationSubject.close();
     webApi.closeWebView();
