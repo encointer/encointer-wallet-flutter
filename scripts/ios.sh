cf2b0a110ec5c67acc01e7e7c6735d1020209d6b
Christian Langenabcher <christian.langenbacher91@gmail.com>
2021-04-14 11:56:07 +0200

    [github-action] add integration-test iphone and helper script to start emulator

---
+++ scripts/ios.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o pipefail
+
+echo "Available devices are:"
+xcrun instruments -s
+
+echo "Start device with ID ${DEVICE_ID}."
+DEVICE_MATCHING="device=${DEVICE_ID}"
+UDID=$(xcrun instruments -s | awk -F ' *[][]' -v "${DEVICE_MATCHING}" '$1 == device { print $2 }')
+xcrun simctl boot "${UDID:?No Simulator with this name found}"
