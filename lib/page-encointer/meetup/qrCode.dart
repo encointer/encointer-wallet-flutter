dd8d6d306e5cec4e962b0bc1ef6539245f4f4084
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-18 11:01:46 +0200

    [meetup] improve QrCode layout and add scan button (onTap is not yet implemented)

--- lib/page-encointer/meetup/qrCode.dart
+++ lib/page-encointer/meetup/qrCode.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:encointer_wallet/common/components/addressIcon.dart';
-import 'package:encointer_wallet/common/components/roundedButton.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/utils/i18n/index.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -40,7 +39,8 @@ class QrCode extends StatelessWidget {
                   child: Image.asset('assets/images/assets/receive_line_indigo.png'),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 40),
+                  margin: EdgeInsets.only(top: 40, left: 40, right: 40),
+                  padding: EdgeInsets.only(left: 10, right: 10),
                   decoration: BoxDecoration(
                     borderRadius: const BorderRadius.all(const Radius.circular(4)),
                     color: Colors.white,
@@ -58,29 +58,46 @@ class QrCode extends StatelessWidget {
                         title,
                         style: Theme.of(context).textTheme.headline4,
                       ),
+                      Container(width: 8, height: 8),
                       Container(
                         decoration: BoxDecoration(
                           border: Border.all(width: 4, color: themeColor),
                           borderRadius: BorderRadius.all(const Radius.circular(8)),
                         ),
-                        //margin: EdgeInsets.fromLTRB(64, 24, 48, 64),
                         child: QrImage(
                           data: qrCodeData,
-                          size: 350,
+                          size: 280,
                           //embeddedImage:
                           //    AssetImage('assets/images/public/app.png'),
                           //embeddedImageStyle:
                           //    QrEmbeddedImageStyle(size: Size(40, 40)),
                         ),
                       ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2,
-                        padding: EdgeInsets.only(top: 16, bottom: 32),
-                        child: RoundedButton(
-                          text: I18n.of(context).encointer['done'],
-                          onPressed: () => Navigator.pop(context),
+                      ButtonBar(children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(top: 16, bottom: 16),
+                          child: ElevatedButton(
+                            child: Text(I18n.of(context).encointer['done']),
+                            onPressed: () => Navigator.pop(context),
+                          ),
                         ),
-                      )
+                        Container(
+                          padding: EdgeInsets.only(top: 16, bottom: 16),
+                          child: TextButton(
+                            child: Row(
+                              children: [
+                                Text('Scan'),
+                                SizedBox(width: 4),
+                                Image.asset(
+                                  'assets/images/assets/qrcode_indigo.png',
+                                  width: 24,
+                                ),
+                              ],
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ),
+                      ])
                     ],
                   ),
                 )
