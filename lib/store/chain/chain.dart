58af8addfd7b645374748f05ff81c32d9f11dd59
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-20 10:30:12 +0200

    fix unit tests

--- lib/store/chain/chain.dart
+++ lib/store/chain/chain.dart
@@ -29,7 +29,9 @@ abstract class _ChainStore with Store {
   get latestHeaderNumber => latestHeader?.number;
 
   Future<void> loadCache() async {
-    latestHeader = await rootStore.loadObject(latestHeaderKey)
-      .then((header) => Header.fromJson(header));
+    Map h = await rootStore.loadObject(latestHeaderKey);
+    if (h != null) {
+        latestHeader = Header.fromJson(h);
+    }
   }
 }
