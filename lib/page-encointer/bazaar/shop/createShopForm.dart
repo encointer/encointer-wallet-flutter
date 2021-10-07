6ac1cbf64cbbeeb6f421bacb3b9b30ee1ba276db
clangenb <37865735+clangenb@users.noreply.github.com>
2021-10-07 11:36:07 +0200

    [dart] add some comments in createShopForm.dart

--- lib/page-encointer/bazaar/shop/createShopForm.dart
+++ lib/page-encointer/bazaar/shop/createShopForm.dart
@@ -1,12 +1,11 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:encointer_wallet/common/components/roundedButton.dart';
 import 'package:encointer_wallet/page-encointer/bazaar/common/imagePickerHandler.dart';
-import 'package:encointer_wallet/page-encointer/bazaar/shop/shopClass.dart';
-import 'package:encointer_wallet/page/account/txConfirmPage.dart';
+// import 'package:encointer_wallet/page/account/txConfirmPage.dart';
 import 'package:encointer_wallet/service/ipfsApi/httpApi.dart';
 import 'package:encointer_wallet/store/app.dart';
+import 'package:encointer_wallet/store/encointer/types/bazaar.dart';
 import 'package:encointer_wallet/utils/i18n/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -39,7 +38,7 @@ class _CreateShopForm extends State<CreateShopForm> {
   }
 
   // upload Json to IPFS
-  Future<String> _uploadShop(Shop shop) async {
+  Future<String> _uploadShop(IpfsBusiness shop) async {
     var jsonToUpload = shop.toJson();
     final String shopHash = await Ipfs().uploadJson(jsonToUpload);
     return shopHash;
@@ -49,34 +48,41 @@ class _CreateShopForm extends State<CreateShopForm> {
     if (_formKey.currentState.validate()) {
       // upload shop image to ipfs
       final String _imageHash = await _uploadImage();
-      // create shop with newly generated image hash
-      Shop newShop = new Shop(
-        name: _nameCtrl.text.trim(),
-        description: _descriptionCtrl.text.trim(),
-        imageHash: _imageHash,
-      );
-      final String _shopHash = await _uploadShop(newShop);
 
-      var args = {
-        "title": 'new_shop',
-        "txInfo": {
-          "module": 'encointerBazaar',
-          "call": 'newShop',
-        },
-        "detail": jsonEncode({
-          "cid": store.encointer.chosenCid,
-          "name": _nameCtrl.text.trim(),
-          "description": _descriptionCtrl.text.trim(),
-        }),
-        "params": [
-          store.encointer.chosenCid,
-          _shopHash,
-        ],
-        'onFinish': (BuildContext txPageContext, Map res) {
-          Navigator.popUntil(txPageContext, ModalRoute.withName('/'));
-        }
-      };
-      Navigator.of(context).pushNamed(TxConfirmPage.route, arguments: args);
+      // This part is commented out because the types used here do no longer exist. After the pallet-encointer-bazaar`
+      // revamp, the flow here should be implemented as follows:
+      //
+      // 1. Upload an `IpfsBusiness` to ipfs. As ipfs-cid is returned as a result
+      // 2. Register that business with the chain by sending a `register_business` extrinsic
+
+      // create shop with newly generated image hash
+      // Shop newShop = new Shop(
+      //   name: _nameCtrl.text.trim(),
+      //   description: _descriptionCtrl.text.trim(),
+      //   imageHash: _imageHash,
+      // );
+      // final String _shopHash = await _uploadShop(newShop);
+      //
+      // var args = {
+      //   "title": 'new_shop',
+      //   "txInfo": {
+      //     "module": 'encointerBazaar',
+      //     "call": 'newShop',
+      //   },
+      //   "detail": jsonEncode({
+      //     "cid": store.encointer.chosenCid,
+      //     "name": _nameCtrl.text.trim(),
+      //     "description": _descriptionCtrl.text.trim(),
+      //   }),
+      //   "params": [
+      //     store.encointer.chosenCid,
+      //     _shopHash,
+      //   ],
+      //   'onFinish': (BuildContext txPageContext, Map res) {
+      //     Navigator.popUntil(txPageContext, ModalRoute.withName('/'));
+      //   }
+      // };
+      // Navigator.of(context).pushNamed(TxConfirmPage.route, arguments: args);
     }
   }
 
