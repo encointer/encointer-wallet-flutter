24bc1378cfc887ea320bd40787344b344138dc58
clangenb <37865735+clangenb@users.noreply.github.com>
2021-12-22 14:48:28 +0100

    revert showing PasswordDialog at startup but ask for it if necessary.

--- lib/page/assets/index.dart
+++ lib/page/assets/index.dart
@@ -369,7 +369,8 @@ class _AssetsState extends State<Assets> {
             communityIds.retainWhere((i) => i != symbol);
           }
           final BalancesInfo balancesInfo = store.assets.balances[symbol];
-          if (ModalRoute.of(context).isCurrent && !_enteredPin & store.account.cachedPin.isEmpty) {
+          if (ModalRoute.of(context).isCurrent &&
+              !_enteredPin & store.account.cachedPin.isEmpty & !store.settings.endpointIsGesell) {
             // The pin is not immeditally propagated to the store, hence we track if the pin has been entered to prevent
             // showing the dialog multiple times.
             WidgetsBinding.instance.addPostFrameCallback(
