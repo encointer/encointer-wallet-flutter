a65a14334b122699d24fcf26f148fd01c11a56d6
Stierli Armin <armin.stierli@scs.ch>
2022-01-07 19:21:22 +0100

    Wrap in SafeArea

--- lib/page/assets/index.dart
+++ lib/page/assets/index.dart
@@ -53,173 +53,177 @@ class _AssetsState extends State<Assets> {
   @override
   Widget build(BuildContext context) {
     final Map dic = I18n.of(context).assets;
-    return ListView(
-      padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
-      children: [
-        Observer(builder: (_) {
-          String symbol = store.settings.networkState.tokenSymbol ?? '';
+    return SafeArea(
+      child: ListView(
+        padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+        children: [
+          Observer(builder: (_) {
+            String symbol = store.settings.networkState.tokenSymbol ?? '';
 
-          int decimals = store.settings.networkState.tokenDecimals ?? ert_decimals;
-          String networkName = store.settings.networkName ?? '';
-          final String tokenView = Fmt.tokenView(symbol);
+            int decimals = store.settings.networkState.tokenDecimals ?? ert_decimals;
+            String networkName = store.settings.networkName ?? '';
+            final String tokenView = Fmt.tokenView(symbol);
 
-          List<String> communityIds = [];
-          if (store.settings.endpointIsEncointer && networkName != null) {
-            if (store.settings.networkConst['communityIds'] != null) {
-              communityIds.addAll(List<String>.from(store.settings.networkConst['communityIds']));
+            List<String> communityIds = [];
+            if (store.settings.endpointIsEncointer && networkName != null) {
+              if (store.settings.networkConst['communityIds'] != null) {
+                communityIds.addAll(List<String>.from(store.settings.networkConst['communityIds']));
+              }
+              communityIds.retainWhere((i) => i != symbol);
             }
-            communityIds.retainWhere((i) => i != symbol);
-          }
-          final BalancesInfo balancesInfo = store.assets.balances[symbol];
-          if (ModalRoute.of(context).isCurrent &&
-              !_enteredPin & store.account.cachedPin.isEmpty & !store.settings.endpointIsGesell) {
-            // The pin is not immeditally propagated to the store, hence we track if the pin has been entered to prevent
-            // showing the dialog multiple times.
-            WidgetsBinding.instance.addPostFrameCallback(
-              (_) {
-                _showPasswordDialog(context);
-              },
-            );
-          }
-          var dic = I18n.of(context).assets;
-          String network = store.settings.loading ? dic['node.connecting'] : store.settings.networkName ?? dic['node.failed'];
+            final BalancesInfo balancesInfo = store.assets.balances[symbol];
+            if (ModalRoute.of(context).isCurrent &&
+                !_enteredPin & store.account.cachedPin.isEmpty & !store.settings.endpointIsGesell) {
+              // The pin is not immeditally propagated to the store, hence we track if the pin has been entered to prevent
+              // showing the dialog multiple times.
+              WidgetsBinding.instance.addPostFrameCallback(
+                (_) {
+                  _showPasswordDialog(context);
+                },
+              );
+            }
+            var dic = I18n.of(context).assets;
+            String network =
+                store.settings.loading ? dic['node.connecting'] : store.settings.networkName ?? dic['node.failed'];
 
-          AccountData acc = store.account.currentAccount;
+            AccountData acc = store.account.currentAccount;
 
-          final accInfo = store.account.accountIndexMap[acc.address];
-          final String accIndex = accInfo != null && accInfo['accountIndex'] != null ? '${accInfo['accountIndex']}\n' : '';
-          final double devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+            final accInfo = store.account.accountIndexMap[acc.address];
+            final String accIndex =
+                accInfo != null && accInfo['accountIndex'] != null ? '${accInfo['accountIndex']}\n' : '';
+            final double devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
 
-          return Column(
-            children: <Widget>[
-              RoundedCard(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      leading: AddressIcon('', pubKey: acc.pubKey),
-                      title: Text(Fmt.accountName(context, acc)),
-                      subtitle: Text(network),
-                      trailing: IconButton(
-                        icon: Icon(Icons.menu),
-                        onPressed: () => Navigator.of(context).pushNamed('/network'),
+            return Column(
+              children: <Widget>[
+                RoundedCard(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: AddressIcon('', pubKey: acc.pubKey),
+                        title: Text(Fmt.accountName(context, acc)),
+                        subtitle: Text(network),
+                        trailing: IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () => Navigator.of(context).pushNamed('/network'),
+                        ),
                       ),
-                    ),
-                    ListTile(
-                      title: Row(
-                        children: [
-                          GestureDetector(
-                            child: Padding(
-                              key: Key('qr-receive'),
-                              padding: EdgeInsets.only(left: 2),
-                              child: Image.asset(
-                                'assets/images/assets/qrcode_${store.settings.endpoint.color ?? 'pink'}.png',
-                                width: 24,
+                      ListTile(
+                        title: Row(
+                          children: [
+                            GestureDetector(
+                              child: Padding(
+                                key: Key('qr-receive'),
+                                padding: EdgeInsets.only(left: 2),
+                                child: Image.asset(
+                                  'assets/images/assets/qrcode_${store.settings.endpoint.color ?? 'pink'}.png',
+                                  width: 24,
+                                ),
                               ),
+                              onTap: () {
+                                if (acc.address != '') {
+                                  Navigator.pushNamed(context, ReceivePage.route);
+                                }
+                              },
                             ),
-                            onTap: () {
-                              if (acc.address != '') {
-                                Navigator.pushNamed(context, ReceivePage.route);
-                              }
-                            },
+                            Padding(
+                              padding: EdgeInsets.only(left: 8),
+                              child: Text(
+                                '$accIndex${Fmt.address(store.account.currentAddress)}',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            )
+                          ],
+                        ),
+                        trailing: IconButton(
+                          icon: Image.asset('assets/images/assets/qrcode_indigo.png'),
+                          onPressed: () {
+                            if (acc.address != '') {
+                              _handleScan();
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          BorderedTitle(
+                            title: dic['community.currency'],
                           ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 8),
-                            child: Text(
-                              '$accIndex${Fmt.address(store.account.currentAddress)}',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          )
                         ],
                       ),
-                      trailing: IconButton(
-                        icon: Image.asset('assets/images/assets/qrcode_indigo.png'),
-                        onPressed: () {
-                          if (acc.address != '') {
-                            _handleScan();
-                          }
-                        },
-                      ),
+                    ),
+                    CommunityChooserPanel(store),
+                    Observer(
+                      builder: (_) {
+                        return (store.encointer.communityName != null) & (store.encointer.chosenCid != null)
+                            ? RoundedCard(
+                                margin: EdgeInsets.only(top: 16),
+                                child: ListTile(
+                                  key: Key('cid-asset'),
+                                  leading: Container(
+                                    width: 36,
+                                    child: webApi.ipfs
+                                        .getCommunityIcon(store.encointer.communityIconsCid, devicePixelRatio),
+                                  ),
+                                  title: Text(store.encointer.communityName + " (${store.encointer.communitySymbol})"),
+                                  trailing: store.encointer.communityBalance != null
+                                      ? Text(
+                                          Fmt.doubleFormat(store.encointer.communityBalance),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
+                                        )
+                                      : CupertinoActivityIndicator(),
+                                  onTap: store.encointer.communityBalance != null
+                                      ? () {
+                                          Navigator.pushNamed(context, AssetPage.route,
+                                              arguments: AssetPageParams(
+                                                  token: store.encointer.chosenCid.toFmtString(),
+                                                  isEncointerCommunityCurrency: true,
+                                                  communityName: store.encointer.communityName,
+                                                  communitySymbol: store.encointer.communitySymbol));
+                                        }
+                                      : null,
+                                ),
+                              )
+                            : RoundedCard(
+                                margin: EdgeInsets.only(top: 16),
+                                padding: EdgeInsets.symmetric(vertical: 8),
+                                child: (store.encointer.chosenCid == null)
+                                    ? Container(
+                                        width: double.infinity,
+                                        child: Text(dic['community.not.selected'], textAlign: TextAlign.center))
+                                    : Container(
+                                        width: double.infinity,
+                                        child: CupertinoActivityIndicator(),
+                                      ),
+                              );
+                      },
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 32),
                     ),
                   ],
                 ),
-              ),
-
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        BorderedTitle(
-                          title: dic['community.currency'],
-                        ),
-                      ],
-                    ),
-                  ),
-                  CommunityChooserPanel(store),
-                  Observer(
-                    builder: (_) {
-                      return (store.encointer.communityName != null) & (store.encointer.chosenCid != null)
-                          ? RoundedCard(
-                        margin: EdgeInsets.only(top: 16),
-                        child: ListTile(
-                          key: Key('cid-asset'),
-                          leading: Container(
-                            width: 36,
-                            child: webApi.ipfs.getCommunityIcon(store.encointer.communityIconsCid, devicePixelRatio),
-                          ),
-                          title: Text(store.encointer.communityName + " (${store.encointer.communitySymbol})"),
-                          trailing: store.encointer.communityBalance != null
-                              ? Text(
-                            Fmt.doubleFormat(store.encointer.communityBalance),
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
-                          )
-                              : CupertinoActivityIndicator(),
-                          onTap: store.encointer.communityBalance != null
-                              ? () {
-                            Navigator.pushNamed(context, AssetPage.route,
-                                arguments: AssetPageParams(
-                                    token: store.encointer.chosenCid.toFmtString(),
-                                    isEncointerCommunityCurrency: true,
-                                    communityName: store.encointer.communityName,
-                                    communitySymbol: store.encointer.communitySymbol));
-                          }
-                              : null,
-                        ),
-                      )
-                          : RoundedCard(
-                        margin: EdgeInsets.only(top: 16),
-                        padding: EdgeInsets.symmetric(vertical: 8),
-                        child: (store.encointer.chosenCid == null)
-                            ? Container(
-                            width: double.infinity,
-                            child: Text(dic['community.not.selected'], textAlign: TextAlign.center))
-                            : Container(
-                          width: double.infinity,
-                          child: CupertinoActivityIndicator(),
-                        ),
-                      );
-                    },
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(bottom: 32),
-                  ),
-                ],
-              ),
-
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  Navigator.pushNamed(context, AssetPage.route,
-                      arguments: AssetPageParams(token: symbol, isEncointerCommunityCurrency: false));
-                },
-              ),
-            ],
-          );
-        }),
-      ],
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    Navigator.pushNamed(context, AssetPage.route,
+                        arguments: AssetPageParams(token: symbol, isEncointerCommunityCurrency: false));
+                  },
+                ),
+              ],
+            );
+          }),
+        ],
+      ),
     );
   }
 
@@ -308,8 +312,8 @@ class _AssetsState extends State<Assets> {
               content: sender['error'] != null
                   ? Text(sender['error'])
                   : sender['signer'] == null
-                  ? Text(dic['uos.qr.invalid'])
-                  : Text(dic['uos.acc.mismatch']),
+                      ? Text(dic['uos.qr.invalid'])
+                      : Text(dic['uos.acc.mismatch']),
               actions: <Widget>[
                 CupertinoButton(
                   child: Text(I18n.of(context).home['ok']),
@@ -327,7 +331,7 @@ class _AssetsState extends State<Assets> {
               context,
               store.account.currentAccount,
               Text(dic['uos.title']),
-                  (password) {
+              (password) {
                 print('pass ok: $password');
                 _signAsync(password);
               },
@@ -366,4 +370,3 @@ class _AssetsState extends State<Assets> {
     );
   }
 }
-
