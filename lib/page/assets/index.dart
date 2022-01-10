99cb7f04ea7ac7c825b3be49ef242027ee944295
Stierli Armin <armin.stierli@scs.ch>
2022-01-10 13:12:58 +0100

    Autoformat code

--- lib/page/assets/index.dart
+++ lib/page/assets/index.dart
@@ -154,46 +154,51 @@ class _AssetsState extends State<Assets> {
                   builder: (_) {
                     return (store.encointer.communityName != null) & (store.encointer.chosenCid != null)
                         ? RoundedCard(
-                      margin: EdgeInsets.only(top: 16),
-                      child: ListTile(
-                        key: Key('cid-asset'),
-                        leading: Container(
-                          width: 36,
-                          child: webApi.ipfs
-                              .getCommunityIcon(store.encointer.communityIconsCid, devicePixelRatio),
-                        ),
-                        title: Text(store.encointer.communityName + " (${store.encointer.communitySymbol})"),
-                        trailing: store.encointer.communityBalance != null
-                            ? Text(
-                          Fmt.doubleFormat(store.encointer.communityBalance),
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
-                        )
-                            : CupertinoActivityIndicator(),
-                        onTap: store.encointer.communityBalance != null
-                            ? () {
-                          Navigator.pushNamed(context, AssetPage.route,
-                              arguments: AssetPageParams(
-                                  token: store.encointer.chosenCid.toFmtString(),
-                                  isEncointerCommunityCurrency: true,
-                                  communityName: store.encointer.communityName,
-                                  communitySymbol: store.encointer.communitySymbol));
-                        }
-                            : null,
-                      ),
-                    )
+                            margin: EdgeInsets.only(top: 16),
+                            child: ListTile(
+                              key: Key('cid-asset'),
+                              leading: Container(
+                                width: 36,
+                                child:
+                                    webApi.ipfs.getCommunityIcon(store.encointer.communityIconsCid, devicePixelRatio),
+                              ),
+                              title: Text(
+                                store.encointer.communityName + " (${store.encointer.communitySymbol})",
+                              ),
+                              trailing: store.encointer.communityBalance != null
+                                  ? Text(
+                                      Fmt.doubleFormat(store.encointer.communityBalance),
+                                      style:
+                                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
+                                    )
+                                  : CupertinoActivityIndicator(),
+                              onTap: store.encointer.communityBalance != null
+                                  ? () {
+                                      Navigator.pushNamed(
+                                        context,
+                                        AssetPage.route,
+                                        arguments: AssetPageParams(
+                                            token: store.encointer.chosenCid.toFmtString(),
+                                            isEncointerCommunityCurrency: true,
+                                            communityName: store.encointer.communityName,
+                                            communitySymbol: store.encointer.communitySymbol),
+                                      );
+                                    }
+                                  : null,
+                            ),
+                          )
                         : Container(
-                      margin: EdgeInsets.only(top: 16),
-                      padding: EdgeInsets.symmetric(vertical: 8),
-                      child: (store.encointer.chosenCid == null)
-                          ? Container(
-                          width: double.infinity,
-                          child: Text(dic['community.not.selected'], textAlign: TextAlign.center))
-                          : Container(
-                        width: double.infinity,
-                        child: CupertinoActivityIndicator(),
-                      ),
-                    );
+                            margin: EdgeInsets.only(top: 16),
+                            padding: EdgeInsets.symmetric(vertical: 8),
+                            child: (store.encointer.chosenCid == null)
+                                ? Container(
+                                    width: double.infinity,
+                                    child: Text(dic['community.not.selected'], textAlign: TextAlign.center))
+                                : Container(
+                                    width: double.infinity,
+                                    child: CupertinoActivityIndicator(),
+                                  ),
+                          );
                   },
                 ),
               ],
