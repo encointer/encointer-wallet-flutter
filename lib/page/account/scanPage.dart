87cb2599f87ba059702a96e727a184a6ee96f199
anizeani <36300021+anizeani@users.noreply.github.com>
2022-02-01 12:27:00 -0300

    add text to dict

--- lib/page/account/scanPage.dart
+++ lib/page/account/scanPage.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_qr_scan/qrcode_reader_view.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:encointer_wallet/utils/i18n/index.dart';
 
 // TODO: scan image failed
 class ScanPage extends StatelessWidget {
@@ -61,7 +62,7 @@ class ScanPage extends StatelessWidget {
             print("the context is: ${ModalRoute.of(context).settings.name}");
             return QrcodeReaderView(
                 key: _qrViewKey,
-                helpWidget: Text("scan QR code"),
+                helpWidget: Text(I18n.of(context).account['qr.scan']),
                 headerWidget: SafeArea(
                   // check if we are in root, then don't offer the exit button to pop, this is the case when pressing scan in the navbar
                   child: ModalRoute.of(context).settings.name != '/'
