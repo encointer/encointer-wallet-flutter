e611666c8787f2974c3ffbc1fb19f2dea0e8e9a8
Stierli Armin <armin.stierli@scs.ch>
2022-02-07 17:25:21 +0100

    Remove no longer needed parts of the Receive Page

--- lib/page/assets/receive/receivePage.dart
+++ lib/page/assets/receive/receivePage.dart
@@ -1,4 +1,5 @@
 import 'package:encointer_wallet/common/components/roundedButton.dart';
+import 'package:encointer_wallet/common/theme.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/utils/UI.dart';
 import 'package:encointer_wallet/utils/i18n/index.dart';
@@ -26,7 +27,6 @@ class ReceivePage extends StatelessWidget {
     bool isEncointer = store.settings.endpointIsEncointer;
     final accInfo = store.account.accountIndexMap[store.account.currentAccount.address];
     return Scaffold(
-      backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: isShare ? Text(I18n.of(context).profile['share']) : Text(I18n.of(context).assets['receive']),
@@ -35,63 +35,23 @@ class ReceivePage extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: <Widget>[
-            Stack(
-              alignment: AlignmentDirectional.topCenter,
+            Column(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 32),
-                  child: Image.asset('assets/images/assets/receive_line_${isEncointer ? 'indigo' : 'pink'}.png'),
+                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                  child: Text(
+                    I18n.of(context).profile['qr.scan.hint'],
+                    style: Theme.of(context).textTheme.headline2.copyWith(color: encointerBlack),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 40),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(const Radius.circular(4)),
-                    color: Colors.white,
+                  child: QrImage(
+                    data: codeAddress,
+                    embeddedImage: AssetImage('assets/images/public/app.png'),
+                    embeddedImageStyle: QrEmbeddedImageStyle(size: Size(40, 40)),
                   ),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        I18n.of(context).profile['qr.scan.hint'],
-                        style: Theme.of(context).textTheme.headline3,
-                        textAlign: TextAlign.center,
-                      ),
-                      accInfo != null && accInfo['accountIndex'] != null
-                          ? Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Text(accInfo['accountIndex']),
-                            )
-                          : Container(width: 8, height: 8),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 4, color: themeColor),
-                          borderRadius: BorderRadius.all(const Radius.circular(8)),
-                        ),
-                        margin: EdgeInsets.fromLTRB(48, 16, 48, 24),
-                        child: QrImage(
-                          data: codeAddress,
-                          size: 200,
-                          embeddedImage: AssetImage('assets/images/public/app.png'),
-                          embeddedImageStyle: QrEmbeddedImageStyle(size: Size(40, 40)),
-                        ),
-                      ),
-                      Container(
-                        width: 160,
-                        child: Text(store.account.currentAddress),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2,
-                        padding: EdgeInsets.only(top: 16, bottom: 32),
-                        child: isShare
-                            ? RoundedButton(
-                                text: I18n.of(context).profile['share'], onPressed: () => Share.share(codeAddress))
-                            : RoundedButton(
-                                text: I18n.of(context).assets['copy'],
-                                onPressed: () => UI.copyAndNotify(context, store.account.currentAddress),
-                              ),
-                      )
-                    ],
-                  ),
-                )
+                ),
               ],
             )
           ],
@@ -100,3 +60,11 @@ class ReceivePage extends StatelessWidget {
     );
   }
 }
+
+// what was this for???:
+// accInfo != null && accInfo['accountIndex'] != null
+// ? Padding(
+// padding: EdgeInsets.all(8),
+// child: Text(accInfo['accountIndex']),
+// )
+// : Container(width: 8, height: 8),
