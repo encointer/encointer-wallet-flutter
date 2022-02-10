c141426268d5d676139c89808b7131971569406c
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-10 12:56:13 +0100

    [createAccountEntry] simplify layout code

--- lib/page/account/createAccountEntryPage.dart
+++ lib/page/account/createAccountEntryPage.dart
@@ -1,11 +1,10 @@
+import 'package:encointer_wallet/common/theme.dart';
 import 'package:encointer_wallet/page/account/create/createAccountPage.dart';
 import 'package:encointer_wallet/page/account/import/importAccountPage.dart';
 import 'package:encointer_wallet/utils/i18n/index.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:encointer_wallet/common/theme.dart';
 
 class CreateAccountEntryPage extends StatelessWidget {
   static final String route = '/account/entry';
@@ -32,61 +31,45 @@ class CreateAccountEntryPage extends StatelessWidget {
                 height: 210,
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-                  child: Container(
-                    child: Center(
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 16)),
-                          key: Key('create-account'),
-                          child: Text(I18n.of(context).home['create'], style: Theme.of(context).textTheme.headline3),
-                          onPressed: () {
-                            Navigator.pushNamed(context, CreateAccountPage.route);
-                          },
-                        ),
-                      ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 16)),
+                      key: Key('create-account'),
+                      child: Text(I18n.of(context).home['create'], style: Theme.of(context).textTheme.headline3),
+                      onPressed: () {
+                        Navigator.pushNamed(context, CreateAccountPage.route);
+                      },
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
+                  SizedBox(height: 16),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(width: 12),
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: '${I18n.of(context).profile['account.have']} ',
-                              style: TextStyle(
-                                color: encointerLightBlue,
-                              ),
-                            ),
-                            TextSpan(
-                              text: I18n.of(context).profile['import'],
-                              style: TextStyle(
-                                color: encointerLightBlue,
-                                decoration: TextDecoration.underline,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.pushNamed(context, ImportAccountPage.route);
-                                },
-                            ),
-                          ],
+                      Text(
+                        '${I18n.of(context).profile['account.have']} ',
+                        style: TextStyle(
+                          color: encointerLightBlue,
                         ),
                       ),
+                      GestureDetector(
+                          child: Text(
+                            I18n.of(context).profile['import'],
+                            style: TextStyle(
+                              color: encointerLightBlue,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          onTap: () => Navigator.pushNamed(context, ImportAccountPage.route)),
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
-              ],
+                ],
+              ),
             ),
           ],
         ),
