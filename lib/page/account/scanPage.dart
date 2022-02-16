293125293f43bad9a35bedf28444870d62df5e55
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-16 11:07:59 +0100

    [scanPage] show mock QR-code buttons only in dev mode.

--- lib/page/account/scanPage.dart
+++ lib/page/account/scanPage.dart
@@ -1,6 +1,7 @@
 import 'package:encointer_wallet/page/assets/transfer/transferPage.dart';
 import 'package:encointer_wallet/page/profile/contacts/contactPage.dart';
 import 'package:encointer_wallet/service/qrScanService.dart';
+import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/utils/translations/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,9 +10,13 @@ import 'package:permission_handler/permission_handler.dart';
 
 // TODO: scan image failed
 class ScanPage extends StatelessWidget {
+  ScanPage(this.store);
+
   static final String route = '/account/scan';
   final GlobalKey<QrcodeReaderViewState> _qrViewKey = GlobalKey();
   final QrScanService qrScanService = QrScanService();
+
+  final AppStore store;
 
   Future<bool> canOpenCamera() async {
     // will do nothing if already granted
@@ -51,25 +56,27 @@ class ScanPage extends StatelessWidget {
           builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
             if (snapshot.hasData && snapshot.data == true) {
               return QrcodeReaderView(
-                headerWidget: Row(children: [
-                  ElevatedButton(
-                    child: Text('addContact'),
-                    onPressed: () => onScan(
-                        "encointer-contact\nV1.0\nHgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX\nsqm1v79dF6b\n\nSara",
-                        null),
-                  ),
-                  ElevatedButton(
-                    child: Text('invoice'),
-                    onPressed: () => onScan(
-                        "encointer-invoice\nV1.0\nHgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX"
-                        "\nsqm1v79dF6b\n0.2343\nAubrey",
-                        null),
-                  ),
-                  Text(
-                    ' <<< Devs only',
-                    style: TextStyle(color: Colors.orange),
-                  ), // TODO only show for Devs
-                ]),
+                headerWidget: store.settings.developerMode
+                    ? Row(children: [
+                        ElevatedButton(
+                          child: Text('addContact'),
+                          onPressed: () => onScan(
+                              "encointer-contact\nV1.0\nHgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX\nsqm1v79dF6b\n\nSara",
+                              null),
+                        ),
+                        ElevatedButton(
+                          child: Text('invoice'),
+                          onPressed: () => onScan(
+                              "encointer-invoice\nV1.0\nHgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX"
+                              "\nsqm1v79dF6b\n0.2343\nAubrey",
+                              null),
+                        ),
+                        Text(
+                          ' <<< Devs only',
+                          style: TextStyle(color: Colors.orange),
+                        ), // TODO only show for Devs
+                      ])
+                    : Container(),
                 key: _qrViewKey,
                 helpWidget: Text(I18n.of(context).translationsForLocale().account.qrScan),
                 onScan: onScan,
