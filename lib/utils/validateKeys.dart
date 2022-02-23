405a15b61a223531ad0a9a29cc1235e51584a44a
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-23 18:41:44 +0100

    [utils] add validateKeys

---
+++ lib/utils/validateKeys.dart
@@ -0,0 +1,27 @@
+class ValidateKeys {
+  static bool isPrivateKey(String privateKey) {
+    privateKey = privateKey.trim();
+    return privateKey.substring(0, 2) == "0x";
+  }
+
+  static bool validatePrivateKey(String privateKey) {
+    privateKey = privateKey.trim();
+    return isPrivateKey(privateKey) && privateKey.length == 66; // 64-byte key plus '0x'
+  }
+
+  static bool isRawSeed(String seed) {
+    seed = seed.trim();
+    return seed.isNotEmpty && seed.substring(0, 2) == '//';
+  }
+
+  static bool validateRawSeed(String seed) {
+    seed = seed.trim();
+    return isRawSeed(seed) && (seed.length > 2) && (seed.length <= 32 || seed.length == 66);
+  }
+
+  static bool validateMnemonic(String mnemonic) {
+    String input = mnemonic.trim();
+    int len = input.split(' ').length;
+    return (len == 12 || len == 24);
+  }
+}
