681b44efe071cb360ded86eec16a2cd2ae225c41
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-24 15:29:43 +0100

    [addAccountForm] re-introduce import account button and clean up the widget

--- lib/page/account/create/addAccountForm.dart
+++ lib/page/account/create/addAccountForm.dart
@@ -1,11 +1,12 @@
 import 'package:encointer_wallet/common/components/gradientElements.dart';
 import 'package:encointer_wallet/common/theme.dart';
+import 'package:encointer_wallet/page/account/import/importAccountPage.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/utils/translations/index.dart';
+import 'package:encointer_wallet/utils/translations/translations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:encointer_wallet/utils/translations/translations.dart';
 
 class AddAccountForm extends StatelessWidget {
   AddAccountForm({this.submitting, this.onSubmit, this.store});
@@ -23,57 +24,74 @@ class AddAccountForm extends StatelessWidget {
 
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-              children: <Widget>[
-                SizedBox(height: 80),
-                Center(
-                  child: Text(I18n.of(context).translationsForLocale().profile.accountNameChoose,
-                      style: Theme.of(context).textTheme.headline2),
-                ),
-                SizedBox(height: 10),
-                Center(
-                  child: Container(
-                    width: 300,
-                    child: Text(
-                      I18n.of(context).translationsForLocale().profile.accountNameChooseHint,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.headline2.copyWith(
-                            color: Colors.black,
-                          ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  SizedBox(height: 80),
+                  Center(
+                    child: Text(I18n.of(context).translationsForLocale().profile.accountNameChoose,
+                        style: Theme.of(context).textTheme.headline2),
+                  ),
+                  SizedBox(height: 10),
+                  Center(
+                    child: Container(
+                      width: 300,
+                      child: Text(
+                        I18n.of(context).translationsForLocale().profile.accountNameChooseHint,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline2.copyWith(
+                              color: Colors.black,
+                            ),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30),
-                Column(
-                  children: <Widget>[
-                    TextFormField(
-                      key: Key('create-account-name'),
-                      decoration: InputDecoration(
-                        enabledBorder: const OutlineInputBorder(
-                          // width: 0.0 produces a thin "hairline" border
-                          borderSide: const BorderSide(color: Colors.transparent, width: 0.0),
-                          borderRadius: BorderRadius.horizontal(left: Radius.circular(15), right: Radius.circular(15)),
+                  SizedBox(height: 30),
+                  Column(
+                    children: <Widget>[
+                      TextFormField(
+                        key: Key('create-account-name'),
+                        decoration: InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            // width: 0.0 produces a thin "hairline" border
+                            borderSide: const BorderSide(color: Colors.transparent, width: 0.0),
+                            borderRadius:
+                                BorderRadius.horizontal(left: Radius.circular(15), right: Radius.circular(15)),
+                          ),
+                          filled: true,
+                          fillColor: ZurichLion.shade50,
+                          hintText: dic.account.createHint,
+                          labelText: I18n.of(context).translationsForLocale().profile.accountName,
                         ),
-                        filled: true,
-                        fillColor: ZurichLion.shade50,
-                        hintText: dic.account.createHint,
-                        labelText: I18n.of(context).translationsForLocale().profile.accountName,
+                        controller: _nameCtrl,
                       ),
-                      controller: _nameCtrl,
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            key: Key('create-account-confirm'),
-            padding: EdgeInsets.all(16),
-            child: PrimaryButton(
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 16)),
+              key: Key('import-account'),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Iconsax.import_2),
+                  SizedBox(width: 10),
+                  Text(I18n.of(context).translationsForLocale().home.accountImport,
+                      style: Theme.of(context).textTheme.headline3),
+                ],
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, ImportAccountPage.route);
+              },
+            ),
+            SizedBox(height: 10),
+            PrimaryButton(
+              key: Key('create-account-confirm'),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -100,8 +118,8 @@ class AddAccountForm extends StatelessWidget {
                 }
               },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
