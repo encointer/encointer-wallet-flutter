d0034cdbc3361c1f0f3595e52027b9d60ec3b20a
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-28 16:37:37 +0100

    [scripts/ios_init_env] remove brew upgrade

--- scripts/ios_init_env.sh
+++ scripts/ios_init_env.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew upgrade
-
 brew tap sconaway/brew https://github.com/SConaway/homebrew-brew/tree/patch-1
 brew install applesimutils
 
