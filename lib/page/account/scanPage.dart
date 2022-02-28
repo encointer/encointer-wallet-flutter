501dbb51585c37151786aa05452a81f1a9092e41
clangenb <37865735+clangenb@users.noreply.github.com>
2022-02-28 14:00:50 +0100

    [scanPage] Scanner fails gracefully

--- lib/page/account/scanPage.dart
+++ lib/page/account/scanPage.dart
@@ -8,14 +8,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qr_scan/qrcode_reader_view.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-// TODO: scan image failed
 class ScanPage extends StatelessWidget {
   ScanPage(this.store);
 
   static final String route = '/account/scan';
   final GlobalKey<QrcodeReaderViewState> _qrViewKey = GlobalKey();
-  final QrScanService qrScanService = QrScanService();
 
+  final QrScanService qrScanService = QrScanService();
   final AppStore store;
 
   Future<bool> canOpenCamera() async {
@@ -23,30 +22,52 @@ class ScanPage extends StatelessWidget {
     return Permission.camera.request().isGranted;
   }
 
+  void _showSnackBar(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: Colors.white,
+      content: Text(msg, style: TextStyle(color: Colors.black54)),
+      duration: Duration(milliseconds: 1500),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     Future onScan(String data, String rawData) {
-      QrScanData qrScanData = qrScanService.parse(data);
-      switch (qrScanData.context) {
-        case QrScanContext.contact:
-          // show add contact and auto-fill data
-          Navigator.of(context).popAndPushNamed(
-            ContactPage.route,
-            arguments: qrScanData,
-          );
-          break;
-        case QrScanContext.invoice:
-          // go to transfer page and auto-fill data
-          Navigator.of(context).popAndPushNamed(
-            TransferPage.route,
-            arguments: TransferPageParams(
-                cid: qrScanData.cid, recipient: qrScanData.account, amount: qrScanData.amount, redirect: '/'),
-          );
-          break;
-        default:
-          throw UnimplementedError(
-              'Scan functionality for the case [${qrScanData.context}] has not yet been implemented!');
+      try {
+        QrScanData qrScanData = qrScanService.parse(data);
+        switch (qrScanData.context) {
+          case QrScanContext.contact:
+            // show add contact and auto-fill data
+            Navigator.of(context).popAndPushNamed(
+              ContactPage.route,
+              arguments: qrScanData,
+            );
+            break;
+          case QrScanContext.invoice:
+            // go to transfer page and auto-fill data
+            Navigator.of(context).popAndPushNamed(
+              TransferPage.route,
+              arguments: TransferPageParams(
+                  cid: qrScanData.cid, recipient: qrScanData.account, amount: qrScanData.amount, redirect: '/'),
+            );
+            break;
+          default:
+            throw UnimplementedError(
+                'Scan functionality for the case [${qrScanData.context}] has not yet been implemented!');
+        }
+      } catch (e) {
+        print("[ScanPage]: ${e.toString()}");
+        _showSnackBar(context, e.toString());
+
+        // If we don't wait, scans  of the same qr code are spammed.
+        // My fairly recent cellphone gets too much load for duration < 500 ms. We might need to increase
+        // this for older phones.
+        Future.delayed(const Duration(milliseconds: 1500), () {
+          _qrViewKey.currentState.startScan();
+        });
       }
+
       return null;
     }
 
