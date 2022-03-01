31187e0406bfc50f4fb556e8673fe2fa252c349c
clangenb <37865735+clangenb@users.noreply.github.com>
2022-03-01 08:46:30 +0100

    scripts/ios_init_env: remove obsolete wix/brew fork as upstream has been fixed

--- scripts/ios_init_env.sh
+++ scripts/ios_init_env.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew tap wix/brew https://github.com/encointer/homebrew-brew
+brew tap wix/brew
 brew install applesimutils
 
 # show available simulator runtimes
