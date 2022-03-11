5e0302b89638ab081a2e09b7dbc307705d56f49e
clangenb <37865735+clangenb@users.noreply.github.com>
2022-03-11 14:53:56 +0100

    remove wrong comment

--- lib/utils/enum.dart
+++ lib/utils/enum.dart
@@ -1,17 +1,8 @@
-
-// this becomes possible if dart min SDK > 2.6
-// extension FromStringExtension on CeremonyPhase {
-//
-//   CeremonyPhase fromString(String value) {
-//     return ceremonyPhaseFromString(value);
-//   }
-// }
-
 /// Parses a String into an enum
 /// Recognizes patterns like: CeremonyPhase.Registering, CeremonyPhase.REGISTERING, Registering, REGISTERING,
 T getEnumFromString<T>(Iterable<T> values, String value) {
   return values.firstWhere(
-        (type) => type.toString().split(".").last == value.toString().split(".").last.toUpperCase(),
+    (type) => type.toString().split(".").last == value.toString().split(".").last.toUpperCase(),
     orElse: () => null,
   );
 }
