fedca48242679019d6a8b75972101abc6f7dd686
clangenb <37865735+clangenb@users.noreply.github.com>
2022-03-29 15:25:48 +0200

    [profile] remove unused import

--- lib/page/profile/index.dart
+++ lib/page/profile/index.dart
@@ -1,5 +1,4 @@
 import 'package:encointer_wallet/common/components/addressIcon.dart';
-import 'package:encointer_wallet/common/components/passwordInputDialog.dart';
 import 'package:encointer_wallet/common/theme.dart';
 import 'package:encointer_wallet/page/account/create/addAccountPage.dart';
 import 'package:encointer_wallet/page/profile/aboutPage.dart';
