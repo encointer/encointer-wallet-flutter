515595e0337fac3348d3dbc374bcb04cef1913f2
clangenb <37865735+clangenb@users.noreply.github.com>
2022-04-12 16:51:46 -0400

    PoC: substrateDartApi

---
+++ lib/models/index.dart
@@ -0,0 +1,6 @@
+/// This should in the future contain all type definitions see:
+/// https://github.com/encointer/encointer-wallet-flutter/issues/522
+///
+library models;
+
+export 'commons/commons.dart';
---
+++ test/service/substrate_api/encointer/dartApi_test.dart
@@ -0,0 +1,16 @@
+import 'package:encointer_wallet/service/substrate_api/core/dartApi.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('can connect', () {
+    test('rpc methods contains getAggregatedAccountData', () async {
+      var encointerApi = SubstrateDartApi();
+
+      await encointerApi.connect('ws://localhost:9944');
+
+      expect(encointerApi.rpcMethods.contains("ceremonies_getAggregatedAccountData"), true);
+
+      await encointerApi.close();
+    });
+  });
+}
