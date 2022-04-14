de4ba0e3a604f5e8dfee76a3c576c99ba866b799
clangenb <37865735+clangenb@users.noreply.github.com>
2022-04-14 11:26:53 -0400

    [communityAccountStore] jsonSerialization of whole store works

--- lib/store/encointer/sub_stores/communityAccountStore.dart
+++ lib/store/encointer/sub_stores/communityAccountStore.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 import 'package:mobx/mobx.dart';
 
@@ -10,7 +12,12 @@ part 'communityAccountStore.g.dart';
 ///
 @JsonSerializable(explicitToJson: true)
 class CommunityAccountStore extends _CommunityAccountStore with _$CommunityAccountStore {
-  CommunityAccountStore(): super();
+  CommunityAccountStore() : super();
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
 
   factory CommunityAccountStore.fromJson(Map<String, dynamic> json) => _$CommunityAccountStoreFromJson(json);
   Map<String, dynamic> toJson() => _$CommunityAccountStoreToJson(this);
@@ -20,4 +27,9 @@ abstract class _CommunityAccountStore with Store {
   /// Contains the meetup data if the account has been assigned to a meetup in this community.
   @observable
   Meetup meetup;
+
+  @action
+  void setMeetup(Meetup meetup) {
+    this.meetup = meetup;
+  }
 }
---
+++ test/store/encointer/sub_stores/communityAccountStore_test.dart
@@ -0,0 +1,46 @@
+import 'package:encointer_wallet/mocks/testUtils.dart';
+import 'package:encointer_wallet/models/ceremonies/ceremonies.dart';
+import 'package:encointer_wallet/store/encointer/sub_stores/communityAccountStore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CommunityAccountStore', () {
+    test('json serialization works', () {
+      var communityAccountStore = CommunityAccountStore();
+      communityAccountStore
+          .setMeetup(Meetup(ParticipantType.Bootstrapper, 2, 3, 10, [ALICE_ADDRESS, BOB_ADDRESS, CHARLIE_ADDRESS]));
+
+      const Map<String, dynamic> targetJson = {
+        "meetup": {
+          "participantType": 'Bootstrapper',
+          "meetupIndex": 2,
+          "meetupLocationIndex": 3,
+          "meetupTime": 10,
+          "meetupRegistry": [ALICE_ADDRESS, BOB_ADDRESS, CHARLIE_ADDRESS]
+        }
+      };
+
+      expect(communityAccountStore.toJson(), targetJson);
+    });
+
+    test('json deserialization works', () {
+      const Map<String, dynamic> sourceJson = {
+        "meetup": {
+          "participantType": 'Bootstrapper',
+          "meetupIndex": 2,
+          "meetupLocationIndex": 3,
+          "meetupTime": 10,
+          "meetupRegistry": [ALICE_ADDRESS, BOB_ADDRESS, CHARLIE_ADDRESS]
+        }
+      };
+
+      var store = CommunityAccountStore.fromJson(sourceJson);
+
+      expect(store.meetup.participantType, ParticipantType.Bootstrapper);
+      expect(store.meetup.meetupIndex, 2);
+      expect(store.meetup.meetupLocationIndex, 3);
+      expect(store.meetup.meetupTime, 10);
+      expect(store.meetup.meetupRegistry, [ALICE_ADDRESS, BOB_ADDRESS, CHARLIE_ADDRESS]);
+    });
+  });
+}
