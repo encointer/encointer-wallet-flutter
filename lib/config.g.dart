23df7006cbd5a75261f4411f1455c6adbdbac0f7
clangenb <37865735+clangenb@users.noreply.github.com>
2022-04-19 10:14:18 -0400

    [AppConfig] regenerated types

--- lib/config.g.dart
+++ lib/config.g.dart
@@ -11,6 +11,7 @@ Config _$ConfigFromJson(Map<String, dynamic> json) {
     initialRoute: json['initialRoute'] as String,
     mockLocalStorage: json['mockLocalStorage'] as bool,
     mockSubstrateApi: json['mockSubstrateApi'] as bool,
+    appStoreConfig: _$enumDecodeNullable(_$StoreConfigEnumMap, json['appStoreConfig']),
   );
 }
 
@@ -18,4 +19,40 @@ Map<String, dynamic> _$ConfigToJson(Config instance) => <String, dynamic>{
       'initialRoute': instance.initialRoute,
       'mockLocalStorage': instance.mockLocalStorage,
       'mockSubstrateApi': instance.mockSubstrateApi,
+      'appStoreConfig': _$StoreConfigEnumMap[instance.appStoreConfig],
     };
+
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError('A value must be provided. Supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+
+  final value = enumValues.entries.singleWhere((e) => e.value == source, orElse: () => null)?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
+}
+
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$StoreConfigEnumMap = {
+  StoreConfig.Normal: 'Normal',
+  StoreConfig.Test: 'Test',
+};
