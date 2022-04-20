d01575693e89472054f9529cfec4b89eeea8daa8
clangenb <37865735+clangenb@users.noreply.github.com>
2022-04-20 15:11:28 -0400

    Bump encointer-js (#536)
    * update encointer-js to 0.8.5 to unify rpc naming
    
    * update rpc-prefix to be encointer
    
    * [CI] bump encointer-node artifact
    
    Co-authored-by: Alain Brenzikofer <news@brenzi.ch>

--- lib/service/substrate_api/encointer/encointerApi.dart
+++ lib/service/substrate_api/encointer/encointerApi.dart
@@ -28,7 +28,7 @@ import 'teeProxyApi.dart';
 ///
 /// NOTE: If the js-code was changed a rebuild of the application is needed to update the code.
 
-const aggregatedAccountDataRpc = "ceremonies_getAggregatedAccountData";
+const aggregatedAccountDataRpc = "encointer_getAggregatedAccountData";
 
 class EncointerApi {
   EncointerApi(this.jsApi, SubstrateDartApi dartApi)
@@ -109,7 +109,7 @@ class EncointerApi {
     store.encointer.phaseDurations = phaseDurations;
   }
 
-  /// Queries the rpc 'ceremonies_getAggregatedAccountData' with the dart api.
+  /// Queries the rpc 'encointer_getAggregatedAccountData' with the dart api.
   ///
   Future<AggregatedAccountData> getAggregatedAccountData(CommunityIdentifier cid, String account) async {
     try {
