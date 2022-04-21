a173df80e8724bcac07edd2c4f80ef5920dfac74
clangenb <37865735+clangenb@users.noreply.github.com>
2022-04-21 10:58:15 -0400

    fix rebase errors

--- lib/page-encointer/common/assignmentPanel.dart
+++ lib/page-encointer/common/assignmentPanel.dart
@@ -31,8 +31,6 @@ class AssignmentPanel extends StatelessWidget {
             Observer(builder: (_) {
               Meetup meetup = store.encointer.communityAccount.meetup;
 
-              _log("communityAccount: ${store.encointer.communityAccount.toJson()}");
-
               Location meetupLocation = meetup == null
                   ? store.encointer.community.meetupLocations?.first
                   : store.encointer.community.meetupLocations[meetup.locationIndex];
@@ -142,8 +140,4 @@ class MaybeMeetupTime extends StatelessWidget {
 
     return meetupTime != null ? Text(date, style: this.style) : CupertinoActivityIndicator();
   }
-}
-
-_log(String msg) {
-  print("[AssignmentPanel] $msg");
 }
