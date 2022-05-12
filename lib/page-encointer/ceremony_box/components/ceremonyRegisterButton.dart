c9381976bb787e03f82acfe45a6bffed555a9796
clangenb <37865735+clangenb@users.noreply.github.com>
2022-05-12 10:22:47 -0400

    [CeremonyBox] fix padding of register participant button

--- lib/page-encointer/ceremony_box/components/ceremonyRegisterButton.dart
+++ lib/page-encointer/ceremony_box/components/ceremonyRegisterButton.dart
@@ -1,8 +1,8 @@
+import 'package:encointer_wallet/common/components/gradientElements.dart';
+import 'package:encointer_wallet/utils/translations/index.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:intl/intl.dart';
-import 'package:encointer_wallet/common/components/gradientElements.dart';
-import 'package:encointer_wallet/utils/translations/index.dart';
 
 class CeremonyRegisterButton extends StatelessWidget {
   const CeremonyRegisterButton({
@@ -19,23 +19,18 @@ class CeremonyRegisterButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var dic = I18n.of(context).translationsForLocale();
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 24.0),
-      child: PrimaryButton(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Iconsax.login_1),
-            SizedBox(
-              width: 6,
-            ),
-            Text(
-              '${dic.encointer.registerUntil} ${DateFormat.yMd(languageCode).format(registerUntilDate)}',
-            ),
-          ],
-        ),
-        onPressed: onPressed,
+    return PrimaryButton(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Iconsax.login_1),
+          SizedBox(width: 6),
+          Text(
+            '${dic.encointer.registerUntil} ${DateFormat.yMd(languageCode).format(registerUntilDate)}',
+          ),
+        ],
       ),
+      onPressed: onPressed,
     );
   }
 }
