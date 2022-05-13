11398540f4850712f32393b9851c988921f53ece
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2022-05-13 10:59:11 +0200

    fmt

--- lib/common/components/addressIcon.dart
+++ lib/common/components/addressIcon.dart
@@ -1,7 +1,6 @@
 import 'package:encointer_wallet/utils/UI.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jdenticon_dart/jdenticon_dart.dart';
 
@@ -31,22 +30,22 @@ class AddressIcon extends StatelessWidget {
         backColor: '#d4edf8ff',
         hues: [199]);
     return GestureDetector(
-          child: Container(
-            width: size ?? 96,
-            height: size ?? 96,
-            child: ClipOval(
-              child: SizedBox.fromSize(
-                size: Size.fromRadius(48), // Image radius
-                child: SvgPicture.string(
-                  rawSvg,
-                  fit: BoxFit.fill,
-                  height: size ?? 96,
-                  width: size ?? 96,
-                ),
-              ),
+      child: Container(
+        width: size ?? 96,
+        height: size ?? 96,
+        child: ClipOval(
+          child: SizedBox.fromSize(
+            size: Size.fromRadius(48), // Image radius
+            child: SvgPicture.string(
+              rawSvg,
+              fit: BoxFit.fill,
+              height: size ?? 96,
+              width: size ?? 96,
             ),
           ),
-          onTap: tapToCopy ? () => UI.copyAndNotify(context, addressToCopy ?? address) : null,
-        );
+        ),
+      ),
+      onTap: tapToCopy ? () => UI.copyAndNotify(context, addressToCopy ?? address) : null,
+    );
   }
 }
