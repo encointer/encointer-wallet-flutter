0c4c2e7ca12eef04342fae09e1288847630885e4
clangenb <37865735+clangenb@users.noreply.github.com>
2022-05-22 18:48:03 -0400

    [tx] extract submitTx wrapper

--- lib/page-encointer/phases/registering/registerParticipantPanel.dart
+++ lib/page-encointer/phases/registering/registerParticipantPanel.dart
@@ -1,4 +1,3 @@
-import 'package:encointer_wallet/common/components/passwordInputDialog.dart';
 import 'package:encointer_wallet/common/components/roundedButton.dart';
 import 'package:encointer_wallet/service/substrate_api/api.dart';
 import 'package:encointer_wallet/store/app.dart';
@@ -31,40 +30,23 @@ class _RegisterParticipantPanel extends State<RegisterParticipantPanel> {
   }
 
   Future<void> _submit() async {
-    if (store.settings.cachedPin.isEmpty) {
-      await showCupertinoDialog(
-        context: context,
-        builder: (context) {
-          return showPasswordInputDialog(
-              context,
-              store.account.currentAccount,
-              Text(I18n.of(context)
-                  .translationsForLocale()
-                  .home
-                  .unlockAccount
-                  .replaceAll('CURRENT_ACCOUNT_NAME', store.account.currentAccount.name.toString())), (password) {
-            store.settings.setPin(password);
-          });
+    submitTx(
+      context, store, webApi, (txPaymentAsset) {
+      submitRegisterParticipant(
+        context,
+        webApi,
+        store.encointer.chosenCid,
+        txPaymentAsset: txPaymentAsset,
+        proof: webApi.encointer.getProofOfAttendance(),
+        onFinish: (BuildContext txPageContext, Map res) {
+          webApi.encointer.getAggregatedAccountData(store.encointer.chosenCid, store.account.currentAddress);
+          Navigator.popUntil(
+            txPageContext,
+            ModalRoute.withName('/'),
+          );
         },
       );
-    }
-
-    final txPaymentAsset = store.encointer.getTxPaymentAsset(store.encointer.chosenCid);
-
-    submitRegisterParticipant(
-      context,
-      webApi,
-      store.encointer.chosenCid,
-      txPaymentAsset: txPaymentAsset,
-      proof: webApi.encointer.getProofOfAttendance(),
-      onFinish: (BuildContext txPageContext, Map res) {
-        webApi.encointer.getAggregatedAccountData(store.encointer.chosenCid, store.account.currentAddress);
-        Navigator.popUntil(
-          txPageContext,
-          ModalRoute.withName('/'),
-        );
-      },
-    );
+    });
   }
 
   @override
--- lib/utils/tx.dart
+++ lib/utils/tx.dart
@@ -1,10 +1,42 @@
 import 'dart:convert';
 
+import 'package:encointer_wallet/common/components/passwordInputDialog.dart';
 import 'package:encointer_wallet/page/account/txConfirmPage.dart';
 import 'package:encointer_wallet/service/substrate_api/api.dart';
+import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/store/encointer/types/communities.dart';
 import 'package:encointer_wallet/store/encointer/types/proofOfAttendance.dart';
+import 'package:encointer_wallet/utils/translations/index.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+/// Helpers to send transactions.
+///
+/// Refactor when the issue 'remove tx confirm page' is tackled.
+
+Future<void> submitTx(BuildContext context, AppStore store, Api api, Function(CommunityIdentifier) submitFn) async {
+  if (store.settings.cachedPin.isEmpty) {
+    await showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return showPasswordInputDialog(
+            context,
+            store.account.currentAccount,
+            Text(I18n.of(context)
+                .translationsForLocale()
+                .home
+                .unlockAccount
+                .replaceAll('CURRENT_ACCOUNT_NAME', store.account.currentAccount.name.toString())), (password) {
+          store.settings.setPin(password);
+        });
+      },
+    );
+  }
+
+  final txPaymentAsset = store.encointer.getTxPaymentAsset(store.encointer.chosenCid);
+
+  return submitFn(txPaymentAsset);
+}
 
 Future<void> submitClaimRewards(
   BuildContext context,
