e3ab593f44f5c7c74cc94c7f9580ed46c15f7c49
clangenb <37865735+clangenb@users.noreply.github.com>
2022-06-13 08:43:44 +0100

    [tests] extract contact tests

---
+++ test/page/qr-scanner/qr_codes/contact_test.dart
@@ -0,0 +1,76 @@
+import 'package:encointer_wallet/page/qr_scan/qrCodeBase.dart';
+import 'package:encointer_wallet/page/qr_scan/qr_codes/index.dart';
+import 'package:encointer_wallet/store/encointer/types/communities.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Contact QrCode', () {
+    test('toQrPayload V2 works', () async {
+      final qrCode = ContactQrCode(
+        account: "account",
+        label: "label",
+        cid: CommunityIdentifier.fromFmtString("sqm1v79dF6b"),
+        network: "nctr-k",
+        version: QrCodeVersion.v2_0,
+      );
+
+      expect(
+        qrCode.toQrPayload(),
+        "encointer-contact\n"
+        "v2.0\n"
+        "account\n"
+        "sqm1v79dF6b\n"
+        "nctr-k\n"
+        "label",
+      );
+    });
+
+    test('fromQrPayload V2 works', () async {
+      final payload = "encointer-contact\n"
+          "v2.0\n"
+          "account\n"
+          "sqm1v79dF6b\n"
+          "nctr-k\n"
+          "label";
+
+      final qrCode = ContactQrCode.fromPayload(payload);
+
+      expect(qrCode.context, QrCodeContext.contact);
+      expect(qrCode.version, QrCodeVersion.v2_0);
+      expect(qrCode.data.account, "account");
+      expect(qrCode.data.cid.toFmtString(), "sqm1v79dF6b");
+      expect(qrCode.data.network, "nctr-k");
+      expect(qrCode.data.label, "label");
+    });
+
+    test('toQrPayload V1 works', () async {
+      final qrCode = ContactQrCode(
+        account: "account",
+        label: "label",
+        version: QrCodeVersion.v1_0,
+      );
+
+      expect(
+        qrCode.toQrPayload(),
+        "encointer-contact\n"
+        "v1.0\n"
+        "account\n"
+        "label",
+      );
+    });
+
+    test('fromQrPayload V1 works', () async {
+      final payload = "encointer-contact\n"
+          "v1.0\n"
+          "account\n"
+          "label";
+
+      final qrCode = ContactQrCode.fromPayload(payload);
+
+      expect(qrCode.context, QrCodeContext.contact);
+      expect(qrCode.version, QrCodeVersion.v1_0);
+      expect(qrCode.data.account, "account");
+      expect(qrCode.data.label, "label");
+    });
+  });
+}
