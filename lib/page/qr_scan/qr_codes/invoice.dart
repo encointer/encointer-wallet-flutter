77a4713768b96f77f19e35a1a6cb313aba04683b
clangenb <37865735+clangenb@users.noreply.github.com>
2022-06-13 08:59:33 +0100

    [qr_codes] make invoice qr code backwards compatible and extract tests

--- lib/page/qr_scan/qr_codes/invoice.dart
+++ lib/page/qr_scan/qr_codes/invoice.dart
@@ -4,26 +4,51 @@ import 'package:flutter/material.dart';
 import '../qrCodeBase.dart';
 
 class InvoiceQrCode extends QrCode<InvoiceData> {
-  InvoiceQrCode.withData(InvoiceData data) : super(data);
+  InvoiceQrCode.withData(
+    InvoiceData data, {
+    this.version = QrCodeVersion.v1_0,
+  }) : super(data);
 
   InvoiceQrCode({
     @required String account,
     CommunityIdentifier cid,
+    String network,
     num amount,
     @required String label,
-  }) : super(InvoiceData(account: account, cid: cid, amount: amount, label: label));
+    this.version = QrCodeVersion.v1_0,
+  }) : super(InvoiceData(account: account, cid: cid, network: network, amount: amount, label: label));
 
   var context = QrCodeContext.invoice;
 
-  var version = QrCodeVersion.v2_0;
+  var version;
+
+  @override
+  String toQrPayload() {
+    final qrFields = [context.toQrField(), version.toVersionNumber()];
+    if (version == QrCodeVersion.v1_0) {
+      qrFields.addAll(this.data.toQrFields());
+    } else {
+      qrFields.addAll(this.data.toQrFieldsV2());
+    }
+    return qrFields.join(QR_CODE_FIELD_SEPARATOR);
+  }
 
   static InvoiceQrCode fromPayload(String payload) {
     return fromQrFields(payload.split("\n"));
   }
 
   static InvoiceQrCode fromQrFields(List<String> fields) {
-    // todo verify context and version
-    return InvoiceQrCode.withData(InvoiceData.fromQrFields(fields.sublist(2)));
+    if (QrCodeVersionExt.fromQrField(fields[1]) == QrCodeVersion.v1_0) {
+      return InvoiceQrCode.withData(
+        InvoiceData.fromQrFieldsV1(fields.sublist(2)),
+        version: QrCodeVersion.v1_0,
+      );
+    } else {
+      return InvoiceQrCode.withData(
+        InvoiceData.fromQrFieldsV2(fields.sublist(2)),
+        version: QrCodeVersion.v2_0,
+      );
+    }
   }
 }
 
@@ -31,6 +56,7 @@ class InvoiceData implements ToQrFields {
   InvoiceData({
     @required this.account,
     this.cid,
+    this.network,
     this.amount,
     @required this.label,
   });
@@ -40,6 +66,9 @@ class InvoiceData implements ToQrFields {
 
   /// Community identifier.
   final CommunityIdentifier cid;
+
+  /// Network, e.g: nctr-k, nctr-r.
+  final String network;
 
   /// Optional payment amount for the invoice.
   num amount;
@@ -56,12 +85,32 @@ class InvoiceData implements ToQrFields {
     ];
   }
 
-  static InvoiceData fromQrFields(List<String> fields) {
+  List<String> toQrFieldsV2() {
+    return [
+      account,
+      cid?.toFmtString() ?? "",
+      network,
+      amount?.toString() ?? "",
+      label,
+    ];
+  }
+
+  static InvoiceData fromQrFieldsV1(List<String> fields) {
     return InvoiceData(
       account: fields[0],
       cid: fields[1].isNotEmpty ? CommunityIdentifier.fromFmtString(fields[1]) : null,
       amount: fields[2].trim().isNotEmpty ? double.parse(fields[2]) : null,
       label: fields[3],
+    );
+  }
+
+  static InvoiceData fromQrFieldsV2(List<String> fields) {
+    return InvoiceData(
+      account: fields[0],
+      cid: fields[1].isNotEmpty ? CommunityIdentifier.fromFmtString(fields[1]) : null,
+      network: fields[2],
+      amount: fields[3].trim().isNotEmpty ? double.parse(fields[3]) : null,
+      label: fields[4],
     );
   }
 }
