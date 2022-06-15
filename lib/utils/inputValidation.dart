ccc72a89840b0f3834aafbb01f72fc333be20391
clangenb <37865735+clangenb@users.noreply.github.com>
2022-06-15 13:59:35 +0100

    fmt

--- lib/utils/inputValidation.dart
+++ lib/utils/inputValidation.dart
@@ -3,18 +3,17 @@ import 'package:encointer_wallet/utils/translations/index.dart';
 import 'package:flutter/material.dart';
 
 class InputValidation {
-
   static String validateAccountName(BuildContext context, String input, List<AccountData> existingAccounts) {
     final dic = I18n.of(context).translationsForLocale();
 
-      String name = input.trim();
-      if (name.length == 0) {
-        return dic.profile.contactNameError;
-      }
-      int exist = existingAccounts.indexWhere((i) => i.name == name);
-      if (exist > -1) {
-        return dic.profile.contactNameAlreadyExists;
-      }
-      return null;
+    String name = input.trim();
+    if (name.length == 0) {
+      return dic.profile.contactNameError;
     }
+    int exist = existingAccounts.indexWhere((i) => i.name == name);
+    if (exist > -1) {
+      return dic.profile.contactNameAlreadyExists;
+    }
+    return null;
+  }
 }
