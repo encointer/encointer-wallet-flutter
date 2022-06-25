ca07688feab1ab0c20390f0f61d04266c7367c08
clangenb <37865735+clangenb@users.noreply.github.com>
2022-06-25 15:21:12 +0200

    [models] add meetupOverrides data model

---
+++ lib/models/encointer_feed/meetupOverrides.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+// Run: `flutter pub run build_runner build` in order to create/update the *.g.dart
+part 'meetupOverrides.g.dart';
+
+@JsonSerializable(explicitToJson: true, fieldRename: FieldRename.kebab)
+class MeetupOverrides {
+  MeetupOverrides(
+    this.overrideName,
+    this.network,
+    this.communities,
+    this.meetupTimes,
+  );
+
+  String overrideName;
+  String network;
+  List<String> communities;
+  List<String> meetupTimes;
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+
+  factory MeetupOverrides.fromJson(Map<String, dynamic> json) => _$MeetupOverridesFromJson(json);
+  Map<String, dynamic> toJson() => _$MeetupOverridesToJson(this);
+}
---
+++ lib/models/encointer_feed/meetupOverrides.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meetupOverrides.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MeetupOverrides _$MeetupOverridesFromJson(Map<String, dynamic> json) {
+  return MeetupOverrides(
+    json['override-name'] as String,
+    json['network'] as String,
+    (json['communities'] as List)?.map((e) => e as String)?.toList(),
+    (json['meetup-times'] as List)?.map((e) => e as String)?.toList(),
+  );
+}
+
+Map<String, dynamic> _$MeetupOverridesToJson(MeetupOverrides instance) => <String, dynamic>{
+      'override-name': instance.overrideName,
+      'network': instance.network,
+      'communities': instance.communities,
+      'meetup-times': instance.meetupTimes,
+    };
