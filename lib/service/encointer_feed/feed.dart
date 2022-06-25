3be06c15a8520c21026227f166b4c4e64de87c71
clangenb <37865735+clangenb@users.noreply.github.com>
2022-06-25 17:01:45 +0200

    [service/encointer_feed] correctly return a list

--- lib/service/encointer_feed/feed.dart
+++ lib/service/encointer_feed/feed.dart
@@ -4,11 +4,12 @@ import 'package:encointer_wallet/config/consts.dart';
 import 'package:encointer_wallet/models/encointer_feed/meetupOverrides.dart';
 import 'package:http/http.dart' as http;
 
-Future<MeetupOverrides> getMeetupOverrides() async {
+Future<List<MeetupOverrides>> getMeetupOverrides() async {
   final response = await http.get(Uri.parse(encointer_feed_overrides));
 
   if (response.statusCode == 200) {
-    return MeetupOverrides.fromJson(jsonDecode(response.body));
+    List<dynamic> list = jsonDecode(response.body);
+    return list.map((e) => MeetupOverrides.fromJson(e)).toList();
   } else {
     throw Exception('Failed to get meetup overrides.');
   }
