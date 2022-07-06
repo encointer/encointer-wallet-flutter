4eb76dcc736664f0398b1fd73f8793c35d13d870
clangenb <37865735+clangenb@users.noreply.github.com>
2022-07-05 21:00:18 -0300

    [service/tx] extract params.dart, remove "details" field of extrinsics params because it is no longer used, and causes unnecessary imports.

--- lib/service/tx/tx.dart
+++ lib/service/tx/tx.dart
@@ -4,12 +4,12 @@ import 'package:encointer_wallet/common/components/passwordInputDialog.dart';
 import 'package:encointer_wallet/service/substrate_api/api.dart';
 import 'package:encointer_wallet/service/tx/txConfirmLogic.dart';
 import 'package:encointer_wallet/store/app.dart';
-import 'package:encointer_wallet/store/encointer/types/claimOfAttendance.dart';
 import 'package:encointer_wallet/store/encointer/types/communities.dart';
-import 'package:encointer_wallet/store/encointer/types/proofOfAttendance.dart';
 import 'package:encointer_wallet/utils/translations/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'params.dart';
 
 /// Helpers to send transactions.
 ///
@@ -67,7 +67,6 @@ Future<void> submitClaimRewards(
       "call": 'claimRewards',
       "cid": chosenCid,
     },
-    "detail": "cid: ${chosenCid.toFmtString()}",
     "params": [chosenCid],
   };
 
@@ -78,85 +77,6 @@ Future<void> submitClaimRewards(
     txParams,
     onFinish: (BuildContext txPageContext, Map res) => (res),
   );
-}
-
-Map<String, dynamic> endorseNewcomerParams(
-  CommunityIdentifier chosenCid,
-  String newbie,
-) {
-  return {
-    "title": 'endorse_newcomer',
-    "txInfo": {
-      "module": 'encointerCeremonies',
-      "call": 'endorseNewcomer',
-      "cid": chosenCid,
-    },
-    "detail": "cid: ${chosenCid.toFmtString()}, newbie: $newbie",
-    "params": [chosenCid, newbie],
-  };
-}
-
-Map<String, dynamic> registerParticipantParams(
-  CommunityIdentifier chosenCid, {
-  ProofOfAttendance proof,
-}) {
-  return {
-    "title": 'register_participant',
-    "txInfo": {
-      "module": 'encointerCeremonies',
-      "call": 'registerParticipant',
-      "cid": chosenCid,
-    },
-    "detail": jsonEncode({
-      "cid": chosenCid.toFmtString(),
-      "proof": proof == null
-          ? "No proof of past attendance found" // Note: hardcoded strings are ok here, the page  will be removed.
-          : "Sending proof for cIndex: ${proof.ceremonyIndex}, community: ${proof.communityIdentifier.toFmtString()}",
-    }),
-    "params": [
-      chosenCid,
-      proof,
-    ],
-  };
-}
-
-Map<String, dynamic> attestClaimsParams(
-  BuildContext context,
-  CommunityIdentifier chosenCid,
-  int scannedClaimsCount,
-  List<ClaimOfAttendance> claims,
-) {
-  final dic = I18n.of(context).translationsForLocale();
-  return {
-    "title": 'attest_claims',
-    "txInfo": {
-      "module": 'encointerCeremonies',
-      "call": 'attestClaims',
-      "cid": chosenCid,
-    },
-    "detail": dic.encointer.claimsSubmitDetail.replaceAll('AMOUNT', scannedClaimsCount.toString()),
-    "params": [claims],
-  };
-}
-
-Map<String, dynamic> encointerBalanceTransferParams(
-  CommunityIdentifier cid,
-  String recipientAddress,
-  double amount,
-) {
-  return {
-    "title": 'encointerBalancesTransfer',
-    "txInfo": {
-      "module": 'encointerBalances',
-      "call": 'transfer',
-      "cid": cid,
-    },
-    "params": [
-      recipientAddress,
-      cid,
-      amount.toString(),
-    ],
-  };
 }
 
 Future<void> submitEndorseNewcomer(
@@ -211,7 +131,6 @@ Future<void> submitRegisterParticipant(BuildContext context, AppStore store, Api
 
 Future<void> submitAttestClaims(BuildContext context, AppStore store, Api api) async {
   final params = attestClaimsParams(
-    context,
     store.encointer.chosenCid,
     store.encointer.communityAccount.scannedClaimsCount,
     store.encointer.communityAccount.participantsClaims.values.toList(),
