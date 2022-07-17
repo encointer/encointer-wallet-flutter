dd6455d680f665f9cb76e6e6f274f7a7b62c9790
clangenb <37865735+clangenb@users.noreply.github.com>
2022-07-17 18:56:31 -0300

    [jsApi] strip unnecessary stuff from html container

--- lib/service/substrate_api/core/jsApi.dart
+++ lib/service/substrate_api/core/jsApi.dart
@@ -148,12 +148,7 @@ String jSSourceHtmlContainer(String jSSource) {
   return """
   <!DOCTYPE html>
   <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    </head>
     <body>
-      <h1>JavaScript Handlers (Channels) TEST</h1>
       <script>
         $jSSource
       </script>
