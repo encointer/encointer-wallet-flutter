2ad05c0ff7e588de8823c648abf90fc40041b9e0
clangenb <37865735+clangenb@users.noreply.github.com>
2022-07-25 22:04:41 +0200

    better handling of nullability

--- lib/common/components/maybeDateTime.dart
+++ lib/common/components/maybeDateTime.dart
@@ -5,11 +5,15 @@ import 'package:intl/intl.dart';
 ///
 /// Useful because we have many cases where the time is first 'null' because it is still being fetched.
 class MaybeDateTime extends StatelessWidget {
-  MaybeDateTime(this.meetupTime, {this.dateFormat, this.style});
+  MaybeDateTime(
+    this.meetupTime, {
+    required this.dateFormat,
+    this.style,
+  });
 
   final int? meetupTime;
 
-  final DateFormat? dateFormat;
+  final DateFormat dateFormat;
   final TextStyle? style;
 
   @override
@@ -17,7 +21,7 @@ class MaybeDateTime extends StatelessWidget {
     late String date;
 
     if (meetupTime != null) {
-      date = dateFormat!.format(new DateTime.fromMillisecondsSinceEpoch(meetupTime!));
+      date = dateFormat.format(new DateTime.fromMillisecondsSinceEpoch(meetupTime!));
     }
 
     return meetupTime != null ? Text(date, style: this.style) : CupertinoActivityIndicator();
