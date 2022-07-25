d9f3a3e8b20e5ded2a467ed42f44024c364ab326
clangenb <37865735+clangenb@users.noreply.github.com>
2022-07-25 22:14:54 +0200

    better nullability-handling

--- lib/common/components/willPopScopeWrapper.dart
+++ lib/common/components/willPopScopeWrapper.dart
@@ -4,29 +4,30 @@ import 'package:encointer_wallet/utils/translations/index.dart';
 import 'package:flutter/cupertino.dart';
 
 class WillPopScopeWrapper extends StatelessWidget {
-  WillPopScopeWrapper({this.child});
+  WillPopScopeWrapper({required this.child});
 
-  final Widget? child;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
+    final dic = I18n.of(context)!.translationsForLocale();
+
     return new WillPopScope(
-      child: child!,
+      child: child,
       onWillPop: () {
         return Platform.isAndroid
             ? showCupertinoDialog(
                 context: context,
                 builder: (context) => CupertinoAlertDialog(
-                  title: Text(I18n.of(context)!.translationsForLocale().home.exitConfirm),
+                  title: Text(dic.home.exitConfirm),
                   actions: <Widget>[
                     CupertinoButton(
                       onPressed: () => Navigator.of(context).pop(false),
-                      child: Text(I18n.of(context)!.translationsForLocale().home.cancel),
+                      child: Text(dic.home.cancel),
                     ),
                     CupertinoButton(
                       onPressed: () => Navigator.of(context).pop(true),
-                      /*Navigator.of(context).pop(true)*/
-                      child: Text(I18n.of(context)!.translationsForLocale().home.ok),
+                      child: Text(dic.home.ok),
                     ),
                   ],
                 ),
