1d95878dd06d033d76f8486026a9e73044449ea5
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2022-09-06 22:03:47 +0600

    Remove default recipient in send process (#738)
    * fixed default recipient
    
    * [transfer_page] go back to old behaviour: fallback to chosen community if there is no supplied cid.
    
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

--- lib/page/assets/index.dart
+++ lib/page/assets/index.dart
@@ -262,14 +262,7 @@ class _AssetsState extends State<Assets> {
                                   key: const Key('transfer'),
                                   onPressed: store.encointer.communityBalance != null
                                       ? () {
-                                          Navigator.pushNamed(
-                                            context,
-                                            TransferPage.route,
-                                            arguments: TransferPageParams(
-                                                redirect: '/',
-                                                cid: store.encointer.chosenCid!,
-                                                communitySymbol: store.encointer.community!.symbol!),
-                                          );
+                                          Navigator.pushNamed(context, TransferPage.route);
                                         }
                                       : null,
                                 ),
