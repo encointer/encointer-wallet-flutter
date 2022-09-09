03b4e66093cc90f3d16e7a2fb760288c70de532e
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-09-09 13:44:40 +0600

    fixed to future builder

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
+import 'package:encointer_wallet/mocks/substrate_api/core/mock_dart_api.dart';
+import 'package:encointer_wallet/mocks/substrate_api/mock_api.dart';
+import 'package:encointer_wallet/mocks/substrate_api/mock_js_api.dart';
 import 'package:encointer_wallet/page-encointer/home_page.dart';
 import 'package:encointer_wallet/page/account/create_account_entry_page.dart';
+import 'package:encointer_wallet/service/log/log_service.dart';
+import 'package:encointer_wallet/service/substrate_api/api.dart';
+import 'package:encointer_wallet/service/substrate_api/core/dart_api.dart';
+import 'package:encointer_wallet/service/substrate_api/core/js_api.dart';
 import 'package:encointer_wallet/store/app.dart';
 
 class SplashView extends StatefulWidget {
@@ -20,27 +27,28 @@ class _SplashViewState extends State<SplashView> {
   final String mosaicBackground = 'assets/nctr_mosaic_background.svg';
   final String nctrLogo = 'assets/nctr_logo.svg';
 
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await _initPage();
-    });
-  }
-
   Future<void> _initPage() async {
-    await context.read<AppStore>().init(Localizations.localeOf(context).toString());
-    context.read<AppStore>().setApiReady(true);
+    final _store = context.watch<AppStore>();
+    await _store.init(Localizations.localeOf(context).toString());
+    final js = await DefaultAssetBundle.of(context).loadString('lib/js_service_encointer/dist/main.js');
+    webApi = (_store.config == StoreConfig.Test
+        ? MockApi(_store, MockJSApi(), MockSubstrateDartApi(), js, withUi: true)
+        : Api.create(_store, JSApi(), SubstrateDartApi(), js))
+      ..init().timeout(
+        const Duration(seconds: 20),
+        onTimeout: () => Log.d('webApi.init() has run into a timeout. We might be offline.'),
+      );
+    _store.setApiReady(true);
 
-    await context.read<AppStore>().dataUpdate.setupUpdateReaction(() async {
-      if (mounted) await context.read<AppStore>().encointer.updateState();
+    await _store.dataUpdate.setupUpdateReaction(() async {
+      if (mounted) await _store.encointer.updateState();
     });
 
-    if (context.read<AppStore>().account.accountListAll.length > 0) {
-      Navigator.pushAndRemoveUntil(
+    if (_store.account.accountListAll.length > 0) {
+      await Navigator.pushAndRemoveUntil(
           context, CupertinoPageRoute(builder: (context) => EncointerHomePage()), (route) => false);
     } else {
-      Navigator.pushAndRemoveUntil(
+      await Navigator.pushAndRemoveUntil(
           context, CupertinoPageRoute(builder: (context) => CreateAccountEntryPage()), (route) => false);
     }
   }
@@ -48,22 +56,27 @@ class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            mosaicBackground,
-            fit: BoxFit.fill,
-            width: MediaQuery.of(context).size.width,
-          ),
-          Center(
-            child: SvgPicture.asset(
-              nctrLogo,
-              color: Colors.white,
-              width: 210,
-              height: 210,
-            ),
-          ),
-        ],
+      body: FutureBuilder(
+        future: _initPage(),
+        builder: (context, s) {
+          return Stack(
+            children: [
+              SvgPicture.asset(
+                mosaicBackground,
+                fit: BoxFit.fill,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Center(
+                child: SvgPicture.asset(
+                  nctrLogo,
+                  color: Colors.white,
+                  width: 210,
+                  height: 210,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
