07f161364e1ef4c8005347fbcec74188afcbac73
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-09-19 23:25:48 +0600

    write unit test for notificationForLoop function

--- test/service/background_service/meetup/callback_dispatcher_test.dart
+++ test/service/background_service/meetup/callback_dispatcher_test.dart
@@ -19,18 +19,68 @@ class MockFunctions extends Mock implements Functions {}
 
 void main() async {
   final feeds = feedFromJson(fixture('feed_list'));
-  final list = <String>['msg-1', 'msg-2'];
   final lastList = <String>['msg-1', 'msg-2', 'msg-3'];
 
   final functions = MockFunctions();
 
-  test('notificationForLoop', () async {
+  test('notificationForLoop show notification 3, cache list=[]', () async {
+    final list = <String>[];
+    when(() => functions.showNotification(0, feeds[0].title, feeds[0].content)).thenAnswer((i) async => true);
+    when(() => functions.showNotification(1, feeds[1].title, feeds[1].content)).thenAnswer((i) async => true);
     when(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).thenAnswer((i) async => true);
+
     when(() => functions.cache(lastList)).thenAnswer((i) async => true);
 
     await notificationForLoop(feeds, list, functions.showNotification, functions.cache);
 
+    verify(() => functions.showNotification(0, feeds[0].title, feeds[0].content)).called(1);
+    verify(() => functions.showNotification(1, feeds[1].title, feeds[1].content)).called(1);
     verify(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).called(1);
+
     verify(() => functions.cache(lastList)).called(1);
+  });
+
+  test('notificationForLoop show notification 2, cache list=["msg-1"]', () async {
+    final list = <String>['msg-1'];
+    when(() => functions.showNotification(1, feeds[1].title, feeds[1].content)).thenAnswer((i) async => true);
+    when(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).thenAnswer((i) async => true);
+
+    when(() => functions.cache(lastList)).thenAnswer((i) async => true);
+
+    await notificationForLoop(feeds, list, functions.showNotification, functions.cache);
+
+    verifyNever(() => functions.showNotification(0, feeds[0].title, feeds[0].content));
+    verify(() => functions.showNotification(1, feeds[1].title, feeds[1].content)).called(1);
+    verify(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).called(1);
+
+    verify(() => functions.cache(lastList)).called(1);
+  });
+
+  test('notificationForLoop show notification 1, cache list=["msg-1", "msg-2"]', () async {
+    final list = <String>['msg-1', 'msg-2'];
+
+    when(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).thenAnswer((i) async => true);
+
+    when(() => functions.cache(lastList)).thenAnswer((i) async => true);
+
+    await notificationForLoop(feeds, list, functions.showNotification, functions.cache);
+
+    verifyNever(() => functions.showNotification(0, feeds[0].title, feeds[0].content));
+    verifyNever(() => functions.showNotification(1, feeds[1].title, feeds[1].content));
+    verify(() => functions.showNotification(2, feeds[2].title, feeds[2].content)).called(1);
+
+    verify(() => functions.cache(lastList)).called(1);
+  });
+
+  test('notificationForLoop show notification 0, cache list=["msg-1", "msg-2", "msg-3"]', () async {
+    final list = <String>['msg-1', 'msg-2', 'msg-3'];
+
+    await notificationForLoop(feeds, list, functions.showNotification, functions.cache);
+
+    verifyNever(() => functions.showNotification(0, feeds[0].title, feeds[0].content));
+    verifyNever(() => functions.showNotification(1, feeds[1].title, feeds[1].content));
+    verifyNever(() => functions.showNotification(2, feeds[2].title, feeds[2].content));
+
+    verifyNever(() => functions.cache(lastList));
   });
 }
