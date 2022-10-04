edb7ee428a88281e5869c0dd3666c53dcf724ccb
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-10-04 22:59:47 +0600

    change scheme encointer.app

--- lib/config/consts.dart
+++ lib/config/consts.dart
@@ -120,7 +120,7 @@ const locale_place_holder = 'LOCALE_PLACEHOLDER';
 const ceremony_info_link_base = 'https://leu.zuerich/$locale_place_holder/#zeremonien';
 const leu_zurich_link = 'https://leu.zuerich/$locale_place_holder';
 const meetup_notification_link = 'https://encointer.github.io/feed/community_messages/$locale_place_holder/cm.json';
-const encointerLink = 'https://encointer.org/';
+const encointerLink = 'com.encointer://encointer/';
 
 String setDeepLink([String? linkText])=> '$encointerLink${linkText?.replaceAll('\n', '_')}';
 
