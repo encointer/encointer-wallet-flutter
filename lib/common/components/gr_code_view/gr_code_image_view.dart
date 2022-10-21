6390cf48e8375dddc987fe5ee63fdfee8c18aaf3
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2022-10-21 21:19:48 +0600

    QR code gets cropped (#827)
    * fixed: QR code gets cropped on some screens, breaking scanning
    
    * [GA] update ios runtimes for ipad simulator creation (#832)
    
    * added test space
    
    * added test deleted space
    
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

--- lib/common/components/gr_code_view/gr_code_image_view.dart
+++ lib/common/components/gr_code_view/gr_code_image_view.dart
@@ -1,11 +1,12 @@
-import 'package:encointer_wallet/common/theme.dart';
-import 'package:encointer_wallet/utils/extensions/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:qr_flutter_fork/qr_flutter_fork.dart';
 
-class QrCodeImage extends StatelessWidget {
-  const QrCodeImage({
+import 'package:encointer_wallet/common/theme.dart';
+import 'package:encointer_wallet/utils/extensions/extensions.dart';
+
+class QrCodeImageWithButton extends StatelessWidget {
+  const QrCodeImageWithButton({
     Key? key,
     required this.onTap,
     required this.qrCode,
@@ -20,23 +21,7 @@ class QrCodeImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          width: context.isMobile ? 600 : 1200,
-          // MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
-          child: PhotoView.customChild(
-            maxScale: context.isMobile ? 1.0001 : 1.1,
-            minScale: context.isMobile ? 0.2 : 0.4,
-            initialScale: context.isMobile ? 0.8 : 0.5,
-            backgroundDecoration: BoxDecoration(
-              color: ZurichLion.shade50,
-            ),
-            child: QrImage(
-              backgroundColor: Theme.of(context).canvasColor,
-              data: qrCode,
-            ),
-          ),
-        ),
+        QrCodeImage(qrCode: qrCode),
         const SizedBox(height: 10),
         TextButton.icon(
           onPressed: onTap,
@@ -48,6 +33,38 @@ class QrCodeImage extends StatelessWidget {
         ),
         const SizedBox(height: 20),
       ],
+    );
+  }
+}
+
+class QrCodeImage extends StatelessWidget {
+  const QrCodeImage({
+    Key? key,
+    required this.qrCode,
+    this.errorCorrectionLevel = QrErrorCorrectLevel.L,
+  }) : super(key: key);
+
+  final String qrCode;
+  final int errorCorrectionLevel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: context.isMobile ? MediaQuery.of(context).size.width : 1200,
+      height: MediaQuery.of(context).size.width,
+      child: PhotoView.customChild(
+        maxScale: context.isMobile ? 1.0001 : 1.1,
+        minScale: context.isMobile ? 0.2 : 0.4,
+        initialScale: context.isMobile ? 0.8 : 0.5,
+        backgroundDecoration: BoxDecoration(
+          color: ZurichLion.shade50,
+        ),
+        child: QrImage(
+          backgroundColor: Theme.of(context).canvasColor,
+          data: qrCode,
+          errorCorrectionLevel: errorCorrectionLevel,
+        ),
+      ),
     );
   }
 }
