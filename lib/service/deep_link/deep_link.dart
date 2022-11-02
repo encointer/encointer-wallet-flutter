7d9e6940b3550762fdf4cd7b2537d50b373a977b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-11-02 07:33:35 +0600

    successfully worked universal link

--- lib/service/deep_link/deep_link.dart
+++ lib/service/deep_link/deep_link.dart
@@ -22,29 +22,21 @@ Future<void> initialDeepLinks(BuildContext context) async {
 }
 
 Future<void> _init(BuildContext context, String? link) async {
-  Log.d('==============> initialDeepLink started', 'initialDeepLinks');
-
   if (link != null) {
     try {
       final qrScanService = QrScanService();
       final qrCode = qrScanService.parse(link.replaceAll(encointerLink, '').replaceAll('_', '\n'));
-      _navigationWithWrScanContext(context, qrCode);
-      Log.d('==============> $qrCode', 'initialDeepLinks');
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(qrCode.data.toString())));
+      await _navigationWithWrScanContext(context, qrCode);
     } catch (e, s) {
       Log.e('Deeplink format $e', 'initialDeepLinks', s);
     }
-  } else {
-    Log.d('==============> $link', 'initialDeepLinks');
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('url null keldi')));
   }
 }
 
-void _navigationWithWrScanContext(BuildContext context, QrCode<dynamic> qrCode) {
+Future<void> _navigationWithWrScanContext(BuildContext context, QrCode<dynamic> qrCode) async {
   switch (qrCode.context) {
     case QrCodeContext.invoice:
-      Log.d('==============> qrCode.context QrCodeContext.invoice', 'initialDeepLinks');
-      Navigator.of(context).pushNamed(
+      await Navigator.of(context).pushNamed(
         TransferPage.route,
         arguments: TransferPageParams(
           recipient: qrCode.data.account,
@@ -54,16 +46,14 @@ void _navigationWithWrScanContext(BuildContext context, QrCode<dynamic> qrCode) 
       );
       break;
     case QrCodeContext.contact:
-      Navigator.of(context).pushNamed(
+      await Navigator.of(context).pushNamed(
         ContactPage.route,
         arguments: qrCode.data,
       );
-      Log.d('==============> qrCode.context QrCodeContext.contact', 'initialDeepLinks');
       break;
     case QrCodeContext.voucher:
-      Log.d('==============> qrCode.context QrCodeContext.voucher', 'initialDeepLinks');
       break;
     default:
-      throw const FormatException('[handleQrScan] Unhandled qr scan context');
+      break;
   }
 }
