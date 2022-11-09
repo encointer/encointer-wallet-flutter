fa9e8362b773e8cdb4f5607e16782f4a9ef8357b
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2022-11-09 15:32:06 +0600

    Integration very good analysis (#862)
    * integration very_good_analysis
    
    * fixed getter return type warning. Ignored other all errors, warnings
    
    * fixed format analysis_option file
    
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

--- lib/utils/translations/translations_home.dart
+++ lib/utils/translations/translations_home.dart
@@ -51,290 +51,290 @@ abstract class TranslationsHome {
 
 class TranslationsEnHome implements TranslationsHome {
   @override
-  get cancel => 'Cancel';
+  String get cancel => 'Cancel';
   @override
-  get scan => 'Scan';
+  String get scan => 'Scan';
   @override
-  get contacts => 'Contacts';
+  String get contacts => 'Contacts';
   @override
-  get closeApp => 'Close app';
+  String get closeApp => 'Close app';
   @override
-  get copy => 'Copy to clipboard';
+  String get copy => 'Copy to clipboard';
   @override
-  get create => 'Create Account';
+  String get create => 'Create Account';
   @override
-  get detail => 'Detail';
+  String get detail => 'Detail';
   @override
-  get exitConfirm => 'Do you want to exit the App?';
+  String get exitConfirm => 'Do you want to exit the App?';
   @override
-  get errorOccurred => 'An error occurred:';
-  get inputInvalid => 'Invalid input';
+  String get errorOccurred => 'An error occurred:';
+  String get inputInvalid => 'Invalid input';
   @override
-  get loading => 'Loading...';
+  String get loading => 'Loading...';
   @override
-  get next => 'Next Step';
+  String get next => 'Next Step';
   @override
-  get notifySubmitted => 'transaction Submitted';
+  String get notifySubmitted => 'transaction Submitted';
   @override
-  get notifySubmittedQueued => 'Queued transaction Submitted';
+  String get notifySubmittedQueued => 'Queued transaction Submitted';
   @override
-  get ok => 'OK';
+  String get ok => 'OK';
   @override
-  get pinNeeded => 'PIN is needed to use the app';
+  String get pinNeeded => 'PIN is needed to use the app';
   @override
-  get settingNetwork => 'Select Wallet';
+  String get settingNetwork => 'Select Wallet';
   @override
-  get submit => 'Sign and Submit';
+  String get submit => 'Sign and Submit';
   @override
-  get submitCall => 'Calling ';
+  String get submitCall => 'Calling ';
   @override
-  get submitFees => 'Fees';
+  String get submitFees => 'Fees';
   @override
-  get submitFeesOffline => 'Fees unavailable (offline)';
+  String get submitFeesOffline => 'Fees unavailable (offline)';
   @override
-  get submitFrom => 'You are about to sign a transaction from ';
+  String get submitFrom => 'You are about to sign a transaction from ';
   @override
-  get submitNoSign => 'Submit (no sign)';
+  String get submitNoSign => 'Submit (no sign)';
   @override
-  get submitQr => 'Sign via QR';
+  String get submitQr => 'Sign via QR';
   @override
-  get submitTransaction => 'Submit Transaction';
+  String get submitTransaction => 'Submit Transaction';
   @override
-  get success => 'Success';
+  String get success => 'Success';
   @override
-  get switchAccount => 'Switch Account';
+  String get switchAccount => 'Switch Account';
   @override
-  get switchCommunity => 'Switch Community';
+  String get switchCommunity => 'Switch Community';
   @override
-  get transactionQueued => 'Queued';
+  String get transactionQueued => 'Queued';
   @override
-  get transactionQueuedOffline =>
+  String get transactionQueuedOffline =>
       'App is not connected to the blockchain. Queued transaction (will be sent automatically upon reconnection).';
   @override
-  get unlock => 'Unlock Account with PIN';
+  String get unlock => 'Unlock Account with PIN';
   @override
-  get unlockAccount => 'Unlock account CURRENT_ACCOUNT_NAME with PIN';
+  String get unlockAccount => 'Unlock account CURRENT_ACCOUNT_NAME with PIN';
   @override
-  get updateDownload => 'Downloading...';
+  String get updateDownload => 'Downloading...';
   @override
-  get updateError => 'Update Failed';
+  String get updateError => 'Update Failed';
   @override
-  get updateJsUp => 'Metadata needs to be updated to continue.';
+  String get updateJsUp => 'Metadata needs to be updated to continue.';
   @override
-  get updateLatest => 'Your App is the newest version.';
+  String get updateLatest => 'Your App is the newest version.';
   @override
-  get updateToNewerVersionQ => 'New version found, update now?';
+  String get updateToNewerVersionQ => 'New version found, update now?';
   @override
-  get accountImport => 'Import account';
+  String get accountImport => 'Import account';
   @override
-  get txQueued => 'Queued Transaction';
+  String get txQueued => 'Queued Transaction';
   @override
-  get txQueuedOffline => 'You are offline. Transaction will be sent when you are back online.';
+  String get txQueuedOffline => 'You are offline. Transaction will be sent when you are back online.';
   @override
-  get txReady => 'Transaction is ready.';
+  String get txReady => 'Transaction is ready.';
   @override
-  get txBroadcast => 'Transaction has been broadcast.';
+  String get txBroadcast => 'Transaction has been broadcast.';
   @override
-  get txInBlock => 'Transaction is in a block.';
+  String get txInBlock => 'Transaction is in a block.';
   @override
-  get txError => 'Transaction error';
+  String get txError => 'Transaction error';
   @override
-  get updatingAppState => 'Updating the app state...';
+  String get updatingAppState => 'Updating the app state...';
   @override
-  get cameraPermissionError => 'There was an error getting the camera permission. '
+  String get cameraPermissionError => 'There was an error String getting the camera permission. '
       'Alternatively, you can grant permission in the app settings.';
   @override
-  get appSettings => 'App settings';
+  String get appSettings => 'App settings';
 }
 
 class TranslationsDeHome implements TranslationsHome {
   @override
-  get cancel => 'Abbruch';
+  String get cancel => 'Abbruch';
   @override
-  get closeApp => 'App schliessen';
+  String get closeApp => 'App schliessen';
   @override
-  get contacts => 'Kontakte';
+  String get contacts => 'Kontakte';
   @override
-  get copy => 'In die Zwischenablage kopieren';
+  String get copy => 'In die Zwischenablage kopieren';
   @override
-  get create => 'Konto erstellen';
+  String get create => 'Konto erstellen';
   @override
-  get detail => 'Detail';
+  String get detail => 'Detail';
   @override
-  get exitConfirm => 'Möchtest du die App verlassen?';
+  String get exitConfirm => 'Möchtest du die App verlassen?';
   @override
-  get errorOccurred => 'Es is ein Fehler aufgetaucht:';
+  String get errorOccurred => 'Es is ein Fehler aufString getaucht:';
   @override
-  get loading => 'Lädt...';
+  String get loading => 'Lädt...';
   @override
-  get next => 'Nächster Schritt';
+  String get next => 'Nächster Schritt';
   @override
-  get notifySubmitted => 'Transaktion eingereicht';
+  String get notifySubmitted => 'Transaktion eingereicht';
   @override
-  get notifySubmittedQueued => 'Die Transaktion in der Warteschlange wurde eingereicht';
+  String get notifySubmittedQueued => 'Die Transaktion in der Warteschlange wurde eingereicht';
   @override
-  get ok => 'OK';
+  String get ok => 'OK';
   @override
-  get pinNeeded => 'PIN ist notwendig um die App zu benutzen';
+  String get pinNeeded => 'PIN ist notwendig um die App zu benutzen';
   @override
-  get scan => 'Scannen';
+  String get scan => 'Scannen';
   @override
-  get settingNetwork => 'Wähle ein Portemonnaie';
+  String get settingNetwork => 'Wähle ein Portemonnaie';
   @override
-  get submit => 'Signieren und senden';
+  String get submit => 'Signieren und senden';
   @override
-  get submitCall => 'Wird ausgeführt';
+  String get submitCall => 'Wird ausgeführt';
   @override
-  get submitFees => 'Gebühren';
+  String get submitFees => 'Gebühren';
   @override
-  get submitFeesOffline => 'Gebühren nicht verfügbar (offline)';
+  String get submitFeesOffline => 'Gebühren nicht verfügbar (offline)';
   @override
-  get submitFrom => 'Transaktion wird signiert von ';
+  String get submitFrom => 'Transaktion wird signiert von ';
   @override
-  get submitNoSign => 'Senden (ohne Signatur)';
+  String get submitNoSign => 'Senden (ohne Signatur)';
   @override
-  get submitQr => 'Signieren via QR';
+  String get submitQr => 'Signieren via QR';
   @override
-  get submitTransaction => 'Transaktion senden';
+  String get submitTransaction => 'Transaktion senden';
   @override
-  get success => 'Erfolgreich';
+  String get success => 'Erfolgreich';
   @override
-  get switchAccount => 'Konto wechseln';
+  String get switchAccount => 'Konto wechseln';
   @override
-  get switchCommunity => 'Gemeinschaft wechseln';
+  String get switchCommunity => 'Gemeinschaft wechseln';
   @override
-  get transactionQueued => 'In der Warteschlange';
+  String get transactionQueued => 'In der Warteschlange';
   @override
-  get transactionQueuedOffline =>
+  String get transactionQueuedOffline =>
       'Die App ist nicht mit der Blockchain verbunden. Die Transaktionen in der Warteschlange werden automatisch bei erfolgreicher Verbindung versendet.';
   @override
-  get unlock => 'Konto entsperren mit PIN';
+  String get unlock => 'Konto entsperren mit PIN';
   @override
-  get unlockAccount => 'Entsperre Konto CURRENT_ACCOUNT_NAME mit PIN';
+  String get unlockAccount => 'Entsperre Konto CURRENT_ACCOUNT_NAME mit PIN';
   @override
-  get updateDownload => 'Herunterladen...';
+  String get updateDownload => 'Herunterladen...';
   @override
-  get updateError => 'Update fehlgeschlagen';
+  String get updateError => 'Update fehlgeschlagen';
   @override
-  get updateJsUp => 'Metadaten müssen aktualisiert werden um fortzusetzen.';
+  String get updateJsUp => 'Metadaten müssen aktualisiert werden um fortzusetzen.';
   @override
-  get updateLatest => 'Die App ist auf dem neusten Stand.';
+  String get updateLatest => 'Die App ist auf dem neusten Stand.';
   @override
-  get updateToNewerVersionQ => 'Neue Version gefunden, jetzt aktualisieren?';
+  String get updateToNewerVersionQ => 'Neue Version gefunden, jetzt aktualisieren?';
   @override
-  get accountImport => 'Konto importieren';
+  String get accountImport => 'Konto importieren';
   @override
-  get txQueued => 'Transaktion is in der Warteschlange';
+  String get txQueued => 'Transaktion is in der Warteschlange';
   @override
-  get txQueuedOffline => 'Du bist offline. Die Transaktion wird geschickt, wenn du wieder online bist.';
+  String get txQueuedOffline => 'Du bist offline. Die Transaktion wird geschickt, wenn du wieder online bist.';
   @override
-  get txReady => 'Transaktion bereit.';
+  String get txReady => 'Transaktion bereit.';
   @override
-  get txBroadcast => 'Transaktion wurde im Netzwerk';
+  String get txBroadcast => 'Transaktion wurde im Netzwerk';
   @override
-  get txInBlock => 'Transaction ist in einem Block';
+  String get txInBlock => 'Transaction ist in einem Block';
   @override
-  get txError => 'Transaktionsfehler';
+  String get txError => 'Transaktionsfehler';
   @override
-  get updatingAppState => 'App-Zustand wird aktualisiert...';
+  String get updatingAppState => 'App-Zustand wird aktualisiert...';
   @override
-  get cameraPermissionError => 'Es gab einen Fehler beim überprüfen der Kameraerlaubnis.'
+  String get cameraPermissionError => 'Es gab einen Fehler beim überprüfen der Kameraerlaubnis.'
       'Du kannst die Erlaubnis für die Kamera auch über die App-Einstellungen erteilen.';
   @override
-  get appSettings => 'App-Einstellungen';
+  String get appSettings => 'App-Einstellungen';
 }
 
 class TranslationsFrHome implements TranslationsHome {
   @override
-  get cancel => throw UnimplementedError();
+  String get cancel => throw UnimplementedError();
   @override
-  get scan => throw UnimplementedError();
+  String get scan => throw UnimplementedError();
   @override
-  get contacts => throw UnimplementedError();
+  String get contacts => throw UnimplementedError();
   @override
-  get closeApp => throw UnimplementedError();
+  String get closeApp => throw UnimplementedError();
   @override
-  get copy => throw UnimplementedError();
+  String get copy => throw UnimplementedError();
   @override
-  get create => throw UnimplementedError();
+  String get create => throw UnimplementedError();
   @override
-  get detail => throw UnimplementedError();
+  String get detail => throw UnimplementedError();
   @override
-  get exitConfirm => throw UnimplementedError();
+  String get exitConfirm => throw UnimplementedError();
   @override
-  get errorOccurred => throw UnimplementedError();
+  String get errorOccurred => throw UnimplementedError();
   @override
-  get loading => throw UnimplementedError();
+  String get loading => throw UnimplementedError();
   @override
-  get next => throw UnimplementedError();
+  String get next => throw UnimplementedError();
   @override
-  get notifySubmitted => throw UnimplementedError();
+  String get notifySubmitted => throw UnimplementedError();
   @override
-  get notifySubmittedQueued => throw UnimplementedError();
+  String get notifySubmittedQueued => throw UnimplementedError();
   @override
-  get ok => throw UnimplementedError();
+  String get ok => throw UnimplementedError();
   @override
-  get pinNeeded => throw UnimplementedError();
+  String get pinNeeded => throw UnimplementedError();
   @override
-  get settingNetwork => throw UnimplementedError();
+  String get settingNetwork => throw UnimplementedError();
   @override
-  get submit => throw UnimplementedError();
+  String get submit => throw UnimplementedError();
   @override
-  get submitCall => throw UnimplementedError();
+  String get submitCall => throw UnimplementedError();
   @override
-  get submitFees => throw UnimplementedError();
+  String get submitFees => throw UnimplementedError();
   @override
-  get submitFeesOffline => throw UnimplementedError();
+  String get submitFeesOffline => throw UnimplementedError();
   @override
-  get submitFrom => throw UnimplementedError();
+  String get submitFrom => throw UnimplementedError();
   @override
-  get submitNoSign => throw UnimplementedError();
+  String get submitNoSign => throw UnimplementedError();
   @override
-  get submitQr => throw UnimplementedError();
+  String get submitQr => throw UnimplementedError();
   @override
-  get submitTransaction => throw UnimplementedError();
+  String get submitTransaction => throw UnimplementedError();
   @override
-  get success => throw UnimplementedError();
+  String get success => throw UnimplementedError();
   @override
-  get switchAccount => throw UnimplementedError();
+  String get switchAccount => throw UnimplementedError();
   @override
-  get switchCommunity => throw UnimplementedError();
+  String get switchCommunity => throw UnimplementedError();
   @override
-  get transactionQueued => throw UnimplementedError();
+  String get transactionQueued => throw UnimplementedError();
   @override
-  get transactionQueuedOffline => throw UnimplementedError();
+  String get transactionQueuedOffline => throw UnimplementedError();
   @override
-  get unlock => throw UnimplementedError();
+  String get unlock => throw UnimplementedError();
   @override
-  get unlockAccount => throw UnimplementedError();
+  String get unlockAccount => throw UnimplementedError();
   @override
-  get updateDownload => throw UnimplementedError();
+  String get updateDownload => throw UnimplementedError();
   @override
-  get updateError => throw UnimplementedError();
+  String get updateError => throw UnimplementedError();
   @override
-  get updateJsUp => throw UnimplementedError();
+  String get updateJsUp => throw UnimplementedError();
   @override
-  get updateLatest => throw UnimplementedError();
+  String get updateLatest => throw UnimplementedError();
   @override
-  get updateToNewerVersionQ => throw UnimplementedError();
+  String get updateToNewerVersionQ => throw UnimplementedError();
   @override
-  get accountImport => throw UnimplementedError();
+  String get accountImport => throw UnimplementedError();
   @override
-  get txQueued => throw UnimplementedError();
+  String get txQueued => throw UnimplementedError();
   @override
-  get txQueuedOffline => throw UnimplementedError();
+  String get txQueuedOffline => throw UnimplementedError();
   @override
-  get txReady => throw UnimplementedError();
+  String get txReady => throw UnimplementedError();
   @override
-  get txBroadcast => throw UnimplementedError();
+  String get txBroadcast => throw UnimplementedError();
   @override
-  get txInBlock => throw UnimplementedError();
+  String get txInBlock => throw UnimplementedError();
   @override
-  get txError => throw UnimplementedError();
+  String get txError => throw UnimplementedError();
   @override
-  get updatingAppState => throw UnimplementedError();
+  String get updatingAppState => throw UnimplementedError();
   @override
-  get cameraPermissionError => throw UnimplementedError();
+  String get cameraPermissionError => throw UnimplementedError();
   @override
-  get appSettings => throw UnimplementedError();
+  String get appSettings => throw UnimplementedError();
 }
