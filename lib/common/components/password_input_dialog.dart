fa5df260df8031b2beae198c690b3ff097f72b64
Janara Sourbayeva <janara2610@gmail.com>
2022-11-18 22:39:34 +0600

    index.dart line 551, 446, password_input_dialog.dart line 10

--- lib/common/components/password_input_dialog.dart
+++ lib/common/components/password_input_dialog.dart
@@ -7,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-showPasswordInputDialog(
+Widget showPasswordInputDialog(
   BuildContext context,
   AccountData account,
   Widget? title,
