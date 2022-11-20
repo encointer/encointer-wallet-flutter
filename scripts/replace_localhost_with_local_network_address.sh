d6ec84e3f0a9cb37b7a698c35b0c41f564cb19f2
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-11-21 04:19:13 +0600

    added script get local ip address

---
+++ scripts/replace_localhost_with_local_network_address.sh
@@ -0,0 +1,19 @@
+#!bin/sh
+set -exuo pipefail
+
+# 'value': 'ws://127.0.0.1:9944',
+# 192.168.43.52
+
+# ifconfig | grep inet
+
+# echo '$msg1' two three
+
+LOCAL_IP=$(ifconfig | awk '/inet /&&!/127.0.0.1/{print $2;exit}')
+
+# ls
+
+cd lib/config
+# printf "some data for the file\nAnd a new line">> consts.dart
+sed -i 's/10.0.2.2/LOCAL_IP/g' consts.dart
+
+# echo LOCAL_IP
