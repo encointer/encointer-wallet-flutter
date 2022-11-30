462656e8b3e87cf2199bef015d9e0e30ee085a82
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-11-30 15:11:56 +0600

    tested on IOS

--- test_driver/helpers/other_test.dart
+++ test_driver/helpers/other_test.dart
@@ -19,10 +19,7 @@ Future<void> createNewbieAccountAndSendMoney(FlutterDriver driver, String accoun
   await driver.enterText(account);
   await driver.tap(find.byValueKey('create-account-confirm'));
 
-  // await driver.tap(find.byValueKey('Alice'));
   await closePanel(driver);
-
-  // await driver.tap(find.byValueKey('transfer'));
 }
 
 Future<void> sendMonayToAccount(FlutterDriver driver) async {
@@ -31,11 +28,6 @@ Future<void> sendMonayToAccount(FlutterDriver driver) async {
   await driver.enterText('0.2');
 
   await driver.runUnsynchronized(() async {
-    // await scrollToSendAddress(driver);
-    // await driver.tap(find.byValueKey('send-to-address'));
-    // await driver.waitFor(find.byValueKey(account));
-    // await driver.(find.byValueKey(account));
-
     await driver.waitFor(find.byValueKey('make-transfer'));
     await driver.tap(find.byValueKey('make-transfer'));
 
@@ -43,10 +35,6 @@ Future<void> sendMonayToAccount(FlutterDriver driver) async {
     await driver.tap(find.byValueKey('make-transfer-send'));
     await driver.waitFor(find.byValueKey('transfer-done'));
     await driver.tap(find.byValueKey('transfer-done'));
-    // await addDelay(1000);
-    // await driver.tap(find.byValueKey('panel-controller'));
-    // await driver.tap(find.byValueKey(account));
-    // await closePanel(driver);
     await addDelay(1000);
   });
 }
