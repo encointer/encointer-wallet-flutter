4e4669196af81eae2c8d0806665ab75a161ba710
clangenb <37865735+clangenb@users.noreply.github.com>
2022-12-09 10:28:37 +0100

    Build script improvements (#904)
    * use flutter_wrapper in appstore post clone scripts
    
    * [scripts] rename install_flutter.sh to install_flutter_wrapper.sh
    
    * [scripts] add `fdroid_build.sh`
    
    * [GA] execute fdroid stuff also when we commit on master and also check `fdroid_build.sh`
    
    * make fdroid_build.sh and install_flutter_wrapper.sh executable

---
+++ scripts/fdroid_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Build file for the f-droid build. The idea is that we do not need to update the
+#
+# https://gitlab.com/fdroid/fdroiddata/-/blob/master/metadata/org.encointer.wallet.yml
+#
+# if something in the build-process changes.
+
+set -exuo pipefail
+
+./flutterw config --no-analytics
+./flutterw build apk --flavor fdroid
