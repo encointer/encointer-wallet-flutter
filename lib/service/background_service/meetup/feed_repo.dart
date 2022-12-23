f7b3ec58d0df73f12f59f45662bd33b68aee2758
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-12-23 15:18:25 +0600

    tested success on real device samsung A12

--- lib/service/background_service/meetup/feed_repo.dart
+++ lib/service/background_service/meetup/feed_repo.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:http/http.dart' as http;
 
-// import 'package:encointer_wallet/config/consts.dart';
-import 'package:encointer_wallet/service/log/log_service.dart';
+import 'package:encointer_wallet/config/consts.dart';
 import 'package:encointer_wallet/service/background_service/meetup/feed_model.dart';
+import 'package:encointer_wallet/service/log/log_service.dart';
 
 class FeedRepo {
   FeedRepo([http.Client? client]) : _client = client ?? http.Client();
@@ -12,8 +12,7 @@ class FeedRepo {
   final http.Client _client;
 
   Future<List<Feed>?> fetchData([String langCode = 'en']) async {
-    // final uri = Uri.parse(replaceLocalePlaceholder(meetupNotificationLink, langCode));
-    final uri = Uri.parse('https://eldar2021.github.io/test/cm.json');
+    final uri = Uri.parse(replaceLocalePlaceholder(meetupNotificationLink, langCode));
     try {
       final response = await _client.get(uri);
       return feedFromJson(response.body);
