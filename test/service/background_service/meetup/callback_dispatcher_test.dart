2df08185d02fa9ef4b9f26b5fccc0711c58cfe63
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-12-23 16:19:18 +0600

    fix unit test

--- test/service/background_service/meetup/callback_dispatcher_test.dart
+++ test/service/background_service/meetup/callback_dispatcher_test.dart
@@ -1,95 +1,44 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mocktail/mocktail.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:timezone/timezone.dart' as tz;
 
-// import 'package:encointer_wallet/service/background_service/background_service.dart';
+import 'package:encointer_wallet/service/background_service/background_service.dart';
 
-// import '../../../fixtures/fixture_reader.dart';
 
-// class Functions {
-//   Future<bool> showNotification(int id, String title, String body) async {
-//     return Future.value(true);
-//   }
+class Functions {
+  Future<bool> showNotification(int id, String title, String body) async {
+    return Future.value(true);
+  }
 
-//   Future<bool> cache(List<String> value) async {
-//     return Future.value(true);
-//   }
-// }
+  Future<bool> cache(List<String> value) async {
+    return Future.value(true);
+  }
+}
 
-// class MockFunctions extends Mock implements Functions {}
+class MockFunctions extends Mock implements Functions {}
 
-// void main() async {
-//   final feeds = feedFromJson(fixture('feed_list'));
+class MockLocation extends Mock implements tz.Location {}
 
-//   final functions = MockFunctions();
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-//   void _responseTrueShowNotification(int i) {
-//     when(() => functions.showNotification(i, feeds[i].title, feeds[i].content)).thenAnswer((inc) async => true);
-//   }
+  final local = MockLocation();
+  Future<void> mockScheduleNotification(int id, String title, String body, tz.TZDateTime scheduledDate) async {}
 
-//   void _verifyShowNotification(int i) {
-//     verify(() => functions.showNotification(i, feeds[i].title, feeds[i].content)).called(1);
-//   }
+  const channel = MethodChannel('flutter_timezone');
 
-//   void _verifyNeverShowNotification(int i) {
-//     verifyNever(() => functions.showNotification(i, feeds[i].title, feeds[i].content));
-//   }
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
 
-//   test('notificationForLoop show notification 3, cache list=[]', () async {
-//     final list = <String>[];
-//     _responseTrueShowNotification(0);
-//     _responseTrueShowNotification(1);
-//     _responseTrueShowNotification(2);
-
-//     final shownNotifications = await showAllNotificationsFromFeed(feeds, list, functions.showNotification);
-
-//     _verifyShowNotification(0);
-//     _verifyShowNotification(1);
-//     _verifyShowNotification(2);
-//     _verifyNeverShowNotification(3);
-
-//     expect(shownNotifications, ['msg-1', 'msg-2', 'msg-3']);
-//   });
-
-//   test('notificationForLoop show notification 2, cache list=["msg-1"]', () async {
-//     final list = <String>['msg-1'];
-//     _responseTrueShowNotification(1);
-//     _responseTrueShowNotification(2);
-
-//     final shownNotifications = await showAllNotificationsFromFeed(feeds, list, functions.showNotification);
-
-//     _verifyNeverShowNotification(0);
-//     _verifyShowNotification(1);
-//     _verifyShowNotification(2);
-//     _verifyNeverShowNotification(3);
-
-//     expect(shownNotifications, ['msg-2', 'msg-3']);
-//   });
-
-//   test('notificationForLoop show notification 1, cache list=["msg-1", "msg-2"]', () async {
-//     final list = <String>['msg-1', 'msg-2'];
-
-//     _responseTrueShowNotification(2);
-
-//     final shownNotifications = await showAllNotificationsFromFeed(feeds, list, functions.showNotification);
-
-//     _verifyNeverShowNotification(0);
-//     _verifyNeverShowNotification(1);
-//     _verifyShowNotification(2);
-//     _verifyNeverShowNotification(3);
-
-//     expect(shownNotifications, ['msg-3']);
-//   });
-
-//   test('notificationForLoop show notification 0, cache list=["msg-1", "msg-2", "msg-3"]', () async {
-//     final list = <String>['msg-1', 'msg-2', 'msg-3'];
-
-//     final shownNotifications = await showAllNotificationsFromFeed(feeds, list, functions.showNotification);
-
-//     _verifyNeverShowNotification(0);
-//     _verifyNeverShowNotification(1);
-//     _verifyNeverShowNotification(2);
-//     _verifyNeverShowNotification(3);
-
-//     expect(shownNotifications, <String>[]);
-//   });
-// }
+  test('notificationForLoop show notification 3, cache list=[]', () async {
+    await MeetupNotification.executeTaskIsolate(
+      local,
+      mockScheduleNotification,
+      'en',
+    );
+  });
+}
--- test/service/background_service/meetup/feed_model_test.dart
+++ test/service/background_service/meetup/feed_model_test.dart
@@ -1,26 +1,26 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:encointer_wallet/service/background_service/meetup/feed_model.dart';
+import 'package:encointer_wallet/service/background_service/meetup/feed_model.dart';
 
-// import '../../../fixtures/fixture_reader.dart';
+import '../../../fixtures/fixture_reader.dart';
 
-// void main() {
-//   test('Object is Feed model', () {
-//     final feed = Feed(id: 'msg-1', title: 'Title', content: 'Cotent', showAt: DateTime.now());
-//     expect(feed, isA<Feed>());
-//   });
+void main() {
+  test('Object is Feed model', () {
+    final feed = Feed(id: 'msg-1', title: 'Title', content: 'Cotent', showAt: DateTime.now());
+    expect(feed, isA<Feed>());
+  });
 
-//   test('feedFromJson return List<Feed>', () {
-//     final jsonMap = fixture('feed_list');
-//     final feeds = feedFromJson(jsonMap);
-//     expect(feeds, isA<List<Feed>>());
-//   });
+  test('feedFromJson return List<Feed>', () {
+    final jsonMap = fixture('feed_list');
+    final feeds = feedFromJson(jsonMap);
+    expect(feeds, isA<List<Feed>>());
+  });
 
-//   test('Feed fromJson test', () {
-//     final jsonMap = json.decode(fixture('feed')) as Map<String, dynamic>;
-//     final feed = Feed.fromJson(jsonMap);
-//     expect(feed, isA<Feed>());
-//   });
-// }
+  test('Feed fromJson test', () {
+    final jsonMap = json.decode(fixture('feed')) as Map<String, dynamic>;
+    final feed = Feed.fromJson(jsonMap);
+    expect(feed, isA<Feed>());
+  });
+}
