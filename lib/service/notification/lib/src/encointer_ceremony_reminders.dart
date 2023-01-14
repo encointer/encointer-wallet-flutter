ff22f1f5921268ebba697cbdc352911b1ec255d8
clangenb <37865735+clangenb@users.noreply.github.com>
2023-01-14 12:34:12 +0100

    [notifications] minor refactoring in meetup reminder notification

--- lib/service/notification/lib/src/encointer_ceremony_reminders.dart
+++ lib/service/notification/lib/src/encointer_ceremony_reminders.dart
@@ -9,22 +9,24 @@ const int oneDayBeforeMeetupReminderOffset = 2000000; // two millions
 /// Schedules two meetup reminders for a registered meetup.
 Future<void> scheduleMeetupReminderNotifications(int ceremonyIndex, int meetupTime, TranslationsEncointer dic) async {
   final meetupDateTime = DateTime.fromMillisecondsSinceEpoch(meetupTime);
-  final beforeOneHour = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(hours: 1)), tz.local);
-  final beforeOneDay = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(days: 1)), tz.local);
-  if (beforeOneHour.isAfter(DateTime.now())) {
+
+  final oneHourBeforeMeetup = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(hours: 1)), tz.local);
+  if (oneHourBeforeMeetup.isAfter(DateTime.now())) {
     await NotificationPlugin.scheduleNotification(
       ceremonyIndex + oneHourBeforeMeetupReminderOffset,
       dic.meetupNotificationOneHourBeforeTitle,
       dic.meetupNotificationOneHourBeforeContent,
-      beforeOneHour,
+      oneHourBeforeMeetup,
     );
   }
-  if (beforeOneDay.isAfter(DateTime.now())) {
+
+  final oneDayBeforeMeetup = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(days: 1)), tz.local);
+  if (oneDayBeforeMeetup.isAfter(DateTime.now())) {
     await NotificationPlugin.scheduleNotification(
       ceremonyIndex + oneDayBeforeMeetupReminderOffset,
       dic.meetupNotificationOneDayBeforeTitle,
       dic.meetupNotificationOneDayBeforeContent,
-      beforeOneDay,
+      oneDayBeforeMeetup,
     );
   }
 }
