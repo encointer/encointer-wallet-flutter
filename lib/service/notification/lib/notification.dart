bf723a27a8b0deaf99962837a8593cc80ab24a14
clangenb <37865735+clangenb@users.noreply.github.com>
2023-01-14 12:46:15 +0100

    [notifications] rename encointer_ceremony_reminders to ceremony_notifications

--- lib/service/notification/lib/notification.dart
+++ lib/service/notification/lib/notification.dart
@@ -1,4 +1,4 @@
 /// Manages the notification plugin and contains methods for specific notifications.
 
 export 'src/notification_plugin.dart';
-export 'src/encointer_ceremony_reminders.dart';
+export 'src/ceremony_notifications.dart';
