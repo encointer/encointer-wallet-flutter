9c93f4d7a69596b67c730b13b636f425df210ba2
clangenb <37865735+clangenb@users.noreply.github.com>
2023-01-14 12:27:35 +0100

    [notifications] add constants for meetup reminder notification id offsets

--- lib/service/notification/lib/src/encointer_ceremony_reminders.dart
+++ lib/service/notification/lib/src/encointer_ceremony_reminders.dart
@@ -3,13 +3,17 @@ import 'package:timezone/timezone.dart' as tz;
 import 'package:encointer_wallet/utils/translations/translations_encointer.dart';
 import 'package:encointer_wallet/service/notification/lib/notification.dart';
 
-Future<void> scheduleMeetupReminderNotifications(int notificationId, int meetupTime, TranslationsEncointer dic) async {
+const int oneHourBeforeMeetupReminderOffset = 1000000; // one million
+const int oneDayBeforeMeetupReminderOffset = 2000000; // two millions
+
+/// Schedules two meetup reminders for a registered meetup.
+Future<void> scheduleMeetupReminderNotifications(int ceremonyIndex, int meetupTime, TranslationsEncointer dic) async {
   final meetupDateTime = DateTime.fromMillisecondsSinceEpoch(meetupTime);
   final beforeOneHour = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(hours: 1)), tz.local);
   final beforeOneDay = tz.TZDateTime.from(meetupDateTime.subtract(const Duration(days: 1)), tz.local);
   if (beforeOneHour.isAfter(DateTime.now())) {
     await NotificationPlugin.scheduleNotification(
-      notificationId,
+      ceremonyIndex + oneHourBeforeMeetupReminderOffset,
       dic.meetupNotificationOneHourBeforeTitle,
       dic.meetupNotificationOneHourBeforeContent,
       beforeOneHour,
@@ -17,7 +21,7 @@ Future<void> scheduleMeetupReminderNotifications(int notificationId, int meetupT
   }
   if (beforeOneDay.isAfter(DateTime.now())) {
     await NotificationPlugin.scheduleNotification(
-      24 + notificationId,
+      ceremonyIndex + oneDayBeforeMeetupReminderOffset,
       dic.meetupNotificationOneDayBeforeTitle,
       dic.meetupNotificationOneDayBeforeContent,
       beforeOneDay,
