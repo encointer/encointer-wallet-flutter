4d9335e6241d9a7c12003168db8145b78e384b5d
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-01-19 13:01:49 +0600

    added test

--- packages/translation/test/src/translation_test.dart
+++ packages/translation/test/src/translation_test.dart
@@ -1,12 +1,34 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:translation/translation.dart';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+import 'package:translation/translation.dart';
 
 void main() {
-  group('Translation', () {
-    test('can be instantiated', () {
-      // expect(Translation(), isNotNull);
-    });
+  test('AppLocalizationsDelegate should support en, de, fr, and ru', () {
+    const delegate = AppLocalizationsDelegate(Locale('en'));
+
+    expect(delegate.isSupported(const Locale('en', '')), isTrue);
+    expect(delegate.isSupported(const Locale('de', '')), isTrue);
+    expect(delegate.isSupported(const Locale('fr', '')), isTrue);
+    expect(delegate.isSupported(const Locale('ru', '')), isTrue);
+    expect(delegate.isSupported(const Locale('es', '')), isFalse);
+  });
+
+  test('I18n should return correct translations for different locales', () {
+    const i18nEn = I18n(Locale('en', ''));
+    expect(i18nEn.translationsForLocale(), isA<TranslationsEn>());
+
+    const i18nDe = I18n(Locale('de', ''));
+    expect(i18nDe.translationsForLocale(), isA<TranslationsDe>());
+
+    const i18nFr = I18n(Locale('fr', ''));
+    expect(i18nFr.translationsForLocale(), isA<TranslationsFr>());
+
+    const i18nRu = I18n(Locale('ru', ''));
+    expect(i18nRu.translationsForLocale(), isA<TranslationsRu>());
+  });
+
+  test('I18n should return English translations for unsupported locales', () {
+    const i18n = I18n(Locale('invalid', ''));
+    expect(i18n.translationsForLocale(), isA<TranslationsEn>());
   });
 }
