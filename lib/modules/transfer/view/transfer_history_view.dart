572f369c49482416a2565a2ef1c183712329cb42
Janara Sourbayeva <janara2610@gmail.com>
2023-01-25 23:31:36 +0600

    started ui of the transactionPage

--- lib/modules/transfer/view/transfer_history_view.dart
+++ lib/modules/transfer/view/transfer_history_view.dart
@@ -1,8 +1,12 @@
+import 'package:encointer_wallet/common/theme.dart';
+
+import 'package:encointer_wallet/page/qr_scan/qr_scan_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:provider/provider.dart';
+import 'package:iconsax/iconsax.dart';
 
+import 'package:provider/provider.dart';
 import 'package:encointer_wallet/modules/modules.dart';
 
 class TransferHistoryView extends StatelessWidget {
@@ -14,20 +18,107 @@ class TransferHistoryView extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = context.watch<TransferHistoryStore>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Transfer History'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: AppBar(
+            title: Column(
+              children: [
+                const Text('Transactions'),
+                const SizedBox(height: 8),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+                  Text(
+                    '5G9dry...MEKpNZ',
+                    style: TextStyle(color: encointerBlack, fontSize: 14),
+                  ),
+                  SizedBox(width: 8),
+                  Icon(
+                    Icons.copy_outlined,
+                    size: 18,
+                  )
+                ]),
+
+                // Text('Balance'),
+              ],
+            ),
+            actions: [
+              // IconButton(
+              //     iconSize: 30,
+              //     icon: const Icon(Iconsax.scan_barcode),
+              //     onPressed: () {
+              //       Navigator.of(context).pushNamed(
+              //         ScanPage.route,
+              //         arguments: ScanPageParams(scannerContext: QrScannerContext.transferPage),
+              //       );
+              //     }),
+              IconButton(
+                icon: const Icon(Icons.more_vert),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: SizedBox(
+                          height: 150,
+                          child: Wrap(
+                            children: <Widget>[
+                              ListTile(
+                                  leading: const Icon(
+                                    Icons.qr_code_2,
+                                    color: zurichLion,
+                                  ),
+                                  title: const Text(
+                                    'Scan QR-Code',
+                                    style: TextStyle(color: zurichLion),
+                                  ),
+                                  onTap: () => {
+                                        Navigator.of(context).pushNamed(
+                                          ScanPage.route,
+                                          arguments: ScanPageParams(scannerContext: QrScannerContext.transferPage),
+                                        )
+                                      }),
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.copy_sharp,
+                                  color: zurichLion,
+                                ),
+                                title: const Text(
+                                  'Copy',
+                                  style: TextStyle(color: zurichLion),
+                                ),
+                                onTap: () => {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       body: Observer(builder: (_) {
         if (store.transfers == null) {
           return const Center(child: CupertinoActivityIndicator());
         } else if (store.transfers!.isEmpty) {
-          return const Center(child: Text('No Transfres'));
+          return const Center(child: Text('No Transfers'));
         } else if (store.transfers!.isNotEmpty) {
           return ListView.builder(
             itemCount: store.transfers!.length,
             itemBuilder: (BuildContext context, int index) {
               final transfer = store.transfers![index];
               return ListTile(
+                // leading: Icon(),
                 title: Text(transfer.accountName),
                 subtitle: Text(transfer.accountAddress),
               );
