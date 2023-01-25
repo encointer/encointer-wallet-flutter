23bbf975a20a38aa4ee592aa13341ad6d7e99fcf
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-01-25 18:54:13 +0600

    remove unused code

--- lib/service/substrate_api/encointer/no_tee_api.dart
+++ lib/service/substrate_api/encointer/no_tee_api.dart
@@ -5,38 +5,34 @@ import 'package:encointer_wallet/models/encointer_balance_data/balance_entry.dar
 import 'package:encointer_wallet/service/substrate_api/core/js_api.dart';
 
 class NoTeeApi {
-  NoTeeApi(this.jsApi)
-      : ceremonies = Ceremonies(jsApi),
-        balances = Balances(jsApi);
+  NoTeeApi(JSApi jsApi) : balances = Balances(jsApi);
 
-  final JSApi jsApi;
-  final Ceremonies ceremonies;
   final Balances balances;
 }
 
-class Ceremonies {
-  Ceremonies(this.jsApi);
+// class Ceremonies {
+//   Ceremonies(this.jsApi);
 
-  final JSApi jsApi;
+//   final JSApi jsApi;
 
-  Future<int> participantIndex(CommunityIdentifier cid, int cIndex, String pubKey) async {
-    return jsApi
-        .evalJavascript('encointer.getParticipantIndex(${jsonEncode(cid)}, "$cIndex" ,"$pubKey")')
-        .then((value) => int.parse(value as String));
-  }
+// Future<int> participantIndex(CommunityIdentifier cid, int cIndex, String pubKey) async {
+//   return jsApi
+//       .evalJavascript('encointer.getParticipantIndex(${jsonEncode(cid)}, "$cIndex" ,"$pubKey")')
+//       .then((value) => int.parse(value as String));
+// }
 
-  Future<int> meetupIndex(CommunityIdentifier cid, int cIndex, String pubKey) async {
-    return jsApi
-        .evalJavascript('encointer.getMeetupIndex(${jsonEncode(cid)}, "$cIndex","$pubKey")')
-        .then((value) => int.parse(value as String));
-  }
+// Future<int> meetupIndex(CommunityIdentifier cid, int cIndex, String pubKey) async {
+//   return jsApi
+//       .evalJavascript('encointer.getMeetupIndex(${jsonEncode(cid)}, "$cIndex","$pubKey")')
+//       .then((value) => int.parse(value as String));
+// }
 
-  Future<List<String>> meetupRegistry(CommunityIdentifier cid, int cIndex, int mIndex) async {
-    return jsApi
-        .evalJavascript('encointer.getMeetupRegistry(${jsonEncode(cid)}, "$cIndex", "$mIndex")')
-        .then((value) => List<String>.from(value as Iterable));
-  }
-}
+// Future<List<String>> meetupRegistry(CommunityIdentifier cid, int cIndex, int mIndex) async {
+//   return jsApi
+//       .evalJavascript('encointer.getMeetupRegistry(${jsonEncode(cid)}, "$cIndex", "$mIndex")')
+//       .then((value) => List<String>.from(value as Iterable));
+// }
+// }
 
 class Balances {
   Balances(this.jsApi);
