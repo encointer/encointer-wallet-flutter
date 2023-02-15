9ed21873a4519b29ec62abb05b1c057f533d148f
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2023-02-15 15:06:48 +0600

    Introduce melos for workspace and subpackage managing (#983)
    * create two package
    
    * create melos.yaml file
    
    * try activate melos 01
    
    * try activate melos 02
    
    * try activate melos 03
    
    * try activate melos 04
    
    * try activate melos 05
    
    * try activate melos 06
    
    * try activate melos 07
    
    * try activate melos 08
    
    * try activate melos 09
    
    * try activate melos 10
    
    * write script
    
    * write script test
    
    * write script test 01
    
    * write script test 02
    
    * write script test 03
    
    * write script test 04
    
    * write script test 05
    
    * write script test 06
    
    * delete comments from actions
    
    * refactor README.md file
    
    * write descriptions for melos commands
    
    * Melos use flutterw (#985)
    
    * add el-melos-packages unit and android build CI
    
    * try use select package
    
    * fix file exist to scope
    
    * added export path for molos bootstrap
    
    * try fix unit test
    
    * try fix unit test 01
    
    * remove Melos_sdk_path
    
    * formatted code
    
    * changed ./fluttew and ../../flutterw to flutter
    
    * use for flutter actions
    
    * try fix melos generate build runner
    
    * Use Melos command for all GitHub Actions (#986)
    
    * change Build JS to melos command
    
    * remove some comment
    
    * fix README.md file
    
    * remove el-melos-packages from github actions
    
    * fix by comment (#989)
    
    * Readme: Make code snippets copyable and add remark about outdated `.flutter` submodule (#984)
    
    * [Readme] make code snippets copyable
    
    * [Readme] add comment about fixing the `.flutter` submodule
    
    * [Readme] add shell lexer to code snippets for syntax highlighting
    
    ---------
    
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>
    
    * Make melos_install.sh executable
    
    * romove .gitignore files from packages
    
    * remove coverage files from packages and fix readme md files
    
    * remove export from melos install command
    
    * romeve el-melos-packages from actions
    
    ---------
    
    Co-authored-by: Zhanara <91150590+SourbaevaJanaraJ@users.noreply.github.com>
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>
    
    * fix real test(choosing cid change index)
    
    * fix read me command fields
    
    * Melos cache fixes and fixes for windows. (#992)
    
    * Bump flutter v3.7.1 (#991)
    
    * bump flutter version
    
    * bump package and sdk
    
    * tested
    
    * add melos as dev-dependency
    
    * fix some commands for windows shell and remove need for cache re-export.
    
    * [GA] check if unit tests work with new melos config
    
    * execute CI on on PR into el-melos-packages branch
    
    * remove accidentally added readme section
    
    * [melos] fix yarn build
    
    * [melos] fix build-runner command
    
    * [GA] remove config for build on `el-melos-package` branch because it works.
    
    * fix qrScanHintAccount text
    
    * fix readme files in packages
    
    * added el-melos-packages to some actions
    
    * fix melos commands
    
    * added windows command to readme md
    
    * remove el-melos-packages
    
    ---------
    
    Co-authored-by: Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
    Co-authored-by: Eldar2021 <eldiiaralmazbekov@gmail.com>
    
    * Melos fix by comment (#998)
    
    * created branch
    
    * fix android build action
    
    * try text remove  in install melos
    
    * replate ganarate-build-runner to run-build-runner
    
    * fix melos commands
    
    * fix unit test
    
    * remove el-melos-packages from actions
    
    * remove export from melos install script
    
    ---------
    
    Co-authored-by: Zhanara <91150590+SourbaevaJanaraJ@users.noreply.github.com>
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

---
+++ packages/ew_format/README.md
@@ -0,0 +1,3 @@
+# Ew Format
+
+Encointer Format Package
---
+++ packages/ew_format/test/ew_format_test.dart
@@ -0,0 +1,10 @@
+import 'package:ew_format/ew_format.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('EwFormat', () {
+    test('can be instantiated', () {
+      expect(const EwFormat(), isNotNull);
+    });
+  });
+}
