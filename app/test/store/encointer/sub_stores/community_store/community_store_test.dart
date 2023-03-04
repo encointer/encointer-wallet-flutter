2052677f102264433900097a8d56a80646509ebc
clangenb <37865735+clangenb@users.noreply.github.com>
2023-03-04 10:36:39 +0100

    [community_store_test] use await instead of unawaited.

--- app/test/store/encointer/sub_stores/community_store/community_store_test.dart
+++ app/test/store/encointer/sub_stores/community_store/community_store_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:encointer_wallet/config.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -51,12 +49,12 @@ void main() {
         ..setMeetupTime(10)
         ..setMeetupLocations(testLocations);
 
-      unawaited(Future.wait<void>([
+      await Future.wait<void>([
         communityStore.setCommunityMetadata(testMetadata),
         communityStore.setBootstrappers(bootstrappers),
         communityStore.initCommunityAccountStore(aliceAddress),
         communityStore.initCommunityAccountStore(bobAddress)
-      ]));
+      ]);
 
       final aliceCommunityAccountStore = communityStore.communityAccountStores![aliceAddress]!;
       final bobCommunityAccountStore = communityStore.communityAccountStores![bobAddress]!;
