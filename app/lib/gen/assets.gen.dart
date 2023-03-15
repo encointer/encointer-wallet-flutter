90e52aa455f558498b5b6d4e5d02997b009bece8
AZ Ackmatoff <ackmatoff@gmail.com>
2023-03-15 12:40:53 +0600

    az/Move js_encointer outside lib

--- app/lib/gen/assets.gen.dart
+++ app/lib/gen/assets.gen.dart
@@ -11,74 +11,94 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
 
-class $LibGen {
-  const $LibGen();
+class $JsServiceEncointerGen {
+  const $JsServiceEncointerGen();
 
-  $LibJsServiceEncointerGen get jsServiceEncointer => const $LibJsServiceEncointerGen();
+  $JsServiceEncointerDistGen get dist => const $JsServiceEncointerDistGen();
 }
 
 class $AssetsAvatarsGen {
   const $AssetsAvatarsGen();
 
   /// File path: assets/avatars/participant00.svg
-  SvgGenImage get participant00 => const SvgGenImage('assets/avatars/participant00.svg');
+  SvgGenImage get participant00 =>
+      const SvgGenImage('assets/avatars/participant00.svg');
 
   /// File path: assets/avatars/participant01.svg
-  SvgGenImage get participant01 => const SvgGenImage('assets/avatars/participant01.svg');
+  SvgGenImage get participant01 =>
+      const SvgGenImage('assets/avatars/participant01.svg');
 
   /// File path: assets/avatars/participant02.svg
-  SvgGenImage get participant02 => const SvgGenImage('assets/avatars/participant02.svg');
+  SvgGenImage get participant02 =>
+      const SvgGenImage('assets/avatars/participant02.svg');
 
   /// File path: assets/avatars/participant03.svg
-  SvgGenImage get participant03 => const SvgGenImage('assets/avatars/participant03.svg');
+  SvgGenImage get participant03 =>
+      const SvgGenImage('assets/avatars/participant03.svg');
 
   /// File path: assets/avatars/participant04.svg
-  SvgGenImage get participant04 => const SvgGenImage('assets/avatars/participant04.svg');
+  SvgGenImage get participant04 =>
+      const SvgGenImage('assets/avatars/participant04.svg');
 
   /// File path: assets/avatars/participant05.svg
-  SvgGenImage get participant05 => const SvgGenImage('assets/avatars/participant05.svg');
+  SvgGenImage get participant05 =>
+      const SvgGenImage('assets/avatars/participant05.svg');
 
   /// File path: assets/avatars/participant06.svg
-  SvgGenImage get participant06 => const SvgGenImage('assets/avatars/participant06.svg');
+  SvgGenImage get participant06 =>
+      const SvgGenImage('assets/avatars/participant06.svg');
 
   /// File path: assets/avatars/participant07.svg
-  SvgGenImage get participant07 => const SvgGenImage('assets/avatars/participant07.svg');
+  SvgGenImage get participant07 =>
+      const SvgGenImage('assets/avatars/participant07.svg');
 
   /// File path: assets/avatars/participant08.svg
-  SvgGenImage get participant08 => const SvgGenImage('assets/avatars/participant08.svg');
+  SvgGenImage get participant08 =>
+      const SvgGenImage('assets/avatars/participant08.svg');
 
   /// File path: assets/avatars/participant09.svg
-  SvgGenImage get participant09 => const SvgGenImage('assets/avatars/participant09.svg');
+  SvgGenImage get participant09 =>
+      const SvgGenImage('assets/avatars/participant09.svg');
 
   /// File path: assets/avatars/participant10.svg
-  SvgGenImage get participant10 => const SvgGenImage('assets/avatars/participant10.svg');
+  SvgGenImage get participant10 =>
+      const SvgGenImage('assets/avatars/participant10.svg');
 
   /// File path: assets/avatars/participant11.svg
-  SvgGenImage get participant11 => const SvgGenImage('assets/avatars/participant11.svg');
+  SvgGenImage get participant11 =>
+      const SvgGenImage('assets/avatars/participant11.svg');
 
   /// File path: assets/avatars/participant12.svg
-  SvgGenImage get participant12 => const SvgGenImage('assets/avatars/participant12.svg');
+  SvgGenImage get participant12 =>
+      const SvgGenImage('assets/avatars/participant12.svg');
 
   /// File path: assets/avatars/participant13.svg
-  SvgGenImage get participant13 => const SvgGenImage('assets/avatars/participant13.svg');
+  SvgGenImage get participant13 =>
+      const SvgGenImage('assets/avatars/participant13.svg');
 
   /// File path: assets/avatars/participant14.svg
-  SvgGenImage get participant14 => const SvgGenImage('assets/avatars/participant14.svg');
+  SvgGenImage get participant14 =>
+      const SvgGenImage('assets/avatars/participant14.svg');
 
   /// File path: assets/avatars/participant15.svg
-  SvgGenImage get participant15 => const SvgGenImage('assets/avatars/participant15.svg');
+  SvgGenImage get participant15 =>
+      const SvgGenImage('assets/avatars/participant15.svg');
 
   /// File path: assets/avatars/participant16.svg
-  SvgGenImage get participant16 => const SvgGenImage('assets/avatars/participant16.svg');
+  SvgGenImage get participant16 =>
+      const SvgGenImage('assets/avatars/participant16.svg');
 
   /// File path: assets/avatars/participant17.svg
-  SvgGenImage get participant17 => const SvgGenImage('assets/avatars/participant17.svg');
+  SvgGenImage get participant17 =>
+      const SvgGenImage('assets/avatars/participant17.svg');
 
   /// File path: assets/avatars/participant18.svg
-  SvgGenImage get participant18 => const SvgGenImage('assets/avatars/participant18.svg');
+  SvgGenImage get participant18 =>
+      const SvgGenImage('assets/avatars/participant18.svg');
 
   /// File path: assets/avatars/participant19.svg
-  SvgGenImage get participant19 => const SvgGenImage('assets/avatars/participant19.svg');
+  SvgGenImage get participant19 =>
+      const SvgGenImage('assets/avatars/participant19.svg');
 
   /// List of all assets
   List<SvgGenImage> get values => [
@@ -112,10 +132,14 @@ class $AssetsImagesGen {
   $AssetsImagesPublicGen get public => const $AssetsImagesPublicGen();
 }
 
-class $LibJsServiceEncointerGen {
-  const $LibJsServiceEncointerGen();
+class $JsServiceEncointerDistGen {
+  const $JsServiceEncointerDistGen();
 
-  $LibJsServiceEncointerDistGen get dist => const $LibJsServiceEncointerDistGen();
+  /// File path: js_service_encointer/dist/main.js
+  String get main => 'js_service_encointer/dist/main.js';
+
+  /// List of all assets
+  List<String> get values => [main];
 }
 
 class $AssetsImagesAssetsGen {
@@ -125,34 +149,44 @@ class $AssetsImagesAssetsGen {
   AssetGenImage get ert => const AssetGenImage('assets/images/assets/ERT.png');
 
   /// File path: assets/images/assets/assets_down.png
-  AssetGenImage get assetsDown => const AssetGenImage('assets/images/assets/assets_down.png');
+  AssetGenImage get assetsDown =>
+      const AssetGenImage('assets/images/assets/assets_down.png');
 
   /// File path: assets/images/assets/assets_fail.png
-  AssetGenImage get assetsFail => const AssetGenImage('assets/images/assets/assets_fail.png');
+  AssetGenImage get assetsFail =>
+      const AssetGenImage('assets/images/assets/assets_fail.png');
 
   /// File path: assets/images/assets/assets_nav_0.png
-  AssetGenImage get assetsNav0 => const AssetGenImage('assets/images/assets/assets_nav_0.png');
+  AssetGenImage get assetsNav0 =>
+      const AssetGenImage('assets/images/assets/assets_nav_0.png');
 
   /// File path: assets/images/assets/assets_receive.png
-  AssetGenImage get assetsReceive => const AssetGenImage('assets/images/assets/assets_receive.png');
+  AssetGenImage get assetsReceive =>
+      const AssetGenImage('assets/images/assets/assets_receive.png');
 
   /// File path: assets/images/assets/assets_send.png
-  AssetGenImage get assetsSend => const AssetGenImage('assets/images/assets/assets_send.png');
+  AssetGenImage get assetsSend =>
+      const AssetGenImage('assets/images/assets/assets_send.png');
 
   /// File path: assets/images/assets/assets_up.png
-  AssetGenImage get assetsUp => const AssetGenImage('assets/images/assets/assets_up.png');
+  AssetGenImage get assetsUp =>
+      const AssetGenImage('assets/images/assets/assets_up.png');
 
   /// File path: assets/images/assets/icon_leu.svg
-  SvgGenImage get iconLeu => const SvgGenImage('assets/images/assets/icon_leu.svg');
+  SvgGenImage get iconLeu =>
+      const SvgGenImage('assets/images/assets/icon_leu.svg');
 
   /// File path: assets/images/assets/logo.png
-  AssetGenImage get logo => const AssetGenImage('assets/images/assets/logo.png');
+  AssetGenImage get logo =>
+      const AssetGenImage('assets/images/assets/logo.png');
 
   /// File path: assets/images/assets/mosaic_background.png
-  AssetGenImage get mosaicBackground => const AssetGenImage('assets/images/assets/mosaic_background.png');
+  AssetGenImage get mosaicBackground =>
+      const AssetGenImage('assets/images/assets/mosaic_background.png');
 
   /// File path: assets/images/assets/success.png
-  AssetGenImage get success => const AssetGenImage('assets/images/assets/success.png');
+  AssetGenImage get success =>
+      const AssetGenImage('assets/images/assets/success.png');
 
   /// List of all assets
   List<dynamic> get values => [
@@ -177,61 +211,80 @@ class $AssetsImagesPublicGen {
   AssetGenImage get app => const AssetGenImage('assets/images/public/app.png');
 
   /// File path: assets/images/public/copy.png
-  AssetGenImage get copy => const AssetGenImage('assets/images/public/copy.png');
+  AssetGenImage get copy =>
+      const AssetGenImage('assets/images/public/copy.png');
 
   /// File path: assets/images/public/kusama.png
-  AssetGenImage get kusama => const AssetGenImage('assets/images/public/kusama.png');
+  AssetGenImage get kusama =>
+      const AssetGenImage('assets/images/public/kusama.png');
 
   /// File path: assets/images/public/kusama_gray.png
-  AssetGenImage get kusamaGray => const AssetGenImage('assets/images/public/kusama_gray.png');
+  AssetGenImage get kusamaGray =>
+      const AssetGenImage('assets/images/public/kusama_gray.png');
 
   /// File path: assets/images/public/logo_about.png
-  AssetGenImage get logoAbout => const AssetGenImage('assets/images/public/logo_about.png');
+  AssetGenImage get logoAbout =>
+      const AssetGenImage('assets/images/public/logo_about.png');
 
   /// File path: assets/images/public/nctr-cln-dev.png
-  AssetGenImage get nctrClnDev => const AssetGenImage('assets/images/public/nctr-cln-dev.png');
+  AssetGenImage get nctrClnDev =>
+      const AssetGenImage('assets/images/public/nctr-cln-dev.png');
 
   /// File path: assets/images/public/nctr-cln-dev_gray.png
-  AssetGenImage get nctrClnDevGray => const AssetGenImage('assets/images/public/nctr-cln-dev_gray.png');
+  AssetGenImage get nctrClnDevGray =>
+      const AssetGenImage('assets/images/public/nctr-cln-dev_gray.png');
 
   /// File path: assets/images/public/nctr-cln.png
-  AssetGenImage get nctrCln => const AssetGenImage('assets/images/public/nctr-cln.png');
+  AssetGenImage get nctrCln =>
+      const AssetGenImage('assets/images/public/nctr-cln.png');
 
   /// File path: assets/images/public/nctr-cln_gray.png
-  AssetGenImage get nctrClnGray => const AssetGenImage('assets/images/public/nctr-cln_gray.png');
+  AssetGenImage get nctrClnGray =>
+      const AssetGenImage('assets/images/public/nctr-cln_gray.png');
 
   /// File path: assets/images/public/nctr-gsl-dev.png
-  AssetGenImage get nctrGslDev => const AssetGenImage('assets/images/public/nctr-gsl-dev.png');
+  AssetGenImage get nctrGslDev =>
+      const AssetGenImage('assets/images/public/nctr-gsl-dev.png');
 
   /// File path: assets/images/public/nctr-gsl-dev_gray.png
-  AssetGenImage get nctrGslDevGray => const AssetGenImage('assets/images/public/nctr-gsl-dev_gray.png');
+  AssetGenImage get nctrGslDevGray =>
+      const AssetGenImage('assets/images/public/nctr-gsl-dev_gray.png');
 
   /// File path: assets/images/public/nctr-gsl.png
-  AssetGenImage get nctrGsl => const AssetGenImage('assets/images/public/nctr-gsl.png');
+  AssetGenImage get nctrGsl =>
+      const AssetGenImage('assets/images/public/nctr-gsl.png');
 
   /// File path: assets/images/public/nctr-gsl_gray.png
-  AssetGenImage get nctrGslGray => const AssetGenImage('assets/images/public/nctr-gsl_gray.png');
+  AssetGenImage get nctrGslGray =>
+      const AssetGenImage('assets/images/public/nctr-gsl_gray.png');
 
   /// File path: assets/images/public/nctr-k.png
-  AssetGenImage get nctrK => const AssetGenImage('assets/images/public/nctr-k.png');
+  AssetGenImage get nctrK =>
+      const AssetGenImage('assets/images/public/nctr-k.png');
 
   /// File path: assets/images/public/nctr-k_gray.png
-  AssetGenImage get nctrKGray => const AssetGenImage('assets/images/public/nctr-k_gray.png');
+  AssetGenImage get nctrKGray =>
+      const AssetGenImage('assets/images/public/nctr-k_gray.png');
 
   /// File path: assets/images/public/nctr-r.png
-  AssetGenImage get nctrR => const AssetGenImage('assets/images/public/nctr-r.png');
+  AssetGenImage get nctrR =>
+      const AssetGenImage('assets/images/public/nctr-r.png');
 
   /// File path: assets/images/public/nctr-r_gray.png
-  AssetGenImage get nctrRGray => const AssetGenImage('assets/images/public/nctr-r_gray.png');
+  AssetGenImage get nctrRGray =>
+      const AssetGenImage('assets/images/public/nctr-r_gray.png');
 
   /// File path: assets/images/public/polkadot.png
-  AssetGenImage get polkadot => const AssetGenImage('assets/images/public/polkadot.png');
+  AssetGenImage get polkadot =>
+      const AssetGenImage('assets/images/public/polkadot.png');
 
   /// File path: assets/images/public/polkadot_gray.png
-  AssetGenImage get polkadotGray => const AssetGenImage('assets/images/public/polkadot_gray.png');
+  AssetGenImage get polkadotGray =>
+      const AssetGenImage('assets/images/public/polkadot_gray.png');
 
   /// File path: assets/images/public/polkadot_greyed_out.png
-  AssetGenImage get polkadotGreyedOut => const AssetGenImage('assets/images/public/polkadot_greyed_out.png');
+  AssetGenImage get polkadotGreyedOut =>
+      const AssetGenImage('assets/images/public/polkadot_greyed_out.png');
 
   /// List of all assets
   List<AssetGenImage> get values => [
@@ -258,24 +311,16 @@ class $AssetsImagesPublicGen {
       ];
 }
 
-class $LibJsServiceEncointerDistGen {
-  const $LibJsServiceEncointerDistGen();
-
-  /// File path: lib/js_service_encointer/dist/main.js
-  String get main => 'lib/js_service_encointer/dist/main.js';
-
-  /// List of all assets
-  List<String> get values => [main];
-}
-
 class Assets {
   Assets._();
 
   static const $AssetsAvatarsGen avatars = $AssetsAvatarsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
   static const SvgGenImage nctrLogo = SvgGenImage('assets/nctr_logo.svg');
-  static const SvgGenImage nctrLogoFacesOnlyThick = SvgGenImage('assets/nctr_logo_faces_only_thick.svg');
-  static const $LibGen lib = $LibGen();
+  static const SvgGenImage nctrLogoFacesOnlyThick =
+      SvgGenImage('assets/nctr_logo_faces_only_thick.svg');
+  static const $JsServiceEncointerGen jsServiceEncointer =
+      $JsServiceEncointerGen();
 
   /// List of all assets
   List<SvgGenImage> get values => [nctrLogo, nctrLogoFacesOnlyThick];
