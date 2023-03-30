f73c0fceae8017a170df6da89acd174ecf63d7b9
clangenb <37865735+clangenb@users.noreply.github.com>
2023-03-30 16:43:06 +0200

    [IOS] delete ios because it is not needed in fdroid and there are always conflicts when merging master

--- ios/Runner/Assets.xcassets/LaunchImage.imageset/resize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-            
-convert encointer_logo_nolatin_800px.png -resize 144 LaunchImage.png  
-convert encointer_logo_nolatin_800px.png -resize 288 LaunchImage@2x.png  
-convert encointer_logo_nolatin_800px.png -resize 552 LaunchImage@3x.png  
-
