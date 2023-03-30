f47bfcba4b62a090504d71d1422c66f47264385c
clangenb <37865735+clangenb@users.noreply.github.com>
2023-03-30 13:37:16 +0200

    fix encointer client docker on linux.

--- scripts/docker_run_encointer_client_bootstrap_demo_community.sh
+++ scripts/docker_run_encointer_client_bootstrap_demo_community.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker run encointer/encointer-client-notee:0.0.2 bootstrap_demo_community.py -u ws://host.docker.internal -p 9944
+# `--add-host host.docker.internal:host-gateway` is only needed for linux
+docker run --add-host host.docker.internal:host-gateway encointer/encointer-client-notee:0.0.2 bootstrap_demo_community.py -u ws://host.docker.internal -p 9944
