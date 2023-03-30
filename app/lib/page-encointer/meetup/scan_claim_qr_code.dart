b9baaed21cd79641d0774be72af214f1a626cd4c
clangenb <37865735+clangenb@users.noreply.github.com>
2023-03-30 17:01:22 +0200

    fix merge errors

--- app/lib/page-encointer/meetup/scan_claim_qr_code.dart
+++ app/lib/page-encointer/meetup/scan_claim_qr_code.dart
@@ -1,4 +1,3 @@
-import 'package:encointer_wallet/utils/format.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
