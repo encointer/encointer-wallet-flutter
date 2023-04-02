7764e6ec171a4396b64aa564e0a311f8648c8c8f
clangenb <37865735+clangenb@users.noreply.github.com>
2023-04-02 12:30:02 +0200

    [ew_storage] move both storages in a storage submodule.

--- packages/ew_storage/lib/ew_storage.dart
+++ packages/ew_storage/lib/ew_storage.dart
@@ -3,4 +3,4 @@ library ew_storage;
 
 export 'src/interface/secure_storage_interface.dart';
 export 'src/interface/storage_exception.dart';
-export 'src/secure_storage.dart';
+export 'src/storages/secure_storage.dart';
