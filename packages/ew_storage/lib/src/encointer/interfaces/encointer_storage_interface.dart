547c21992e662ff1673a901226a814ffb3bf2e6a
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2023-04-11 20:15:41 +0600

    Create `ew_storage` package (#1042)
    * created ew_storage package
    
    * added ew_storage
    
    * fix PreferencesStorage constructors
    
    * write unit test for ew_storage
    
    * added T params for read and write
    
    * format code
    
    * format code
    
    * simplified ew_storage
    
    * Documentation has been improved ew_storage
    
    * write README.md file (ew_storage)
    
    * fixed by comment
    
    * deleted Storage abstract class
    
    * created two abstract class
    
    * fix exports
    
    * added PreferencesStorage abstarct class
    
    * changed pref storage
    
    * changed folder
    
    * fix some async functions
    
    * Update local_storage.dart
    
    * deleted messed comment
    
    * rename isTest to mockLocalStorage
    
    * rename interface classes
    
    * try fix
    
    * renaming files by comment
    
    * added StorageInterfaceSyncRead
    
    * fix imports
    
    * remove const type
    
    * added proferences storage
    
    * try fix
    
    * try fix async methods
    
    * fix get methods
    
    * fix set methods
    
    * try fix mock local storage
    
    * added getValueJsonDecodeCompute, setValueJsonEncodeCompute methods
    
    * ew_storage feedback (#1104)
    
    * [ew_storage] split `SplitEncointerStorageInterface` into `StorageExtension` and `EncointerStorageInterface`
    
    * [ew_storage] rename `StorageInterfaceSyncRead` to `SyncReadStorageInterface`
    
    * [ew_storage] move both storages in a storage submodule.
    
    * move exception out of interface, it does not belong there
    
    * [ew_storage] rename `StorageExtensions` to `SyncStorage`
    
    * [ew_storage] create submodule base storages and move interfaces into it.
    
    * [ew_storage] moved encointer into it's own subpackage
    
    * [ew_storage] moved encointer into it's own subpackage
    
    * [ew_storage] rename file for encointer -> `encointer_storage_interface`
    
    * [ew_storage] add generics
    
    * [ew_storage] remove needles `extends SyncStorage` for mock
    
    * Revert "[ew_storage] remove needles `extends SyncStorage` for mock"
    
    This reverts commit b6b338bbfed894cd6b6e30027633706f2c38a6b4.
    
    * Revert "Revert "[ew_storage] remove needles `extends SyncStorage` for mock""
    
    This reverts commit 3c7067f93ac32ab15bfc47244b303f7bb976e593.
    
    * [ew_storage] rename `MockLocalStorage` to `EncointerMockStorage`
    
    * [ew_storage] move exception to base_storage
    
    * [ew_storage] rename `base_storage` to `storages`
    
    * [ew_storage] add newline at the end of the file
    
    * format code
    
    * added compute some places
    
    * community method to account mehtods and other methods
    
    ---------
    
    Co-authored-by: AZ Ackmatoff <ackmatoff@gmail.com>
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

---
+++ packages/ew_storage/lib/src/encointer/interfaces/encointer_storage_interface.dart
@@ -0,0 +1,25 @@
+abstract class EncointerStorageInterface {
+  // ----------- account methods --------------
+  String? getCurrentAccount();
+  Future<void> setCurrentAccount(String pubKey);
+  Future<void> removeAccount(String pubKey);
+
+  List<Map<String, dynamic>> getAccountList();
+  Future<void> addAccount(Map<String, dynamic> acc);
+
+  Object? getAccountCache(String? accPubKey, String key);
+  Future<void> setAccountCache(String accPubKey, String key, Object? value);
+
+  Map<String, dynamic>? getSeeds(String seedType);
+
+  // ----------- contact methods --------------
+  List<Map<String, dynamic>> getContactList();
+
+  Future<void> addContact(Map<String, dynamic> contact);
+  Future<void> removeContact(String address);
+  Future<void> updateContact(Map<String, dynamic> con);
+
+  // ----------- other methods --------------
+  // Check can we delete this method
+  Future<void> setLocale([String languageCode = 'en']);
+}
