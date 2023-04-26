10c6e967481cafff1606137c70b6906163554506
azackmatoff <ackmatoff@gmail.com>
2023-04-26 16:36:47 +0600

    comments are added

--- app/android/app/src/main/kotlin/org/encointer/wallet/MainActivity.kt
+++ app/android/app/src/main/kotlin/org/encointer/wallet/MainActivity.kt
@@ -9,11 +9,11 @@ import com.jakewharton.threetenabp.AndroidThreeTen
 class MainActivity: FlutterActivity() {
   // You do not need to override onCreate() in order to invoke
   // GeneratedPluginRegistrant. Flutter now does that on your behalf.
-
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        // Initialize AndroidThreeTen for older versions of android instead
+        // of java.time, in API 26 or newer versions java.time is used
+        // older versions will use AndroidThreeTen
         AndroidThreeTen.init(this);
     }
 }
-
-
