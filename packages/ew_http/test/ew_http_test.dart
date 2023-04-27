74b1442360ee3563be8c79d67a1d48a02d52abfc
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-04-27 23:17:56 +0600

    refactor test

--- packages/ew_http/test/ew_http_test.dart
+++ packages/ew_http/test/ew_http_test.dart
@@ -14,21 +14,26 @@ void main() {
     ewHttp = EwHttp();
   });
 
-  group('EwHttp', () {
-    test('get', () async {
-      final value = await ewHttp.get<Map<String, dynamic>>(getUrl);
-      expect(value, isNotNull);
-      expect(value, isMap);
-      expect(value, isA<Map<String, dynamic>>());
+  group('EwHttp `get`, `getType`, `getListType`', () {
+    test('Get', () async {
+      final mapValue = await ewHttp.get<Map<String, dynamic>>(getUrl);
+      expect(mapValue, isNotNull);
+      expect(mapValue, isMap);
+      expect(mapValue, isA<Map<String, dynamic>>());
+
+      final listValue = await ewHttp.get<List<dynamic>>(getListUrl);
+      expect(listValue, isNotNull);
+      expect(listValue, isList);
+      expect(listValue, isA<List<dynamic>>());
     });
 
-    test('get List Type', () async {
+    test('Get Type', () async {
       final value = await ewHttp.getType<Feed>(getUrl, fromJson: Feed.fromJson);
       expect(value, isNotNull);
       expect(value, isA<Feed>());
     });
 
-    test('getType', () async {
+    test('Get List Type', () async {
       final value = await ewHttp.getTypeList<Feed>(getListUrl, fromJson: Feed.fromJson);
       expect(value, isNotNull);
       expect(value, isList);
