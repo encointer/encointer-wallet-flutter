315f00f7a1c6cbf1afac7c627df58a5e6d25b663
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-05-01 06:46:22 +0600

    rebuild TransferHistoryStore

--- app/lib/modules/transfer/logic/transfer_gistory_store.g.dart
+++ app/lib/modules/transfer/logic/transfer_gistory_store.g.dart
@@ -9,18 +9,18 @@ part of 'transfer_gistory_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$TransferHistoryStore on _TransferHistoryStoreBase, Store {
-  late final _$transactionsAtom = Atom(name: '_TransferHistoryStoreBase.transactions', context: context);
+  late final _$fetchStatusAtom = Atom(name: '_TransferHistoryStoreBase.fetchStatus', context: context);
 
   @override
-  List<Transaction>? get transactions {
-    _$transactionsAtom.reportRead();
-    return super.transactions;
+  FetchStatus get fetchStatus {
+    _$fetchStatusAtom.reportRead();
+    return super.fetchStatus;
   }
 
   @override
-  set transactions(List<Transaction>? value) {
-    _$transactionsAtom.reportWrite(value, super.transactions, () {
-      super.transactions = value;
+  set fetchStatus(FetchStatus value) {
+    _$fetchStatusAtom.reportWrite(value, super.fetchStatus, () {
+      super.fetchStatus = value;
     });
   }
 
@@ -34,7 +34,7 @@ mixin _$TransferHistoryStore on _TransferHistoryStoreBase, Store {
   @override
   String toString() {
     return '''
-transactions: ${transactions}
+fetchStatus: ${fetchStatus}
     ''';
   }
 }
