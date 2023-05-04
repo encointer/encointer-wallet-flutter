ab36a082db19f4cc804c5a8883b5c906ae14a13e
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-05-04 06:39:25 +0600

    added documantation to transfer history model

--- app/lib/modules/transfer/transfer.dart
+++ app/lib/modules/transfer/transfer.dart
@@ -2,3 +2,4 @@ export 'logic/transfer_gistory_store.dart';
 export 'view/transfer_history_view.dart';
 export 'widgets/transaction_card.dart';
 export 'widgets/transactions_empty.dart';
+export 'widgets/transactions_list.dart';
