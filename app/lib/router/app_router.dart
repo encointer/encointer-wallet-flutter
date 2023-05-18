1e52a5213bb6f8f6eac47d15d4ea0770e9226f32
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2023-05-18 11:41:43 +0600

    Display transaction history (#1176)
    * added repository provider
    
    * added nullable to ew http
    
    * wrapp main app with repository provider
    
    * change
    
    * change Transaction model by backend
    
    * add ewhttp to TransferHistoryStore
    
    * rebuild TransferHistoryStore
    
    * set real endpoint
    
    * delete mock transaction history data
    
    * added test link
    
    * refactoring code
    
    * added empty transaction view and error view
    
    * added documantation to transfer history model
    
    * set start and end time to getTransactionHistoryUrl
    
    * refactor getTransactionHistoryUrl
    
    * fix trabsaltion by commet
    
    * added Retry translations
    
    * Integration `EwHttp` (#1177)
    
    * integrated EwHttp to IpfsApi
    
    * dleted unused SubscanApi and fix tests
    
    * integration EwHttp to getMeetupTimeOverride
    
    * use EwHttp instead FeedRepo
    
    * added const to Api constructor
    
    * deleted http from app pubspec
    
    * fix file name
    
    * delete unused g file
    
    * change rename file htt_overrides
    
    * fix Api.create parameters
    
    * try get name
    
    * added getNameFromContacts to Transaction
    
    * rename TransferHistoryViewStore
    
    * deleted FetchStatus initial

--- app/lib/router/app_router.dart
+++ app/lib/router/app_router.dart
@@ -1,9 +1,12 @@
-import 'package:encointer_wallet/store/account/types/account_data.dart';
+import 'package:ew_http/ew_http.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import 'package:encointer_wallet/models/location/location.dart';
 import 'package:encointer_wallet/modules/modules.dart';
+import 'package:encointer_wallet/store/app.dart';
+import 'package:encointer_wallet/store/account/types/account_data.dart';
+import 'package:encointer_wallet/utils/repository_provider.dart';
 import 'package:encointer_wallet/page-encointer/bazaar/0_main/bazaar_main.dart';
 import 'package:encointer_wallet/page-encointer/common/community_chooser_on_map.dart';
 import 'package:encointer_wallet/page-encointer/home_page.dart';
@@ -191,15 +194,14 @@ class AppRoute {
       case TransferHistoryView.route:
         return CupertinoPageRoute(
           builder: (_) => Provider(
-            create: (context) => TransferHistoryStore()..getTransfers(),
+            create: (context) => TransferHistoryViewStore(
+              RepositoryProvider.of<EwHttp>(context),
+            )..getTransfers(context.read<AppStore>()),
             child: const TransferHistoryView(),
           ),
-          settings: settings,
         );
       default:
-        throw Exception(
-          'no builder specified for route named: [${settings.name}]',
-        );
+        throw Exception('no builder specified for route named: [${settings.name}]');
     }
   }
 }
