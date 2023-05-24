71e2bcd368e6827f08c1d6930a536d4f1df0df82
Eldiiar Almazbek <76556278+Eldar2021@users.noreply.github.com>
2023-05-24 14:59:15 +0600

    Try fix build runner error in unit test (#1226)
    * try fix buil runner error in unit test
    
    * fix documantation

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -3,9 +3,12 @@ import 'package:upgrader/upgrader.dart';
 class AppConfig {
   const AppConfig({this.isIntegrationTest = false, this.appCast});
 
-  /// [appCast] is used to provide fake information about the app version for `Upgrader` package.
+  /// [appCast] is used to provide fake information about the app version for the `Upgrader` package.
   final AppcastConfiguration? appCast;
 
-  /// If [isIntegrationTest] value is `true`, test will close upgrader alert and won't ask notifications permission.
+  /// If the [isIntegrationTest] value is `true`, the following conditions will occur:
+  /// 1) The test will close the Upgrader alert.
+  /// 2) The app won't ask for notification permissions.
+  /// 3) The app will show the full `acoountPubKey` in the `AccountManage` page.
   final bool isIntegrationTest;
 }
