fd9fcda02ff02318da00315c40cc2eacb55f3db0
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-05-25 10:29:40 +0600

    try fix multi panguage screenshot ci test sh file

--- scripts/multi_language_screenshot_ci_test.sh
+++ scripts/multi_language_screenshot_ci_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-melos doctor
-
 ./flutterw pub global run melos integration-scan-test-ios
-./flutterw pub global run melos integration-app-test-ios-screenshot
+./flutterw pub global run melos integration-app-test-ios-screenshot-multi-language
