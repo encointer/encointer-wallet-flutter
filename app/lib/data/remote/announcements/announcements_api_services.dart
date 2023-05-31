8be523d780cc21b669f8cf5fbe13020ddfefd401
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-05-31 17:21:32 +0600

    deleted unused code

--- app/lib/data/remote/announcements/announcements_api_services.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:encointer_wallet/data/common_services/models/network/api_response.dart';
-
-// abstract class AnnouncementsApiServices {
-//   Future<ApiResponse> getCommunityAnnouncements({String? cid});
-//   Future<ApiResponse> getGlobalAnnouncements();
-// }
