c933753ccae72c4a90f03d2699c1c08c9c622bca
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-06-07 16:22:37 +0600

    try fix 01

--- app/lib/page-encointer/new_bazaar/widgets/businesses_card.dart
+++ app/lib/page-encointer/new_bazaar/widgets/businesses_card.dart
@@ -3,68 +3,46 @@ import 'package:flutter/material.dart';
 import 'package:encointer_wallet/models/bazaar/businesses.dart';
 
 class BusinessesCard extends StatelessWidget {
-  const BusinessesCard({
-    super.key,
-    required this.businesses,
-  });
+  const BusinessesCard({super.key, required this.businesses});
 
   final Businesses businesses;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Padding(
-      padding: const EdgeInsets.all(5),
-      child: Card(
-        color: businesses.status != null ? businesses.statusColor : const Color(0xFFf4f7f8),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: SizedBox(
-          height: 140,
-          child: Row(
-            children: [
-              SizedBox(
-                width: 140,
-                height: 140,
-                child: Image.network(
-                  businesses.photo,
-                  fit: BoxFit.cover,
-                ),
+    return Card(
+      margin: const EdgeInsets.only(top: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(businesses.photo, fit: BoxFit.fitHeight),
+          Expanded(
+            child: ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child: Text(businesses.category.name, style: textTheme.bodySmall)),
+                  Text(
+                    businesses.status?.name ?? '',
+                    style: textTheme.bodySmall!.copyWith(color: businesses.status?.textColor ?? Colors.black),
+                  )
+                ],
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: ListTile(
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(businesses.category.name, style: textTheme.bodySmall),
-                          Text(
-                            businesses.status?.name ?? '',
-                            style: textTheme.bodySmall!.copyWith(color: businesses.status?.textColor ?? Colors.black),
-                          )
-                        ],
-                      ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8),
-                          child: Text(businesses.name, style: textTheme.labelLarge),
-                        ),
-                        Text(businesses.description, style: textTheme.bodyMedium),
-                      ],
-                    ),
-                  ),
-                ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 28),
+                  Text(businesses.name, style: textTheme.labelLarge),
+                  const SizedBox(height: 8),
+                  Text(businesses.description, style: textTheme.bodyMedium),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
