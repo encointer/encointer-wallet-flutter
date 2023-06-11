50b04a2b10ad03b3aff87dbf11eef881717f999e
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-06-11 11:19:29 +0600

    Tested on Pizex 4

--- app/lib/page-encointer/new_bazaar/widgets/businesses_card.dart
+++ app/lib/page-encointer/new_bazaar/widgets/businesses_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:encointer_wallet/models/bazaar/businesses.dart';
+import 'package:encointer_wallet/theme/theme.dart';
 
 class BusinessesCard extends StatelessWidget {
   const BusinessesCard({super.key, required this.businesses});
@@ -9,21 +10,27 @@ class BusinessesCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final textTheme = context.textTheme;
     return SizedBox(
       height: 160,
       child: Card(
         margin: const EdgeInsets.only(top: 10),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(
-              businesses.photo,
-              fit: BoxFit.fitHeight,
-              height: double.infinity,
+            DecoratedBox(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(businesses.photo),
+                ),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: const SizedBox(height: double.infinity, width: 130),
             ),
             Expanded(
               child: ListTile(
