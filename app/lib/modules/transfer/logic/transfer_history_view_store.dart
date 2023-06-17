0641be40904307c56821bfc4514577cf065cab83
Janara Sourbayeva <janara2610@gmail.com>
2023-06-17 10:04:10 +0600

    fixed store

--- app/lib/modules/transfer/logic/transfer_history_view_store.dart
+++ app/lib/modules/transfer/logic/transfer_history_view_store.dart
@@ -3,7 +3,7 @@ import 'package:ew_http/ew_http.dart';
 import 'package:mobx/mobx.dart';
 
 import 'package:encointer_wallet/models/index.dart';
-// import 'package:encointer_wallet/utils/format.dart';
+import 'package:encointer_wallet/utils/format.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/config/consts.dart';
 import 'package:encointer_wallet/utils/fetch_status.dart';
@@ -25,17 +25,13 @@ abstract class _TransferHistoryViewStoreBase with Store {
   @action
   Future<void> getTransfers(AppStore appStore) async {
     fetchStatus = FetchStatus.loading;
-    // final address = Fmt.ss58Encode(
-    //   appStore.account.currentAccountPubKey ?? '',
-    //   prefix: appStore.settings.endpoint.ss58 ?? 42,
-    // );
+    final address = Fmt.ss58Encode(
+      appStore.account.currentAccountPubKey ?? '',
+      prefix: appStore.settings.endpoint.ss58 ?? 42,
+    );
 
-    // final response = await ewHttp.getTypeList<Transaction>(
-    //   getTransactionHistoryUrl(appStore.encointer.community?.cid.toFmtString() ?? '', address),
-    //   fromJson: Transaction.fromJson,
-    // );
     final response = await ewHttp.getTypeList<Transaction>(
-      getTransactionHistoryUrl('u0qj944rhWE', 'HxunEG4C3pzFGMKHYAVt3D6LgCSoLcodReJ4xziFPsNWCew'),
+      getTransactionHistoryUrl(appStore.encointer.community?.cid.toFmtString() ?? '', address),
       fromJson: Transaction.fromJson,
     );
 
