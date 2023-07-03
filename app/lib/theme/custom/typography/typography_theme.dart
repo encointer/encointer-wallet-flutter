7c54fe1168cf9ceb5e42ef2069eb8450ab3e4bf0
Janara Sourbayeva <janara2610@gmail.com>
2023-07-03 17:46:22 +0600

    added TextStyles

--- app/lib/theme/custom/typography/typography_theme.dart
+++ app/lib/theme/custom/typography/typography_theme.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 // mixin TypographyTheme on BuildContext {
 //   ThemeData get theme => Theme.of(this);
 //   TextTheme get textTheme => theme.textTheme;
