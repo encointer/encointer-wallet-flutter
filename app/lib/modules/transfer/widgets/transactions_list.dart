bab7893678961d29faadaabe44149e0c95ca5c3b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-07-05 15:11:46 +0600

    added transferHistoryTop

--- app/lib/modules/transfer/widgets/transactions_list.dart
+++ app/lib/modules/transfer/widgets/transactions_list.dart
@@ -17,18 +17,22 @@ class TransactionsList extends StatelessWidget {
     final appStore = context.watch<AppStore>();
     return ListView.builder(
       padding: const EdgeInsets.fromLTRB(14, 20, 14, 35),
-      itemCount: transactions.length + 1,
+      itemCount: transactions.length + 2,
       itemBuilder: (BuildContext context, int index) {
-        if (index < transactions.length) {
-          final transaction = transactions[index];
+        if (index == 0) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(context.l10n.transferHistoryTop, textAlign: TextAlign.center),
+          );
+        } else if (index <= transactions.length) {
+          final transaction = transactions[index - 1];
           return TransactionCard(transaction, appStore.settings.knownAccounts());
-        } else if (index == transactions.length) {
+        } else {
           return Padding(
             padding: const EdgeInsets.only(top: 10),
             child: Text(context.l10n.transferHistoryEnd, textAlign: TextAlign.center),
           );
         }
-        return const SizedBox();
       },
     );
   }
