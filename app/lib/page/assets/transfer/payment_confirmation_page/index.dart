0f4327945f11ca0fc34c568553988eb26832aa4b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-07-13 12:12:18 +0600

    Remove old package animated_check

--- app/lib/page/assets/transfer/payment_confirmation_page/index.dart
+++ app/lib/page/assets/transfer/payment_confirmation_page/index.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:animated_check/animated_check.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
@@ -11,6 +10,7 @@ import 'package:encointer_wallet/common/components/gradient_elements.dart';
 import 'package:encointer_wallet/theme/theme.dart';
 import 'package:encointer_wallet/config.dart';
 import 'package:encointer_wallet/utils/repository_provider.dart';
+import 'package:encointer_wallet/common/components/animation/animated_check.dart';
 import 'package:encointer_wallet/models/communities/community_identifier.dart';
 import 'package:encointer_wallet/page/assets/transfer/payment_confirmation_page/components/payment_overview.dart';
 import 'package:encointer_wallet/page/assets/transfer/payment_confirmation_page/components/transfer_state.dart';
@@ -192,11 +192,7 @@ class _PaymentConfirmationPageState extends State<PaymentConfirmationPage> with 
 
           return DecoratedBox(
             decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.green),
-            child: AnimatedCheck(
-              progress: _animation!,
-              size: 100,
-              color: Colors.white,
-            ),
+            child: AnimatedCheck(progress: _animation!, size: 100, color: Colors.white),
           );
         }
       case TransferState.failed:
