d015fa38f3b5f6dd73bba25bb16987630ed057b5
clangenb <37865735+clangenb@users.noreply.github.com>
2023-07-14 20:48:10 +0200

    [business_utils] better comment

--- app/lib/page-encointer/new_bazaar/businesses/logic/business_utils.dart
+++ app/lib/page-encointer/new_bazaar/businesses/logic/business_utils.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 class BusinessUtils {
-  /// To display, correctly, german names coming from server
+  /// To correctly display German Umlauts coming from IPFS.
   static String utf8convert(String text) {
     final bytes = text.codeUnits;
     return utf8.decode(bytes);
