6c1ff8a33926cc6e535d2ef70f972cfd800efc47
clangenb <37865735+clangenb@users.noreply.github.com>
2023-07-22 13:02:10 +0200

    [transaction_card] revert change for adding `N/A` if symbol == null.

--- app/lib/modules/transfer/widgets/transaction_card.dart
+++ app/lib/modules/transfer/widgets/transaction_card.dart
@@ -74,7 +74,7 @@ class TransactionCard extends StatelessWidget {
                   TextSpan(
                     children: [
                       TextSpan(
-                        text: '${appStore.encointer.community?.symbol ?? 'N/A'} ',
+                        text: '${appStore.encointer.community?.symbol} ',
                         style: context.textTheme.titleMedium!.copyWith(
                           color: transaction.type == TransactionType.incoming
                               ? context.colorScheme.primary
