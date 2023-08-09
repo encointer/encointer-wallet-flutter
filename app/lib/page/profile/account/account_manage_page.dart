fa7e1df431f367fe03a267744df31409d7db8326
clangenb <37865735+clangenb@users.noreply.github.com>
2023-08-09 20:37:39 +0200

    [profile/account_manage_page] remove unnecessary `SingleChildScrollView`

--- app/lib/page/profile/account/account_manage_page.dart
+++ app/lib/page/profile/account/account_manage_page.dart
@@ -182,42 +182,40 @@ class _AccountManagePageState extends State<AccountManagePage> {
             padding: const EdgeInsets.all(16),
             child: Column(
               children: <Widget>[
-                SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 20),
-                      if (!isKeyboard)
-                        AddressIcon(
-                          addressSS58,
-                          accountToBeEditedPubKey,
-                          size: 130,
-                        ),
-                      Text(
+                Column(
+                  children: <Widget>[
+                    const SizedBox(height: 20),
+                    if (!isKeyboard)
+                      AddressIcon(
                         addressSS58,
-                        key: const Key(EWTestKeys.accountPublicKey),
-                        // Text only read `addressSS58` for integration test
-                        style: const TextStyle(fontSize: 2, color: Colors.transparent),
+                        accountToBeEditedPubKey,
+                        size: 130,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            Fmt.address(addressSS58)!,
-                            style: const TextStyle(fontSize: 20),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          IconButton(
-                            icon: const Icon(Iconsax.copy),
-                            color: context.colorScheme.secondary,
-                            onPressed: () => UI.copyAndNotify(context, addressSS58),
-                          ),
-                        ],
-                      ),
-                      Text(l10n.communities,
-                          style: h3.copyWith(color: AppColors.encointerGrey), textAlign: TextAlign.left),
-                    ],
-                  ),
+                    Text(
+                      addressSS58,
+                      key: const Key(EWTestKeys.accountPublicKey),
+                      // Text only read `addressSS58` for integration test
+                      style: const TextStyle(fontSize: 2, color: Colors.transparent),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          Fmt.address(addressSS58)!,
+                          style: const TextStyle(fontSize: 20),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        IconButton(
+                          icon: const Icon(Iconsax.copy),
+                          color: context.colorScheme.secondary,
+                          onPressed: () => UI.copyAndNotify(context, addressSS58),
+                        ),
+                      ],
+                    ),
+                    Text(l10n.communities,
+                        style: h3.copyWith(color: AppColors.encointerGrey), textAlign: TextAlign.left),
+                  ],
                 ),
                 if (appSettingsStore.developerMode)
                   Expanded(
