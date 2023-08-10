2678e53401ab1bdf6f623238ad5f5ebb23e6eac1
clangenb <37865735+clangenb@users.noreply.github.com>
2023-08-10 09:05:16 +0200

    [models] add faucet model and unit test for parsing

---
+++ app/lib/models/faucet/faucet.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:encointer_wallet/models/communities/community_identifier.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'faucet.g.dart';
+
+// explicit = true as we have nested Json with location
+// field rename such that the fields match the ones defined in the runtime
+@JsonSerializable(explicitToJson: true)
+class Faucet {
+  Faucet(
+    this.name,
+    this.purposeId,
+    this.whitelist,
+    this.dripAmount,
+    this.creator,
+  );
+
+  factory Faucet.fromJson(Map<String, dynamic> json) => _$FaucetFromJson(json);
+  Map<String, dynamic> toJson() => _$FaucetToJson(this);
+
+  /// Name of the faucet.
+  String name;
+
+  /// Onchain purpose identifier of a faucet. The faucet can only be dripped once per
+  /// unique ([cid, cIndex], [purposeId, account]) combination.
+  int purposeId;
+
+  /// Communities whose reputation qualifies for a faucet drip.
+  List<CommunityIdentifier> whitelist;
+
+  /// The amount one gits per faucet drip.
+  int dripAmount;
+
+  /// The creator AccountId/PublicKey of the faucet.
+  String creator;
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+}
---
+++ app/lib/models/faucet/faucet.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'faucet.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Faucet _$FaucetFromJson(Map<String, dynamic> json) => Faucet(
+      json['name'] as String,
+      json['purposeId'] as int,
+      (json['whitelist'] as List<dynamic>).map((e) => CommunityIdentifier.fromJson(e as Map<String, dynamic>)).toList(),
+      json['dripAmount'] as int,
+      json['creator'] as String,
+    );
+
+Map<String, dynamic> _$FaucetToJson(Faucet instance) => <String, dynamic>{
+      'name': instance.name,
+      'purposeId': instance.purposeId,
+      'whitelist': instance.whitelist.map((e) => e.toJson()).toList(),
+      'dripAmount': instance.dripAmount,
+      'creator': instance.creator,
+    };
