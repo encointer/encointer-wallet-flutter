f332aa4f918a1269cc9ef7635bdd2dcfe29a69bf
clangenb <37865735+clangenb@users.noreply.github.com>
2023-08-14 14:38:45 +0200

    [scripts] fix command to run the new encointer node

--- scripts/docker_run_encointer_node_notee.sh
+++ scripts/docker_run_encointer_node_notee.sh
@@ -11,5 +11,4 @@ docker run -p 30333:30333 -p 9944:9944 -p 9933:9933 -p 9615:9615 \
   --enable-offchain-indexing true \
   --rpc-methods unsafe \
   -lencointer=debug,parity_ws=warn \
-  --ws-external \
   --rpc-external \
