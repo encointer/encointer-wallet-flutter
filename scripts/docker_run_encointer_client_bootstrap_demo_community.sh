2fa55e1b21bf47018fb350bdfe8fa779292925aa
clangenb <37865735+clangenb@users.noreply.github.com>
2023-08-14 14:16:34 +0200

    [scripts] print docker tag in scripts

--- scripts/docker_run_encointer_client_bootstrap_demo_community.sh
+++ scripts/docker_run_encointer_client_bootstrap_demo_community.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 DOCKER_TAG=${1:-1.5.1}
 
+echo "Encointer client docker tag: ${DOCKER_TAG}"
 
 # `--add-host host.docker.internal:host-gateway` is only needed for linux
 docker run --add-host host.docker.internal:host-gateway encointer/encointer-client-notee:${DOCKER_TAG} bootstrap_demo_community.py -u ws://host.docker.internal -p 9944
--- scripts/docker_run_encointer_node_notee.sh
+++ scripts/docker_run_encointer_node_notee.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 DOCKER_TAG=${1:-1.5.1}
 
+echo "Encointer node docker tag: ${DOCKER_TAG}"
+
 docker run -p 30333:30333 -p 9944:9944 -p 9933:9933 -p 9615:9615 \
   encointer/encointer-node-notee:${DOCKER_TAG} \
   --dev \
