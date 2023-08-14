0d92294d9cf5f2ce3c582042bfefdb524246d214
clangenb <37865735+clangenb@users.noreply.github.com>
2023-08-14 14:13:47 +0200

    [GHA] run integration tests also against the new node

--- scripts/docker_run_encointer_node_notee.sh
+++ scripts/docker_run_encointer_node_notee.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
+DOCKER_TAG=${1:-1.5.1}
+
 docker run -p 30333:30333 -p 9944:9944 -p 9933:9933 -p 9615:9615 \
-  encointer/encointer-node-notee:0.0.2 \
+  encointer/encointer-node-notee:${DOCKER_TAG} \
   --dev \
   --enable-offchain-indexing true \
   --rpc-methods unsafe \
