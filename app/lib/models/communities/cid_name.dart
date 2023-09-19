7f93c2cd1cceea7e17989ced4d650403ee4c32e2
clangenb <37865735+clangenb@users.noreply.github.com>
2023-09-19 15:00:04 +0200

    [EncointerApi] fix parsing of Name in CidNames

--- app/lib/models/communities/cid_name.dart
+++ app/lib/models/communities/cid_name.dart
@@ -10,7 +10,10 @@ part 'cid_name.g.dart';
 class CidName {
   CidName(this.cid, this.name);
 
-  factory CidName.fromJson(Map<String, dynamic> json) => _$CidNameFromJson(json);
+  factory CidName.fromJson(Map<String, dynamic> json) =>CidName(
+    CommunityIdentifier.fromJson(json['cid'] as Map<String, dynamic>),
+    utf8.decode(parseName(json['name'] as String)),
+  );
   Map<String, dynamic> toJson() => _$CidNameToJson(this);
 
   CommunityIdentifier cid;
@@ -20,4 +23,23 @@ class CidName {
   String toString() {
     return jsonEncode(this);
   }
+}
+
+/// For some reason JS returns: 1,2,4,3,5,6
+///
+/// This function parses the individual numbers into a List<int>.
+List<int> parseName(String input) {
+  // Split the string by commas
+  final stringValues = input.split(',');
+
+  // Parse and convert to integers
+  final result = <int>[];
+  for (final stringValue in stringValues) {
+    final intValue = int.tryParse(stringValue.trim()); // Trim to remove leading/trailing whitespace
+    if (intValue != null) {
+      result.add(intValue);
+    }
+  }
+
+  return result;
 }
