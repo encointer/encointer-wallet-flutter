7fe06311fc658777d023ad904c46d885d4ff75e5
clangenb <37865735+clangenb@users.noreply.github.com>
2023-09-20 14:44:54 +0200

    [JS] change naming of test

--- app/js_service_encointer/test/utils/utils.test.js
+++ app/js_service_encointer/test/utils/utils.test.js
@@ -6,7 +6,7 @@ import { describe, expect, it } from '@jest/globals';
 import { stringNumberToEncointerBalanceU8 } from '../../src/utils/utils.js';
 
 describe('utils', () => {
-  it('stringBalanceToU8 works', () => {
+  it('stringNumberToEncointerBalanceU8 works', () => {
     const balanceArray = stringNumberToEncointerBalanceU8('128');
     expect(balanceArray).toStrictEqual(new Uint8Array([
       0, 0, 0, 0, 0, 0,
