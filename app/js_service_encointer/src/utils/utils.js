e8cc89c9148894fa3a9a7623286098ab5108c15e
clangenb <37865735+clangenb@users.noreply.github.com>
2023-09-20 14:44:04 +0200

    [JS] update bn to v5 and fix apply updates

--- app/js_service_encointer/src/utils/utils.js
+++ app/js_service_encointer/src/utils/utils.js
@@ -1,12 +1,15 @@
 import { bnToU8a } from '@polkadot/util';
 import { stringToEncointerBalance } from '@encointer/types';
 
-export function applyDemurrage(balanceEntry, latestBlockNumber, demurrageRate) {
+export function applyDemurrage (balanceEntry, latestBlockNumber, demurrageRate) {
   const elapsed = latestBlockNumber - balanceEntry.lastUpdate;
-  const exponent = -demurrageRate * elapsed
+  const exponent = -demurrageRate * elapsed;
   return balanceEntry.principal * Math.pow(Math.E, exponent);
 }
 
-export function stringNumberToEncointerBalanceU8(balance) {
-  return bnToU8a(stringToEncointerBalance(balance), 128, true);
+export function stringNumberToEncointerBalanceU8 (balance) {
+  return bnToU8a(stringToEncointerBalance(balance), {
+    bitLength: 128,
+    isLe: true
+  });
 }
