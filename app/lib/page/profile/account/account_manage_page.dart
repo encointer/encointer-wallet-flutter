04afe278500c87b149ec16ea3a6fc6c2253fe063
clangenb <37865735+clangenb@users.noreply.github.com>
2023-09-27 13:23:47 +0200

    [account_manage_page] extract `FaucetListTile` to separate file

--- app/lib/page/profile/account/account_manage_page.dart
+++ app/lib/page/profile/account/account_manage_page.dart
@@ -1,24 +1,19 @@
-import 'package:encointer_wallet/common/components/submit_button.dart';
-import 'package:encointer_wallet/config/consts.dart';
-import 'package:encointer_wallet/models/communities/community_identifier.dart';
-import 'package:encointer_wallet/models/faucet/faucet.dart';
-import 'package:encointer_wallet/service/tx/lib/src/submit_tx_wrappers.dart';
-import 'package:ew_test_keys/ew_test_keys.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 
+import 'package:encointer_wallet/models/faucet/faucet.dart';
 import 'package:encointer_wallet/common/components/address_icon.dart';
 import 'package:encointer_wallet/common/components/logo/community_icon.dart';
 import 'package:encointer_wallet/theme/theme.dart';
 import 'package:encointer_wallet/page/profile/account/export_result_page.dart';
 import 'package:encointer_wallet/store/account/account.dart';
-import 'package:encointer_wallet/gen/assets.gen.dart';
 import 'package:encointer_wallet/utils/alerts/app_alert.dart';
 import 'package:encointer_wallet/models/encointer_balance_data/balance_entry.dart';
 import 'package:encointer_wallet/page/profile/contacts/account_share_page.dart';
+import 'package:encointer_wallet/page/profile/account/faucet_list_tile.dart';
 import 'package:encointer_wallet/service/log/log_service.dart';
 import 'package:encointer_wallet/service/substrate_api/api.dart';
 import 'package:encointer_wallet/modules/modules.dart';
@@ -28,6 +23,7 @@ import 'package:encointer_wallet/utils/format.dart';
 import 'package:encointer_wallet/utils/input_validation.dart';
 import 'package:encointer_wallet/l10n/l10.dart';
 import 'package:encointer_wallet/utils/ui.dart';
+import 'package:ew_test_keys/ew_test_keys.dart';
 
 class AccountManagePage extends StatefulWidget {
   const AccountManagePage({super.key});
@@ -362,135 +358,6 @@ class _AccountManagePageState extends State<AccountManagePage> {
         ),
       ),
     );
-  }
-}
-
-class FaucetListTile extends StatefulWidget {
-  const FaucetListTile(
-    this.store, {
-    super.key,
-    required this.userAddress,
-    required this.faucetAccount,
-    required this.faucet,
-  });
-
-  final AppStore store;
-
-  final String userAddress;
-  final String faucetAccount;
-  final Faucet faucet;
-
-  @override
-  State<FaucetListTile> createState() => _FaucetListTileState();
-}
-
-class _FaucetListTileState extends State<FaucetListTile> {
-  late Future<Map<int, CommunityIdentifier>> future;
-  late Future<BigInt> nativeBalance;
-
-  @override
-  void initState() {
-    super.initState();
-    future = _getUncommittedReputationIds(widget.userAddress);
-    nativeBalance = getNativeFreeBalance(widget.userAddress);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.symmetric(),
-      leading: SizedBox(
-        width: 50,
-        height: 50,
-        child: CircleAvatar(
-          backgroundColor: Colors.transparent,
-          child: Assets.kusama.svg(fit: BoxFit.fitHeight),
-        ),
-      ),
-      title: Text(
-        widget.faucet.name,
-        style: context.titleMedium.copyWith(color: context.colorScheme.primary),
-      ),
-      subtitle: Row(
-        children: [
-          const Text('KSM: '),
-          FutureBuilder(
-              future: nativeBalance,
-              builder: (BuildContext context, AsyncSnapshot<BigInt> snapshot) {
-                if (snapshot.hasData) {
-                  return Text(Fmt.token(snapshot.data!, ertDecimals));
-                } else {
-                  return const CupertinoActivityIndicator();
-                }
-              })
-        ],
-      ),
-      trailing: FutureBuilder(
-        future: future,
-        builder: (BuildContext context, AsyncSnapshot<Map<int, CommunityIdentifier>> snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data!.isNotEmpty) {
-              return SubmitButtonSmall(
-                onPressed: (context) async {
-                  await _submitFaucetDripTxs(context, snapshot.data!, widget.faucetAccount);
-                  future = _getUncommittedReputationIds(widget.userAddress);
-                  nativeBalance = getNativeFreeBalance(widget.userAddress);
-                  setState(() {});
-                },
-                child: const Text('Claim'),
-              );
-            } else {
-              return const SubmitButtonSmall(child: Text('No Claim'));
-            }
-          } else {
-            return const CupertinoActivityIndicator();
-          }
-        },
-      ),
-    );
-  }
-
-  /// Returns all reputation ids, which haven't been committed for this faucet's
-  /// purpose id yet, i.e., can be used to drip the faucet currently.
-  Future<Map<int, CommunityIdentifier>> _getUncommittedReputationIds(String address) async {
-    final reputations = widget.store.encointer.accountStores![address]!.reputations;
-    final ids = Map<int, CommunityIdentifier>.of({});
-
-    // Create a set of futures to await in parallel.
-    final futures = reputations.entries.map(
-      (e) async {
-        final cid = e.value.communityIdentifier!;
-        // Only check if the reputations community id is allowed to drip the faucet.
-        if (widget.faucet.whitelist == null || widget.faucet.whitelist!.contains(cid)) {
-          final hasCommitted = await webApi.encointer.hasCommittedFor(
-            cid,
-            e.key,
-            widget.faucet.purposeId,
-            address,
-          );
-
-          if (!hasCommitted) ids[e.key] = e.value.communityIdentifier!;
-        }
-      },
-    );
-
-    await Future.wait(futures);
-    return ids;
-  }
-
-  Future<void> _submitFaucetDripTxs(
-    BuildContext context,
-    Map<int, CommunityIdentifier> ids,
-    String faucetAccount,
-  ) async {
-    final futures =
-        ids.entries.map((e) => submitFaucetDrip(context, widget.store, webApi, faucetAccount, e.value, e.key));
-    await Future.wait(futures);
-  }
-
-  Future<BigInt> getNativeFreeBalance(String address) async {
-    final balance = await webApi.assets.getBalanceOf(address);
-    return balance.freeBalance;
   }
 }
 
