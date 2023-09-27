d3a68e5431e01a17e5216a3c126749c9812e1dea
clangenb <37865735+clangenb@users.noreply.github.com>
2023-09-27 11:16:03 +0200

    [meetup_location_view] fmt

--- app/lib/modules/meetup_location/view/meetup_location_view.dart
+++ app/lib/modules/meetup_location/view/meetup_location_view.dart
@@ -1,9 +1,9 @@
-import 'package:encointer_wallet/models/ceremonies/ceremonies.dart';
-import 'package:encointer_wallet/models/communities/community_metadata.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 
+import 'package:encointer_wallet/models/ceremonies/ceremonies.dart';
 import 'package:encointer_wallet/common/components/map/encointer_map.dart';
+import 'package:encointer_wallet/models/communities/community_metadata.dart';
 import 'package:encointer_wallet/models/location/location.dart';
 import 'package:encointer_wallet/service/launch/app_launch.dart';
 import 'package:encointer_wallet/l10n/l10.dart';
@@ -46,10 +46,9 @@ class MeetupLocationPage extends StatelessWidget {
         center: meetupLocation.toLatLng(),
         // zoom level is equivalent to 1 km^2.
         initialZoom: 17,
-        popupBuilder: (BuildContext context, Marker marker) =>
-        communityRules.isLoCoFlex ?
-            _loCoFlexPopupBuilder(context, marker, meetupLocationArgs) :
-            _loCoPopupBuilder(context, marker, meetupLocationArgs),
+        popupBuilder: (BuildContext context, Marker marker) => communityRules.isLoCoFlex
+            ? _loCoFlexPopupBuilder(context, marker, meetupLocationArgs)
+            : _loCoPopupBuilder(context, marker, meetupLocationArgs),
         mapController: _mapController,
       ),
     );
@@ -66,7 +65,7 @@ class MeetupLocationPage extends StatelessWidget {
           l10n.showRouteMeetupLocation,
           style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black54),
         ),
-        onTap:  () => AppLaunch.launchMap(meetupLocationArgs.meetupLocation),
+        onTap: () => AppLaunch.launchMap(meetupLocationArgs.meetupLocation),
       ),
       maxHeight: 300,
     );
@@ -83,7 +82,7 @@ class MeetupLocationPage extends StatelessWidget {
           l10n.showRouteMeetupLocation,
           style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black54),
         ),
-        onTap:  () => AppLaunch.launchMap(meetupLocationArgs.meetupLocation),
+        onTap: () => AppLaunch.launchMap(meetupLocationArgs.meetupLocation),
       ),
     );
   }
