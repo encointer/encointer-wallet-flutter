b47af848b17a9692ea1a886a6bb43debf0950500
clangenb <37865735+clangenb@users.noreply.github.com>
2023-10-08 18:42:09 +0200

    [scripts/replace_backslash_in_dart_files] fix accidental whitespace

--- scripts/replace_backslash_in_dart_files.sh
+++ scripts/replace_backslash_in_dart_files.sh
@@ -14,7 +14,7 @@ if [ ! -d "$directory" ]; then
   exit 1
 fi
 
-# Use find to locate and process all *.da rt files in the specified directory
+# Use find to locate and process all *.dart files in the specified directory
 find "$directory" -type f -name "*.dart" -exec sed -i 's|\\|/|g' {} \;
 
 echo "Replacement complete. All \\ characters in *.dart files converted to /."
