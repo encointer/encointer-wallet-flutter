1f15d2d64c152b6cda96b0c0236b2fda462aec54
clangenb <37865735+clangenb@users.noreply.github.com>
2023-10-16 12:47:49 +0900

    add wip of `author_submitAndWatchExtrinsic`

---
+++ app/lib/service/tx/lib/src/send_tx_dart.dart
@@ -0,0 +1,58 @@
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+
+import 'package:ew_polkadart/ew_polkadart.dart';
+
+/// Substrate state API
+class EWAuthorApi<P extends Provider> {
+  const EWAuthorApi(this._provider);
+
+  final P _provider;
+
+  /// Submit a fully formatted extrinsic for block inclusion.
+  Future<Uint8List> submitExtrinsic(Uint8List extrinsic) async {
+    final params = <dynamic>['0x${hex.encode(extrinsic)}'];
+
+    final response = await _provider.send('author_submitExtrinsic', params);
+
+    if (response.error != null) {
+      throw Exception(response.error.toString());
+    }
+
+    final data = response.result as String;
+    return Uint8List.fromList(hex.decode(data.substring(2)));
+  }
+
+  /// Submit a fully formatted extrinsic and return a subscription
+  /// which emits txStatus updates.
+  Future<SubscriptionReponse> submitAndWatchExtrinsic(Uint8List extrinsic) async {
+    final params = <dynamic>['0x${hex.encode(extrinsic)}'];
+
+    final subscription = await _provider.subscribe(
+      'author_submitAndWatchExtrinsic',
+      params,
+      onCancel: (subscription) async {
+        await _provider.send('author_unsubmitAndWatchExtrinsic', [subscription]);
+      },
+    );
+
+    return subscription;
+  }
+
+  /// Submit a fully formatted extrinsic and return a subscription
+  /// which emits txStatus updates.
+  Future<SubscriptionReponse> subscribeFinalizedHeads() async {
+    // final params = <dynamic>['0x${hex.encode(extrinsic)}'];
+
+    final subscription = await _provider.subscribe(
+      'chain_subscribeFinalizedHeads',
+      [],
+      onCancel: (subscription) async {
+        await _provider.send('chain_unsubscribeFinalizedHeads', [subscription]);
+      },
+    );
+
+    return subscription;
+  }
+}
---
+++ app/test/service/tx/lib/send_tx_dart_test.dart
@@ -0,0 +1,40 @@
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+import 'package:encointer_wallet/service/tx/lib/src/send_tx_dart.dart';
+
+import 'package:ew_polkadart/ew_polkadart.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('submit xt', () {
+    test('send alice to bob works', () async {
+      final polkadart = Provider.fromUri(Uri.parse('ws://localhost:9944'));
+      final author = EWAuthorApi(polkadart);
+
+      // note this contains some nonce and will not work on an arbitrary setup. Instead,
+      // it will throw a bad signature error, see https://github.com/leonardocustodio/polkadart/pull/337.
+      final tx = hex.decode(
+          '450284008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4801c497336fab8028b149e31be4a93054a58ec229da66aab2be8af9792de54f446c8846ab0ad45921117b35bf15794cbdf853870ec7bf43b8a15a151ad26c04688f45013c00000a0700d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d070010a5d4e8'
+      );
+
+      final sub = await author.submitAndWatchExtrinsic(tx as Uint8List);
+
+      final events = await sub.stream.first;
+      // Fixme: first never arrives
+      print('First: $events');
+    });
+
+    test('subscribing to finalized heads works', () async {
+      final polkadart = Provider.fromUri(Uri.parse('ws://localhost:9944'));
+      // Note: bad name, chain api is only now used for prototyping
+      final author = EWAuthorApi(polkadart);
+
+      final sub = await author.subscribeFinalizedHeads();
+
+      final events = await sub.stream.first;
+      // Fixme: first never arrives
+      print('First: $events');
+    });
+  });
+}
