59594554b2686d10ac9856dc78d9750fa7633141
clangenb <37865735+clangenb@users.noreply.github.com>
2023-10-31 13:41:31 +0900

    [ew_substrate_fixed] restructure fixed point library and implement fixed arithmetics for certain types.

---
+++ packages/ew_substrate_fixed/lib/src/fixed_point_util.dart
@@ -0,0 +1,33 @@
+
+import 'package:ew_substrate_fixed/src/parse_fixed_point.dart' as pf;
+import 'package:ew_substrate_fixed/src/to_fixed_point.dart' as tf;
+
+const u16F16Util = FixedPointUtil(BitCount(16, 16));
+const u32F32Util = FixedPointUtil(BitCount(32, 32));
+const u64F64Util = FixedPointUtil(BitCount(64, 64));
+
+/// Util class that allows to have consts knowing how to handle
+/// fixed point numbers.
+class FixedPointUtil<T extends BitCount> {
+  const FixedPointUtil(this.bitCount);
+
+  final T bitCount;
+
+  BigInt toFixed(double value) {
+    return tf.toFixedPoint(value, integerBitCount: bitCount.integer, fractionalBitCount: bitCount.fractional);
+  }
+
+  double toDouble(BigInt value) {
+    return pf.parseFixedPoint(value, integerBitCount: bitCount.integer, fractionalBitCount: bitCount.fractional);
+  }
+}
+
+class BitCount {
+  const BitCount(this._integerBitCount, this._fractionBitCount);
+
+  final int _integerBitCount;
+  final int _fractionBitCount;
+
+  int get integer => _integerBitCount;
+  int get fractional => _fractionBitCount;
+}
--- packages/ew_substrate_fixed/lib/substrate_fixed.dart
+++ packages/ew_substrate_fixed/lib/substrate_fixed.dart
@@ -1,4 +1,4 @@
 library substrate_fixed;
 
-export 'src/parse_fixed_point.dart';
-export 'src/to_fixed_point.dart';
+export 'src/fixed_point.dart' show U16F16, U32F32, U64F64;
+export 'src/fixed_point_util.dart' show u16F16Util, u32F32Util, u64F64Util;
