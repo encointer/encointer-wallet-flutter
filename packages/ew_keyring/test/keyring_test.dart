1b7311dd0eb719472c41adb5f02dc9d96710de2c
clangenb <37865735+clangenb@users.noreply.github.com>
2023-11-16 16:29:01 +0900

    [ew_keyring] add test adding well known accounts, but they currently fail.

---
+++ packages/ew_keyring/test/keyring_test.dart
@@ -0,0 +1,24 @@
+import 'package:ew_keyring/ew_keyring.dart';
+import 'package:ew_keyring/src/keyring_data.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Well known accounts', () {
+    test('inferring key type works', () async {
+      final alice = KeyringAccount('Alice', '//Alice');
+      final bob = KeyringAccount('Bob', '//Bob');
+      final charlie = KeyringAccount('Charlie', '//Charlie');
+      final accounts = [alice, bob, charlie];
+      final keyring = await EncointerKeyring.fromAccounts(accounts);
+
+      print("keyring: ${keyring.accounts.toString()}");
+      print("keyring: ${keyring.keyring.toString()}");
+
+      final alicePair = keyring.getPairByPublicKey('d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'.codeUnits);
+      final aliceAccount = keyring.getAccountByPublicKey('d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'.codeUnits);
+
+      expect(aliceAccount.name, 'Alice');
+      expect(alicePair.address, '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY');
+    });
+  });
+}
