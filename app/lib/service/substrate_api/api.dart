992a3f2b8e259b4e8a20ef3f66df6fa912ae7a13
clangenb <37865735+clangenb@users.noreply.github.com>
2023-11-20 10:58:20 +0900

    [substrate_api] `ReconnectingWebsocketProvider` fix things for good.

--- app/lib/service/substrate_api/api.dart
+++ app/lib/service/substrate_api/api.dart
@@ -242,8 +242,13 @@ class Api {
 }
 
 class ReconnectingWsProvider extends Provider {
-  ReconnectingWsProvider(Uri url, {bool autoConnect = true}) : provider = WsProvider(url, autoConnect: autoConnect);
+  ReconnectingWsProvider(this.url, {bool autoConnect = true})
+      : provider = WsProvider(
+          url,
+          autoConnect: autoConnect,
+        );
 
+  final Uri url;
   WsProvider provider;
 
   Future<void> connectToNewEndpoint(Uri url) async {
@@ -256,13 +261,19 @@ class ReconnectingWsProvider extends Provider {
     if (isConnected()) {
       return Future.value();
     } else {
+      // We want to use a new channel even if the channel exists but it was closed.
+      provider.channel = null;
+      provider = WsProvider(url, autoConnect: false);
       return provider.connect();
     }
   }
 
   @override
   Future disconnect() {
-    if (!isConnected()) {
+    // We only care if the channel is not equal to null.
+    // Because we still want the internal cleanup if
+    // the connection was closed from the other end.
+    if (provider.channel != null) {
       return Future.value();
     } else {
       return provider.disconnect();
@@ -271,7 +282,10 @@ class ReconnectingWsProvider extends Provider {
 
   @override
   bool isConnected() {
-    return provider.isConnected();
+    // the `provider.isConnected()` check is wrong upstream.
+    // Hence, we implement it ourselves.
+    final channel = provider.channel;
+    return channel != null && channel.closeCode == null;
   }
 
   @override
