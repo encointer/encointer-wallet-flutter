353c37e23736c814861a6bb6b03ffbce401f413e
clangenb <37865735+clangenb@users.noreply.github.com>
2023-11-20 13:10:50 +0900

    [mockApi] add missing overrides for new encointer kusama api

--- app/test/mock/api/mock_encointer_kusama_api.dart
+++ app/test/mock/api/mock_encointer_kusama_api.dart
@@ -1,5 +1,5 @@
 import 'package:encointer_wallet/service/service.dart';
-import 'package:ew_polkadart/ew_polkadart.dart';
+import 'package:ew_polkadart/generated/encointer_kusama/encointer_kusama.dart';
 
 class MockEncointerKusamaApi implements EncointerKusama {
   MockEncointerKusamaApi();
@@ -26,4 +26,12 @@ class MockEncointerKusamaApi implements EncointerKusama {
   @override
   // TODO: implement rpc
   Rpc get rpc => throw UnimplementedError();
+
+  @override
+  // TODO: implement registry
+  Registry get registry => throw UnimplementedError();
+
+  @override
+  // TODO: implement tx
+  Extrinsics get tx => throw UnimplementedError();
 }
