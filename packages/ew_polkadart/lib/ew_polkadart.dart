77c0264e338cc48b86107811c42fe409ad0daeee
clangenb <37865735+clangenb@users.noreply.github.com>
2023-11-21 16:18:44 +0900

    fmt

--- packages/ew_polkadart/lib/ew_polkadart.dart
+++ packages/ew_polkadart/lib/ew_polkadart.dart
@@ -6,15 +6,4 @@ export 'generated/encointer_kusama/types/tuples.dart' show Tuple2, Tuple3;
 export 'package:polkadart/polkadart.dart';
 
 export 'package:polkadart_scale_codec/polkadart_scale_codec.dart'
-    show
-    ByteInput,
-    ByteOutput,
-    BoolCodec,
-    Codec,
-    Input,
-    Output,
-    U32Codec,
-    U64Codec,
-    U8Codec,
-    SequenceCodec,
-    StrCodec;
+    show ByteInput, ByteOutput, BoolCodec, Codec, Input, Output, U32Codec, U64Codec, U8Codec, SequenceCodec, StrCodec;
