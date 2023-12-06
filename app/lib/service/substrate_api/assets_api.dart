82bfdbc6e2d9e04d5a718522c6570022e373aa68
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-06 13:57:47 +0700

    [assetsApi] fix native balance getter (#1586)

--- app/lib/service/substrate_api/assets_api.dart
+++ app/lib/service/substrate_api/assets_api.dart
@@ -5,6 +5,7 @@ import 'package:encointer_wallet/service/substrate_api/core/js_api.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:ew_keyring/ew_keyring.dart';
 import 'package:ew_polkadart/ew_polkadart.dart' show ByteInput, EncointerKusama, StorageChangeSet;
+import 'package:ew_polkadart/generated/encointer_kusama/types/frame_system/account_info.dart';
 import 'package:ew_polkadart/generated/encointer_kusama/types/pallet_balances/types/account_data.dart';
 
 class AssetsApi {
@@ -41,7 +42,7 @@ class AssetsApi {
   }
 
   Future<AccountData> getBalanceOf(String address) async {
-    return encointerKusama.query.balances.account(AddressUtils.addressToPubKey(address));
+    return encointerKusama.query.system.account(AddressUtils.addressToPubKey(address)).then((info) => info.data);
   }
 
   Future<void> subscribeBalance() async {
@@ -52,13 +53,17 @@ class AssetsApi {
 
     if (pubKey == null || pubKey.isEmpty || address.isEmpty) return;
 
-    final balanceKey = encointerKusama.query.balances.accountKey(AddressUtils.addressToPubKey(address));
+    final balanceKey = encointerKusama.query.system.accountKey(AddressUtils.addressToPubKey(address));
 
     _balanceSubscription = await encointerKusama.rpc.state.subscribeStorage([balanceKey], (storageChangeSet) async {
       Log.p('Got account data subscription: $storageChangeSet');
       if (storageChangeSet.changes[0].value != null) {
-        final accountData = AccountData.decode(ByteInput(storageChangeSet.changes[0].value!));
-        await store.assets.setAccountBalances(pubKey, Map.of({store.settings.networkState!.tokenSymbol!: accountData}));
+        final accountData = AccountInfo.decode(ByteInput(storageChangeSet.changes[0].value!));
+        await store.assets.setAccountBalances(
+            pubKey,
+            Map.of({
+              store.settings.networkState!.tokenSymbol!: accountData.data,
+            }));
       }
     });
   }
