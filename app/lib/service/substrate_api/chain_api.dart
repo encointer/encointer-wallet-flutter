d3db3c08fa8d63331188b7ce0de383a749e28acd
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-07 12:17:54 +0700

    Introduce optional `Blockhash at` argument for all getters and use best block instead of finalized block for getters and tx's (#1588)
    * [chainApi] get latest header hash after received latest header.
    
    * [chainApi] fix `BlockHash` computing from string
    
    * wip of getters with `Blockhash at`, and use the polkadart websocket in our dart api.
    
    * wip of getters with `Blockhash at`, and use the polkadart websocket in our dart api.
    
    * Use dart for getting encointer balance
    
    * all encointer getters except for bazaar have now the `at` argument
    
    * [assetsApi] introduce at and fix balance getter in `fetchAccountData`
    
    * [assetsApi] remove obsolete `getBalance` getter
    
    * [encointerApi] add `at` argument for bazaar stuff
    
    * [substrate_api] remove duplicated definition of reconnecting_ws_provider.dart
    
    * fmt
    
    * [store/chain] regenerate mobx file
    
    * [store/chain] add documentation
    
    * [service/send_tx_dart] wait for `inBlock` instead of `finalized`
    
    * [service/send_tx_dart] prevent race conditions in `onFinish` call back, and don't brute force entire data update, only get the values we need.
    
    * [encointerApi] remove hack for polling aggregated account data until it returns the data for the next phase.

--- app/lib/service/substrate_api/chain_api.dart
+++ app/lib/service/substrate_api/chain_api.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:encointer_wallet/service/service.dart';
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/store/chain/types/header.dart';
+import 'package:encointer_wallet/service/substrate_api/core/reconnecting_ws_provider.dart';
 
 class ChainApi {
   ChainApi(this.store, this.provider);
@@ -22,6 +23,22 @@ class ChainApi {
     await _latestHeaderSubscription?.cancel();
   }
 
+  Future<String> getBlockHash(int? number) async {
+    final params = number != null ? [number] : const <String>[];
+    Log.p('[getBlockHash] params: $params');
+
+    final response = await provider.send('chain_getBlockHash', params);
+
+    if (response.error != null) {
+      throw Exception(response.error.toString());
+    }
+
+    final data = response.result;
+    Log.p('[getBlockHash] hash: $data');
+
+    return data as String;
+  }
+
   /// Subscribes to the latest headers
   Future<void> subscribeNewHeads() async {
     await _latestHeaderSubscription?.cancel();
@@ -37,9 +54,12 @@ class ChainApi {
     _latestHeaderSubscription = subscription.stream.map((res) {
       Log.p('Header: ${res.result}');
       return Header.fromRpc(res.result as Map<String, dynamic>);
-    }).listen((header) {
+    }).listen((header) async {
       Log.p('[subscribeNewHeads] Got header: ${header.toJson()}');
       store.chain.setLatestHeader(header);
+
+      final hash = await getBlockHash(header.number);
+      store.chain.setLatestHeaderHash(hash);
     });
   }
 }
--- app/lib/store/chain/chain.dart
+++ app/lib/store/chain/chain.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart' show hex;
+import 'package:mobx/mobx.dart';
+
 import 'package:encointer_wallet/store/app.dart';
 import 'package:encointer_wallet/store/chain/types/header.dart';
-import 'package:mobx/mobx.dart';
 
 part 'chain.g.dart';
 
@@ -17,6 +21,38 @@ abstract class _ChainStore with Store {
 
   @observable
   Header? latestHeader;
+
+  @observable
+  String? latestHashHex;
+
+  /// Computed value ready to be served into polkadart's methods.
+  ///
+  /// Example:
+  /// ```dart
+  /// await encointerKusama.query.encointerScheduler
+  ///   .currentPhase(at: at ?? store.chain.latestHash);
+  /// ```
+  /// The above will:
+  /// 1. Use the provided `at` argument if not null.
+  /// 2. Use the `store.chain.latestHash`.
+  /// 3. If `store.chain.latestHash` is null, it will return the value corresponding
+  /// to the latest finalized head.
+  ///
+  /// Note: we can't use the type alias `BlockHash` here due to a mobx bug:
+  /// https://github.com/mobxjs/mobx.dart/issues/968
+  @computed
+  Uint8List? get latestHash {
+    if (latestHashHex != null) {
+      return Uint8List.fromList(hex.decode(latestHashHex!.replaceFirst('0x', '')));
+    } else {
+      return null;
+    }
+  }
+
+  @action
+  void setLatestHeaderHash(String latestHash) {
+    latestHashHex = latestHash;
+  }
 
   @action
   void setLatestHeader(Header latest) {
