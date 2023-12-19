85f9cd68c6be74d531523c77a29475222702180d
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-19 15:12:54 +0100

    [ew_keyring] getPairByPubicKey/getAccountByPublicKey works

--- packages/ew_keyring/test/keyring_test.dart
+++ packages/ew_keyring/test/keyring_test.dart
@@ -1,3 +1,4 @@
+import 'package:convert/convert.dart';
 import 'package:ew_keyring/src/keyring.dart';
 import 'package:ew_keyring/src/keyring_data.dart';
 import 'package:ew_keyring/src/validate_keys.dart';
@@ -6,7 +7,6 @@ import 'package:test/test.dart';
 
 void main() {
   group('Keyring', () {
-
     Future<EncointerKeyring> testKeyring() async {
       final alice = KeyringAccount.newValidated('Alice', '//Alice');
       final bob = KeyringAccount.newValidated('Bob', '//Bob');
@@ -34,17 +34,24 @@ void main() {
       expect(aliceAccount.seedType, SeedType.raw);
     });
 
-    test('Keyring.getByPublicKey works', () async {
+    test('Keyring.getPairByPublicKey works', () async {
       final keyring = await testKeyring();
 
-      // ignore: avoid_print
-      print('publicKey: ${'0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'.codeUnits}');
-      print('publicKey: ${'0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'.codeUnits}');
+      final alicePair =
+          keyring.getPairByPublicKey(hex.decode('d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'));
+
+      expect(alicePair.address, '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY');
+      expect(alicePair.keyPairType, KeyPairType.sr25519);
+    });
+
+    test('Keyring.getAccountByPublicKey works', () async {
+      final keyring = await testKeyring();
 
       final aliceAccount =
-      keyring.getAccountByPublicKey('d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'.codeUnits);
+          keyring.getAccountByPublicKey(hex.decode('d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'));
 
       expect(aliceAccount.name, 'Alice');
+      expect(aliceAccount.seedType, SeedType.raw);
     });
 
     test('test alice works', () async {
