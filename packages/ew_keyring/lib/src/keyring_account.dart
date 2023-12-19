4f5866f7fdd0c644b15dd67e47a18f013af1b09f
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-19 16:52:40 +0100

    [ew_keyring] fix de-/serialization of `KeyringAccount`

--- packages/ew_keyring/lib/src/keyring_account.dart
+++ packages/ew_keyring/lib/src/keyring_account.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:ew_keyring/src/validate_keys.dart';
 import 'package:json_annotation/json_annotation.dart';
 
@@ -16,16 +18,16 @@ class KeyringAccount {
     }
   }
 
+  factory KeyringAccount.fromJson(Map<String, dynamic> json) => _$KeyringAccountFromJson(json);
+  Map<String, dynamic> toJson() => _$KeyringAccountToJson(this);
+
   final String name;
   final String seed;
-
-  static KeyringAccount fromJson(Map<String, dynamic> json) => _$KeyringAccountFromJson(json);
-  static Map<String, dynamic> toJson(KeyringAccount acc) => _$KeyringAccountToJson(acc);
 
   SeedType get seedType => getSeedTypeFromString(seed);
 
   @override
   String toString() {
-    return KeyringAccount.toJson(this).toString();
+    return jsonEncode(this);
   }
 }
