ac1af7b88f1a2382670ef6a271ba659b9589fd7b
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-21 14:49:34 +0100

    [store/account] properly define account data

--- app/lib/store/account/types/account_data.dart
+++ app/lib/store/account/types/account_data.dart
@@ -4,11 +4,31 @@ part 'account_data.g.dart';
 
 @JsonSerializable()
 class AccountData extends _AccountData {
-  static AccountData fromJson(Map<String, dynamic> json) => _$AccountDataFromJson(json);
+  AccountData({
+    required super.name,
+    required super.pubKey,
+    required super.address,
+  });
+
+  factory AccountData.empty() => AccountData(name: '', pubKey: '', address: '');
+
+  // Fixme: these declarations are wrong. Check `KeyringAccount` for correct json methods.
+  factory AccountData.fromJson(Map<String, dynamic> json) => _$AccountDataFromJson(json);
   static Map<String, dynamic> toJson(AccountData acc) => _$AccountDataToJson(acc);
+
+  @override
+  String toString() {
+    return AccountData.toJson(this).toString();
+  }
 }
 
 abstract class _AccountData {
+  _AccountData({
+    required this.name,
+    required this.pubKey,
+    required this.address,
+  });
+
   String name = '';
   String address = '';
   String? encoded = '';
