5f166c2899e09b514c15ae4269b7bc8c119ddab4
clangenb <37865735+clangenb@users.noreply.github.com>
2024-01-12 14:57:12 +0100

    [AppStore] regenerate with generics

--- app/lib/store/app.g.dart
+++ app/lib/store/app.g.dart
@@ -8,7 +8,7 @@ part of 'app.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$AppStore on _AppStore, Store {
+mixin _$AppStore<S extends SecureStorageInterface, L extends LegacyStorageInterface> on _AppStore<S, L>, Store {
   Computed<DataUpdateStore>? _$dataUpdateComputed;
 
   @override
