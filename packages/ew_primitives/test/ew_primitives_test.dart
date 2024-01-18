2a443db5dfb2cb19792596c505cef253d6d0e303
clangenb <37865735+clangenb@users.noreply.github.com>
2024-01-18 20:41:03 +0100

    [ew_primitives] generate a valid proof of attendance in dart

--- packages/ew_primitives/test/ew_primitives_test.dart
+++ packages/ew_primitives/test/ew_primitives_test.dart
@@ -1,9 +1,26 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:ew_keyring/ew_keyring.dart';
+import 'package:ew_polkadart/encointer_types.dart';
+import 'package:ew_primitives/ew_primitives.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('EwModels', () {
-    test('mock test', () {
+  group('proof of attendance is valid', () {
+    test('signed factory works', () async {
+      final keyring = await testKeyring();
+      final alice = keyring.accountsIter.first;
+
+      final proof = ProofOfAttendanceFactory.signed(
+        proverPublic: alice.pubKey,
+        ceremonyIndex: 1,
+        communityIdentifier: const CommunityIdentifier(
+          geohash: [0, 0, 0, 0, 0],
+          digest: [0, 0, 0, 0],
+        ),
+        attendee: alice.pair,
+      );
+
+      print('${proof.encode()}');
+
       expect(true, true);
     });
   });
