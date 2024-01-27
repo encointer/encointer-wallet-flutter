b5edc9578b860c7019a8e85927c086ccd8e21d33
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-01-27 22:48:04 +0600

    Fixed take screenshot CI on IOS

--- app/test_driver/helpers/extension/screenshot_driver.dart
+++ app/test_driver/helpers/extension/screenshot_driver.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
@@ -19,9 +18,7 @@ extension ScreenshotExtension on FlutterDriver {
     Duration timeout = const Duration(seconds: 30),
     bool waitUntilNoTransientCallbacks = true,
   }) async {
-    if (await canDriverTakeScreenshot() &&
-        locales.contains('en') &&
-        !File('../screenshots/en/$name.png').existsSync()) {
+    if (locales.contains('en') && !File('../screenshots/en/$name.png').existsSync()) {
       await requestData(TestCommand.devModeOff);
       for (final locale in locales) {
         final currentLocale = await requestData('local-$locale');
@@ -50,11 +47,5 @@ extension ScreenshotExtension on FlutterDriver {
     await file.writeAsBytes(pixels);
     // ignore: avoid_print
     print('Screenshot $name created at ${file.path}');
-  }
-
-  Future<bool> canDriverTakeScreenshot() async {
-    final operationSystem = await requestData(TestCommand.getPlatform);
-    log('operationSystem ==================> $operationSystem');
-    return operationSystem == 'android';
   }
 }
