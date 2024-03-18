39adef275fd782f8e166c1214b7749dececc61e3
clangenb <37865735+clangenb@users.noreply.github.com>
2024-03-18 13:22:54 -0700

    fmt

--- app/lib/service/substrate_api/encointer/encointer_api.dart
+++ app/lib/service/substrate_api/encointer/encointer_api.dart
@@ -592,8 +592,7 @@ class EncointerApi {
       final reputationsV1 = await _dartApi.getReputationsV1(address, at: at ?? store.chain.latestHash);
       Log.d('api: getReputations: $reputationsV1', 'EncointerApi');
       final cIndex = store.encointer.currentCeremonyIndex ?? 0;
-      final reputations = Map.fromEntries(
-          reputationsV1.entries.map((e) => MapEntry(e.key, e.value.toV2(cIndex))));
+      final reputations = Map.fromEntries(reputationsV1.entries.map((e) => MapEntry(e.key, e.value.toV2(cIndex))));
       Log.d('api: getReputations (migrated to V2): $reputations', 'EncointerApi');
 
       if (reputations.isNotEmpty) await store.encointer.account?.setReputations(reputations);
