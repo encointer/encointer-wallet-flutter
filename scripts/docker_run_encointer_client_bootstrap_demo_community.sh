d3df1f09ddae03d4f3eea1166e3247a1302c2089
clangenb <37865735+clangenb@users.noreply.github.com>
2024-06-28 16:55:26 +0200

    [scripts] add signer bob to bootstrap democommunity script

--- scripts/docker_run_encointer_client_bootstrap_demo_community.sh
+++ scripts/docker_run_encointer_client_bootstrap_demo_community.sh
@@ -6,4 +6,4 @@ DOCKER_TAG=${1:-1.13.0}
 echo "Encointer client docker tag: ${DOCKER_TAG}"
 
 # `--add-host host.docker.internal:host-gateway` is only needed for linux
-docker run --add-host host.docker.internal:host-gateway encointer/encointer-client-notee:${DOCKER_TAG} bootstrap_demo_community.py -u ws://host.docker.internal -p 9944
+docker run --add-host host.docker.internal:host-gateway encointer/encointer-client-notee:${DOCKER_TAG} bootstrap_demo_community.py -u ws://host.docker.internal -p 9944 --signer //Bob
