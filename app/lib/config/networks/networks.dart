d4a670fdee99c9c3f2eb6fd2891cbe05aa725881
clangenb <37865735+clangenb@users.noreply.github.com>
2024-07-12 16:51:11 +0200

    [config] introduce network enum for different networks (it is not used yet)

---
+++ app/lib/config/networks/networks.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:encointer_wallet/config/consts.dart';
+
+class NetworkEndpoint {
+  NetworkEndpoint({required this.name, required this.address});
+
+  final String name;
+  final String address;
+}
+
+const String gesellInfo = 'nctr-gsl';
+const String gesellDevInfo = 'nctr-gsl-dev';
+const String rococoInfo = 'nctr-r';
+const String kusamaInfo = 'nctr-k';
+
+/// Enum representing the different networks.
+///
+/// NOTE: We shouldn't do `_` wildcard matching in the switch statement so that
+///       we get guaranteed type safety when we extend the enum variant due to
+///       compiler check for exhaustive matching.
+enum Network {
+  gesell,
+  gesellDev,
+  rococo,
+  kusama;
+
+  factory Network.fromInfo(String info) {
+    return switch (info) {
+      gesellInfo => Network.gesell,
+      rococoInfo => Network.rococo,
+      kusamaInfo => Network.kusama,
+      gesellDevInfo => Network.gesellDev,
+      _ => Network.kusama,
+    };
+  }
+
+  String info() {
+    return switch (this) {
+      gesell => gesellInfo,
+      gesellDev => gesellDevInfo,
+      rococo => rococoInfo,
+      kusama => kusamaInfo,
+    };
+  }
+
+  int ss58() {
+    return switch (this) {
+      gesell => 42,
+      gesellDev => 42,
+      rococo => 42,
+      kusama => 2,
+    };
+  }
+
+  String ipfsGateway() {
+    return switch (this) {
+      gesell => ipfsGatewayEncointer,
+      rococo => ipfsGatewayEncointer,
+      kusama => ipfsGatewayEncointer,
+      // only dev network refers to the local one
+      gesellDev => ipfs_gateway_local,
+    };
+  }
+
+  List<NetworkEndpoint> networkEndpoints() {
+    return switch (this) {
+      gesell => gesellEndpoints(),
+      gesellDev => gesellDevEndpoints(),
+      rococo => rococoEndpoints(),
+      kusama => kusamaEndpoints(),
+    };
+  }
+}
+
+List<NetworkEndpoint> gesellEndpoints() {
+  return [
+    NetworkEndpoint(name: 'Encointer Gesell (Hosted by Encointer Association)', address: 'wss://gesell.encointer.org')
+  ];
+}
+
+List<NetworkEndpoint> gesellDevEndpoints() {
+  return [
+    NetworkEndpoint(
+        name: 'Encointer Gesell Local DevNet',
+        address: 'ws://${Platform.isAndroid ? androidLocalHost : iosLocalHost}:9944')
+  ];
+}
+
+List<NetworkEndpoint> rococoEndpoints() {
+  return [
+    NetworkEndpoint(
+        name: 'Encointer Lietaer on Rococo (Hosted by Encointer Association)',
+        address: 'wss://rococo.api.encointer.org')
+  ];
+}
+
+List<NetworkEndpoint> kusamaEndpoints() {
+  return [
+    NetworkEndpoint(
+        name: 'Encointer Network on Kusama (Hosted by Encointer Association)',
+        address: 'wss://kusama.api.encointer.org')
+  ];
+}
