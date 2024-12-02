5c57459c4fa3ed9e0d9bff7b9b7048ea4e2ed4fe
clangenb <37865735+clangenb@users.noreply.github.com>
2024-12-02 13:38:00 +0100

    Fix democracy numbers (#1714)
    * fix constants
    
    * fix lower bound of valid voting cIndexes
    
    * fix demurrage calculation for dispaly in proposal
    
    * remove unused variable
    
    ---------
    
    Co-authored-by: Alain Brenzikofer <alain@integritee.network>

--- packages/ew_polkadart/lib/generated/encointer_kusama/pallets/encointer_democracy.dart
+++ packages/ew_polkadart/lib/generated/encointer_kusama/pallets/encointer_democracy.dart
@@ -260,7 +260,7 @@ class Constants {
   final int maxReputationCount = 64;
 
   /// The Period in which the proposal has to be in passing state before it is approved.
-  final BigInt confirmationPeriod = BigInt.from(300000);
+  final BigInt confirmationPeriod = BigInt.from(172800000);
 
   /// The total lifetime of a proposal.
   ///
@@ -268,9 +268,9 @@ class Constants {
   ///
   /// Note: In cycles this must be smaller than `ReputationLifetime`, otherwise the eligible
   /// electorate will be 0.
-  final BigInt proposalLifetime = BigInt.from(1200000);
+  final BigInt proposalLifetime = BigInt.from(777600000);
 
   /// Minimum turnout in permill for a proposal to be considered as passing and entering the
   /// `Confirming` state.
-  final BigInt minTurnout = BigInt.one;
+  final BigInt minTurnout = BigInt.from(50);
 }
