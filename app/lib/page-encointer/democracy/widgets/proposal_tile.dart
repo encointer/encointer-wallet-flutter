1b12b0443d0d32ae9ce17c3f089e25a77a01f6e5
clangenb <37865735+clangenb@users.noreply.github.com>
2025-01-27 17:36:11 +0100

    [Democracy] hide misleading texts in case there is no turnout yet

--- app/lib/page-encointer/democracy/widgets/proposal_tile.dart
+++ app/lib/page-encointer/democracy/widgets/proposal_tile.dart
@@ -80,13 +80,17 @@ class _ProposalTileState extends State<ProposalTile> {
           ListTile(
             contentPadding: const EdgeInsets.symmetric(),
             leading: Text(widget.proposalId.toString(), style: titleSmall),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('${l10n.proposalTurnout}: $turnout / $electorateSize'),
-                Text(l10n.proposalApprovalThreshold((threshold * 100).toStringAsFixed(2))),
-                passingOrFailingText(context, proposal, tally, widget.params)
-              ],
+            subtitle: SizedBox(
+              // ensure constant height even for missing texts without turnout.
+              height: 60,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${l10n.proposalTurnout}: $turnout / $electorateSize'),
+                  if (threshold != 0) Text(l10n.proposalApprovalThreshold((threshold * 100).toStringAsFixed(2))),
+                  if (threshold != 0) passingOrFailingText(context, proposal, tally, widget.params),
+                ],
+              ),
             ),
             trailing: voteButtonOrProposalStatus(context),
           ),
