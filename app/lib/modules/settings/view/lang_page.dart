9195fb8efe63d4365f18913a6e3ed5cd1c632b06
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-24 15:28:26 +0200

    fix fmt

--- app/lib/modules/settings/view/lang_page.dart
+++ app/lib/modules/settings/view/lang_page.dart
@@ -18,25 +18,25 @@ class _LangPageState extends State<LangPage> {
   Widget build(BuildContext context) {
     final settings = context.watch<AppSettings>();
     return Scaffold(
-  appBar: AppBar(title: Text(context.l10n.lang)),
-  body: RadioGroup<Locale>(
-    groupValue: settings.locale,
-    onChanged: (locale) async {
-      await context.read<AppSettings>().setLocale(locale!.languageCode);
-    },
-    child: ListView.builder(
-      itemCount: AppLocalizations.supportedLocales.length,
-      itemBuilder: (BuildContext context, int index) {
-        final locale = AppLocalizations.supportedLocales[index];
-        final lang = context.localeName(locale.languageCode);
-        return RadioListTile<Locale>(
-          key: Key('locale-${locale.languageCode}'),
-          title: Text(lang),
-          value: locale,
-        );
-      },
-    ),
-  ),
-);
+      appBar: AppBar(title: Text(context.l10n.lang)),
+      body: RadioGroup<Locale>(
+        groupValue: settings.locale,
+        onChanged: (locale) async {
+          await context.read<AppSettings>().setLocale(locale!.languageCode);
+        },
+        child: ListView.builder(
+          itemCount: AppLocalizations.supportedLocales.length,
+          itemBuilder: (BuildContext context, int index) {
+            final locale = AppLocalizations.supportedLocales[index];
+            final lang = context.localeName(locale.languageCode);
+            return RadioListTile<Locale>(
+              key: Key('locale-${locale.languageCode}'),
+              title: Text(lang),
+              value: locale,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
