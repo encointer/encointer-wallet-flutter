c390fdc61c6b93be95e11189a18de169f601c192
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-28 22:29:41 +0200

    Parametrize LocationToAccountApi

--- app/test/service/substrate_api/core/location_to_account_api_test.dart
+++ app/test/service/substrate_api/core/location_to_account_api_test.dart
@@ -5,27 +5,53 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../../../utils/test_tags.dart';
 
-const leuTreasuryOnEncointer = "HNJDzJEGaBgWRXz7bjERsRidJFQBnj1AZ2Tn3Q9uRGynhwq";
-const payNuqTreasuryOnEncointer = "E2mZ1u2xepTF8nuEQVkrimPVwqtqq1joC56cUwYPftXAEQL";
-const nyotaTreasuryOnEncointer = "E9KVuDLEtBBWSqhCiKn31VPBBLe33CbYJTrnWAbjszwskWH";
-
-const leuTreasuryOnAHK = "HNJDzJEGaBgWRXz7bjERsRidJFQBnj1AZ2Tn3Q9uRGynhwq";
-const payNuqTreasuryOnAHK = "E2mZ1u2xepTF8nuEQVkrimPVwqtqq1joC56cUwYPftXAEQL";
-const nyotaTreasuryOnAHK = "E9KVuDLEtBBWSqhCiKn31VPBBLe33CbYJTrnWAbjszwskWH";
-
 void main() {
   group('locationToAccountApi', () {
-    test('leu converts correctly', () async {
-      final provider = ReconnectingWsProvider(Uri.parse('wss://sys.ibp.network/asset-hub-kusama'));
+    late ReconnectingWsProvider provider;
+    late LocationToAccountApi api;
 
-      final leuLocationOnAHK = encointerAccountOnAHK(leuTreasuryOnEncointer);
+    // Initialize provider once for all tests
+    setUpAll(() {
+      provider = ReconnectingWsProvider(
+        Uri.parse('wss://sys.ibp.network/asset-hub-kusama'),
+      );
+      api = LocationToAccountApi(provider);
+    });
 
-      final encointerApi = LocationToAccountApi(provider);
-      final accountId = await encointerApi.locationToAccountId(leuLocationOnAHK);
-
+    // Disconnect provider after all tests
+    tearDownAll(() async {
       await provider.disconnect();
+    });
 
-      expect(accountId, AddressUtils.addressToPubKey(leuTreasuryOnAHK));
-    }, tags: productionE2E);
+    final testCases = [
+      {
+        'name': 'Leu',
+        'encointer': 'HNJDzJEGaBgWRXz7bjERsRidJFQBnj1AZ2Tn3Q9uRGynhwq',
+        'expected': 'DgdA9qwXxBAtdy9veCR4LZpcbYuMgCSL9XpV7gbELFncV2t',
+      },
+      {
+        'name': 'PayNuq',
+        'encointer': 'E2mZ1u2xepTF8nuEQVkrimPVwqtqq1joC56cUwYPftXAEQL',
+        'expected': 'CqCAXF5M51M7xttMuK47TmyuSos8iusFm524ZzaAZnNiner',
+      },
+      {
+        'name': 'Nyota',
+        'encointer': 'E9KVuDLEtBBWSqhCiKn31VPBBLe33CbYJTrnWAbjszwskWH',
+        'expected': 'G8yWL9B48XnbwC5aYpotqUk7ZTcpP7SGQcykoo7TVQTkhwJ',
+      },
+    ];
+
+    for (final testCase in testCases) {
+      test(
+        '${testCase['name']} converts correctly',
+            () async {
+          final locationOnAHK = encointerAccountOnAHK(testCase['encointer']!);
+          final accountId = await api.locationToAccountId(locationOnAHK);
+
+          expect(accountId, AddressUtils.addressToPubKey(testCase['expected']!));
+        },
+        tags: productionE2E,
+      );
+    }
   });
 }
