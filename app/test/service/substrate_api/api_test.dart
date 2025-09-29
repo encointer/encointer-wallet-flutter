3bec717aa5cef0d3aaa6401e15cc5aead6e27c0d
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-29 13:22:09 +0200

    run account conversion tests for all endpoints

--- app/test/service/substrate_api/api_test.dart
+++ app/test/service/substrate_api/api_test.dart
@@ -1,3 +1,4 @@
+import 'package:encointer_wallet/config/networks/networks.dart';
 import 'package:encointer_wallet/models/communities/community_identifier.dart';
 import 'package:encointer_wallet/service/substrate_api/asset_hub/asset_hub_api.dart';
 import 'package:encointer_wallet/service/substrate_api/core/reconnecting_ws_provider.dart';
@@ -16,68 +17,72 @@ void main() {
   late ReconnectingWsProvider encointerProvider;
   late EncointerDartApi encointerApi;
 
-  // Initialize provider once for all tests
-  setUpAll(() {
-    assetHubProvider = ReconnectingWsProvider(
-      Uri.parse('wss://sys.ibp.network/asset-hub-kusama'),
-    );
-    assetHubApi = AssetHubApi(assetHubProvider);
+  for (final endpoint in assetHubKusamaEndpoints()) {
+    group('Asset Hub Kusama Endpoint: ${endpoint.address()}', () {
+      // Initialize provider once for all tests
+      setUpAll(() {
+        assetHubProvider = ReconnectingWsProvider(
+          Uri.parse('wss://sys.ibp.network/asset-hub-kusama'),
+        );
+        assetHubApi = AssetHubApi(assetHubProvider);
 
-    encointerProvider = ReconnectingWsProvider(
-      Uri.parse('wss://kusama.api.encointer.org'),
-    );
-    encointerApi = EncointerDartApi(SubstrateDartApi(encointerProvider));
-  });
+        encointerProvider = ReconnectingWsProvider(
+          Uri.parse('wss://kusama.api.encointer.org'),
+        );
+        encointerApi = EncointerDartApi(SubstrateDartApi(encointerProvider));
+      });
 
-  // Disconnect provider after all tests
-  tearDownAll(() async {
-    await assetHubProvider.disconnect();
-  });
+      // Disconnect provider after all tests
+      tearDownAll(() async {
+        await assetHubProvider.disconnect();
+      });
 
-  group('Resolves Treasury Account Correctly', () {
-    final testCases = [
-      {
-        'name': 'Leu',
-        'cid': 'u0qj944rhWE',
-        'encointer': 'HNJDzJEGaBgWRXz7bjERsRidJFQBnj1AZ2Tn3Q9uRGynhwq',
-        'asset_hub': 'DgdA9qwXxBAtdy9veCR4LZpcbYuMgCSL9XpV7gbELFncV2t',
-      },
-      {
-        'name': 'PayNuq',
-        'cid': 's1vrqQL2SD',
-        'encointer': 'E2mZ1u2xepTF8nuEQVkrimPVwqtqq1joC56cUwYPftXAEQL',
-        'asset_hub': 'CqCAXF5M51M7xttMuK47TmyuSos8iusFm524ZzaAZnNiner',
-      },
-      {
-        'name': 'Nyota',
-        'cid': 'kygch5kVGq7',
-        'encointer': 'E9KVuDLEtBBWSqhCiKn31VPBBLe33CbYJTrnWAbjszwskWH',
-        'asset_hub': 'G8yWL9B48XnbwC5aYpotqUk7ZTcpP7SGQcykoo7TVQTkhwJ',
-      },
-    ];
+      group('Resolves Treasury Account Correctly', () {
+        final testCases = [
+          {
+            'name': 'Leu',
+            'cid': 'u0qj944rhWE',
+            'encointer': 'HNJDzJEGaBgWRXz7bjERsRidJFQBnj1AZ2Tn3Q9uRGynhwq',
+            'asset_hub': 'DgdA9qwXxBAtdy9veCR4LZpcbYuMgCSL9XpV7gbELFncV2t',
+          },
+          {
+            'name': 'PayNuq',
+            'cid': 's1vrqQL2SD',
+            'encointer': 'E2mZ1u2xepTF8nuEQVkrimPVwqtqq1joC56cUwYPftXAEQL',
+            'asset_hub': 'CqCAXF5M51M7xttMuK47TmyuSos8iusFm524ZzaAZnNiner',
+          },
+          {
+            'name': 'Nyota',
+            'cid': 'kygch5kVGq7',
+            'encointer': 'E9KVuDLEtBBWSqhCiKn31VPBBLe33CbYJTrnWAbjszwskWH',
+            'asset_hub': 'G8yWL9B48XnbwC5aYpotqUk7ZTcpP7SGQcykoo7TVQTkhwJ',
+          },
+        ];
 
-    for (final testCase in testCases) {
-      test(
-        '${testCase['name']} has correct treasury accounts on encointer',
-        () async {
-          final cid = CommunityIdentifier.fromFmtString(testCase['cid']!);
-          final encointerAddress = await encointerApi.getTreasuryAccount(cid);
-          expect(AddressUtils.transformPrefix(encointerAddress, 2), testCase['encointer']);
-        },
-        tags: productionE2E,
-      );
-    }
+        for (final testCase in testCases) {
+          test(
+            '${testCase['name']} has correct treasury accounts on encointer',
+                () async {
+              final cid = CommunityIdentifier.fromFmtString(testCase['cid']!);
+              final encointerAddress = await encointerApi.getTreasuryAccount(cid);
+              expect(AddressUtils.transformPrefix(encointerAddress, 2), testCase['encointer']);
+            },
+            tags: productionE2E,
+          );
+        }
 
-    for (final testCase in testCases) {
-      test(
-        '${testCase['name']} has correct treasury accounts on asset hub kusama',
-        () async {
-          final accountId = await assetHubApi.encointerAccountOnAHK(testCase['encointer']!);
+        for (final testCase in testCases) {
+          test(
+            '${testCase['name']} has correct treasury accounts on asset hub kusama',
+                () async {
+              final accountId = await assetHubApi.encointerAccountOnAHK(testCase['encointer']!);
 
-          expect(AddressUtils.pubKeyToAddress(accountId, prefix: 2), testCase['asset_hub']);
-        },
-        tags: productionE2E,
-      );
-    }
-  });
+              expect(AddressUtils.pubKeyToAddress(accountId, prefix: 2), testCase['asset_hub']);
+            },
+            tags: productionE2E,
+          );
+        }
+      });
+    });
+  }
 }
