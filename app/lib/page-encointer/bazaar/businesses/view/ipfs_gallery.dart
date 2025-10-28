b42b1450171a7254e97314833c717be79a6d3887
clangenb <37865735+clangenb@users.noreply.github.com>
2025-10-28 11:58:03 +0800

    fix lints

--- app/lib/page-encointer/bazaar/businesses/view/ipfs_gallery.dart
+++ app/lib/page-encointer/bazaar/businesses/view/ipfs_gallery.dart
@@ -5,15 +5,6 @@ import 'package:encointer_wallet/service/ipfs/ipfs_gallery_stream.dart';
 import 'package:flutter/material.dart';
 
 class IpfsImageGallery extends StatefulWidget {
-  final IpfsApi ipfs;
-  final List<String> cidsOrFolders;
-  final Map<String, List<String>>? includeFiles;
-  final double imageWidth;
-  final double imageHeight;
-  final double borderRadius;
-  final Widget placeholder;
-  final int maxConcurrent;
-
   const IpfsImageGallery({
     super.key,
     required this.ipfs,
@@ -25,6 +16,15 @@ class IpfsImageGallery extends StatefulWidget {
     this.placeholder = const ColoredBox(color: Colors.grey),
     this.maxConcurrent = 4,
   });
+
+  final IpfsApi ipfs;
+  final List<String> cidsOrFolders;
+  final Map<String, List<String>>? includeFiles;
+  final double imageWidth;
+  final double imageHeight;
+  final double borderRadius;
+  final Widget placeholder;
+  final int maxConcurrent;
 
   @override
   State<IpfsImageGallery> createState() => _IpfsImageGalleryState();
