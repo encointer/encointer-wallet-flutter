3ed0381c2ce8a0d4676d7e75bf98e966cd7ef61f
clangenb <37865735+clangenb@users.noreply.github.com>
2025-12-15 08:07:16 +0100

    Refactort CI scripts so that we can always upload recordings

--- scripts/android_integration_test.sh
+++ scripts/android_integration_test.sh
@@ -10,15 +10,6 @@ fi
 
 avdmanager list device || echo "error displaying emulator devices"
 
-if [ "$RECORD" == "true" ]
-then
-  # default / maximum recording time is 180 seconds
-  adb shell screenrecord /sdcard/recording.mp4 &
-
-  export RECORDING_PID=${!}
-  echo "Recording process up with pid: ${RECORDING_PID}"
-fi
-
 echo "🚀 Running Flutter integration tests with WS_ENDPOINT=$WS_ENDPOINT"
 
 cd app
@@ -40,19 +31,4 @@ cd app
 
 cd ..
 
-mkdir -p "$TEMP_DIR"
-
-if [ "$RECORD" == "true" ]
-then
-  echo "killing recording process"
-  sleep 5
-
-  kill -SIGINT $RECORDING_PID || echo "Recording process already stopped"
-  sleep 10
-
-  adb pull /sdcard/recording.mp4  "$TEMP_DIR" || echo "could not fetch recording from device"
-fi
-
-# copy screenshots to TEMP_DIR
-cp -r "$TEMP_DIR/test"/* "$TEMP_DIR" || echo "no screenshots found..."
-rm -r "$TEMP_DIR/test" || true
+echo "✅ Flutter integration tests completed successfully"
