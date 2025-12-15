9579bb4e4f9d2e959597a0e92c4d15794fb08c3c
clangenb <37865735+clangenb@users.noreply.github.com>
2025-12-15 09:58:09 +0100

    Revert "Refactort CI scripts so that we can always upload recordings"
    This reverts commit 3ed0381c

--- scripts/android_integration_test.sh
+++ scripts/android_integration_test.sh
@@ -10,6 +10,15 @@ fi
 
 avdmanager list device || echo "error displaying emulator devices"
 
+if [ "$RECORD" == "true" ]
+then
+  # default / maximum recording time is 180 seconds
+  adb shell screenrecord /sdcard/recording.mp4 &
+
+  export RECORDING_PID=${!}
+  echo "Recording process up with pid: ${RECORDING_PID}"
+fi
+
 echo "🚀 Running Flutter integration tests with WS_ENDPOINT=$WS_ENDPOINT"
 
 cd app
@@ -31,4 +40,19 @@ cd app
 
 cd ..
 
-echo "✅ Flutter integration tests completed successfully"
+mkdir -p "$TEMP_DIR"
+
+if [ "$RECORD" == "true" ]
+then
+  echo "killing recording process"
+  sleep 5
+
+  kill -SIGINT $RECORDING_PID || echo "Recording process already stopped"
+  sleep 10
+
+  adb pull /sdcard/recording.mp4  "$TEMP_DIR" || echo "could not fetch recording from device"
+fi
+
+# copy screenshots to TEMP_DIR
+cp -r "$TEMP_DIR/test"/* "$TEMP_DIR" || echo "no screenshots found..."
+rm -r "$TEMP_DIR/test" || true
