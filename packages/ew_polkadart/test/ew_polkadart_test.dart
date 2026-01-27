f84206124daed6624c52364cb36aa4f2a0df78c1
clangenb <37865735+clangenb@users.noreply.github.com>
2026-01-27 15:08:18 +0100

    Add explicit proxy.Proxies test

--- packages/ew_polkadart/test/ew_polkadart_test.dart
+++ packages/ew_polkadart/test/ew_polkadart_test.dart
@@ -1,4 +1,5 @@
 import 'package:ew_polkadart/generated/asset_hub_kusama/types/sp_core/crypto/account_id32.dart';
+import 'package:ew_polkadart/generated/encointer_kusama/encointer_kusama.dart';
 import 'package:polkadart/scale_codec.dart' show decodeHex;
 import 'package:polkadart/substrate/substrate.dart' show StorageHasher, StorageMap;
 import 'package:polkadart_scale_codec/polkadart_scale_codec.dart' show encodeHex;
@@ -27,6 +28,27 @@ void main() {
       final expectedHash = decodeHex(module + method + twox64ConcatAccountId32);
 
       final key = storageMap.hashedKeyFor(accountId);
+
+      expect(
+        encodeHex(key),
+        encodeHex(expectedHash),
+        reason: 'Twox64 hash of 32-byte AccountId should match PJS',
+      );
+    });
+
+    // This was broken once, so we have an explicit test to confirm it works.
+    test('Proxy.proxies', () {
+      final accountId = decodeHex(
+        '26be5a2c3b0c85d4f4783e2950a293b0c9f6026e5c5a897bbb9d0746a52de907',
+      );
+
+      // Expected output (from Polkadot.js)
+      final module = '1809d78346727a0ef58c0fa03bafa323';
+      final method = '1d885dcfb277f185f2d8e62a5f290c85';
+      final twox64ConcatAccountId32 = '427611b5147e9bca26be5a2c3b0c85d4f4783e2950a293b0c9f6026e5c5a897bbb9d0746a52de907';
+      final expectedHash = decodeHex(module + method + twox64ConcatAccountId32);
+
+      final key = EncointerKusama.url(Uri.parse("ws://localhost:9944")).query.proxy.proxiesKey(accountId.toList());
 
       expect(
         encodeHex(key),
