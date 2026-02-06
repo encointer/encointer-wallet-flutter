6b29669054c7e2774f39ab9ab9df1f1973f67ad1
clangenb <37865735+clangenb@users.noreply.github.com>
2026-02-06 18:17:07 +0100

    Fix mock definitions

---
+++ app/test/mock/api/mock_ipfs_upload_service.dart
@@ -0,0 +1,5 @@
+import 'package:encointer_wallet/service/ipfs/ipfs_upload_service.dart';
+
+class MockIpfsUploadService extends IpfsUploadService {
+  MockIpfsUploadService(super._authService, {required super.gatewayUrl});
+}
